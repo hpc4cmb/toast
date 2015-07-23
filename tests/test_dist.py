cd795accad38f0f9ef2a33f242f7999488b62764
Theodore Kisner <tskisner.public@gmail.com>
2015-07-23 09:41:06 -0700

    relative imports in unit tests work now

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -3,7 +3,7 @@
 # a BSD-style license that can be found in the LICENSE file.
 
 from mpi4py import MPI
-import mpiunittest as unittest
+from . import mpiunittest as unittest
 import sys
 
 from toast.dist import *
