9b017f1a5e2acadfc1a95c66681c190c3624de54
Theodore Kisner <mail@theodorekisner.com>
2016-05-20 08:02:48 -0700

    Fix syntax typos in RNG cython code.

--- toast/_rng.pyx
+++ toast/_rng.pyx
@@ -9,11 +9,10 @@ ctypedef np.int64_t i64_t
 ctypedef np.uint64_t u64_t
 
 cdef extern from "pytoast.h":
-    void generate_grv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array);
-    void generate_neg11rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array);
-    void generate_01rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array);
-    void generate_uint64rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, u64_t* rand_array);
-    pass
+    void generate_grv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array)
+    void generate_neg11rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array)
+    void generate_01rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, f64_t* rand_array)
+    void generate_uint64rv(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, u64_t* rand_array)
 
 
 def cbrng_normal(u64_t size, u64_t offset, u64_t counter1, u64_t counter2, u64_t key1, u64_t key2, np.ndarray[f64_t, ndim=1] rand_array):
