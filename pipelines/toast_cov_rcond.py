9995586a82bdbf929d2329c2b9b9e1e2cf34cbac
Theodore Kisner <mail@theodorekisner.com>
2016-06-15 07:51:54 -0700

    Add helper script which computes the inverse condition number map from a covariance FITS file.

---
+++ pipelines/toast_cov_rcond.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import mpi4py.MPI as MPI
+
+import os
+import sys
+import re
+import argparse
+
+import numpy as np
+
+import healpy as hp
+
+import toast
+import toast.map as tm
+
+
+def main():
+
+    comm = MPI.COMM_WORLD
+
+    if comm.rank == 0:
+        print("Running with {} processes".format(comm.size))
+
+    global_start = MPI.Wtime()
+
+    parser = argparse.ArgumentParser( description='Read a toast covariance matrix and write the inverse condition number map' )
+    parser.add_argument( '--input', required=True, default=None, help='The input covariance FITS file' )
+    parser.add_argument( '--output', required=False, default=None, help='The output inverse condition map FITS file.' )
+    
+    args = parser.parse_args()
+
+    # get options
+
+    infile = args.input
+    outfile = None
+    if args.output is not None:
+        outfile = args.output
+    else:
+        inmat = re.match(r'(.*)\.fits', infile)
+        if inmat is None:
+            print("input file should have .fits extension")
+            sys.exit(0)
+        inroot = inmat.group(1)
+        outfile = "{}_rcond.fits".format(inroot)
+
+    # We need to read the header to get the size of the matrix.
+    # This would be a trivial function call in astropy.fits or
+    # fitsio, but we don't want to bring in a whole new dependency
+    # just for that.  Instead, we open the file with healpy in memmap
+    # mode so that nothing is actually read except the header.
+
+    fake, head = hp.read_map(infile, h=True, memmap=True)
+
+    nside = 0
+    nnz = 0
+    for key, val in head:
+        if key == 'NSIDE':
+            nside = int(val)
+        if key == 'TFIELDS':
+            nnz = int(val)
+
+    npix = 12 * nside**2
+    subnside = int(nside / 16)
+    subnpix = 12 * subnside**2
+    nsubmap = int( npix / subnpix )
+
+    # divide the submaps as evenly as possible among processes
+
+    dist = toast.distribute_uniform(nsubmap, comm.size)
+    local = np.arange(dist[comm.rank][0], dist[comm.rank][0] + dist[comm.rank][1])
+
+    print("proc {} has submaps {} -> {}".format(comm.rank, dist[comm.rank][0], dist[comm.rank][0] + dist[comm.rank][1]))
+
+    # create the covariance and inverse condition number map
+
+    cov = tm.DistPixels(comm=comm, size=npix, nnz=nnz, submap=subnpix, local=local)
+    rcond = tm.DistPixels(comm=comm, size=npix, nnz=1, submap=subnpix, local=local)
+
+    # read the covariance
+
+    cov.read_healpix_fits(infile)
+
+    # every process computes its local piece
+
+    rcond.data[:] = tm.covariance_rcond(cov.data)
+
+    # write the map
+
+    rcond.write_healpix_fits(outfile)
+
+
+
+if __name__ == "__main__":
+    main()
+
