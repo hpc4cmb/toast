Change the way data is passed between the high-level and cython quaternion packages.  Should also fix problem with quaternion array multiplied by single quaternion.

tests/test_qarray.py
@@ -28,22 +28,9 @@ class QarrayTest(MPITestCase):
         self.vec2 = np.array([[ 0.57734543,  8.30271255,  5.75831218],[ 1.57734543,  3.30271255,  0.75831218]])
         self.qeasy = np.array([[.3,.3,.1,.9],[.3,.3,.1,.9]])
         #results from Quaternion CHANGED SIGN TO COMPLY WITH THE ONLINE QUATERNION CALCULATOR
-        self.mult_result = -1*np.array([[-0.44954009, -0.53339352, -0.37370443,  0.61135101]])
-        self.rot_by_q1 = np.array([[0.4176698, 0.84203849, 0.34135482]])
-        self.rot_by_q2 = np.array([[0.8077876, 0.3227185, 0.49328689]])
-
-
-    def test_arraylist_dot_onedimarrays(self):
-        np.testing.assert_array_almost_equal(qarray.arraylist_dot(self.vec, self.vec +1), np.dot(self.vec, self.vec +1)) 
-
-
-    def test_arraylist_dot_1dimbymultidim(self):
-        np.testing.assert_array_almost_equal(qarray.arraylist_dot(self.vec2, self.vec), np.dot(self.vec2,self.vec)) 
-
-
-    def test_arraylist_dot_multidim(self):
-        result = np.hstack(np.dot(v1,v2) for v1,v2 in zip(self.vec2, self.vec2+1))
-        np.testing.assert_array_almost_equal(qarray.arraylist_dot(self.vec2, self.vec2 +1), result) 
+        self.mult_result = -1*np.array([-0.44954009, -0.53339352, -0.37370443,  0.61135101])
+        self.rot_by_q1 = np.array([0.4176698, 0.84203849, 0.34135482])
+        self.rot_by_q2 = np.array([0.8077876, 0.3227185, 0.49328689])
 
 
     def test_inv(self):
@@ -57,8 +44,6 @@ class QarrayTest(MPITestCase):
 
     def test_mult_onequaternion(self):
         my_mult_result = qarray.mult(self.q1, self.q2)
-        self.assertEquals( my_mult_result.shape[0], 1)
-        self.assertEquals( my_mult_result.shape[1], 4)
         np.testing.assert_array_almost_equal(my_mult_result , self.mult_result)
 
 
@@ -69,6 +54,15 @@ class QarrayTest(MPITestCase):
         my_mult_result = qarray.mult(qarray1, qarray2)
         np.testing.assert_array_almost_equal(my_mult_result , np.tile(self.mult_result,dim))
 
+        check = qarray.mult(self.q1, self.q2)
+        res = qarray.mult(np.tile(self.q1, 10).reshape((-1, 4)), self.q2)
+        np.testing.assert_array_almost_equal(res, np.tile(check, 10).reshape((-1, 4)))
+
+        nulquat = np.array([0.0, 0.0, 0.0, 1.0])
+        check = qarray.mult(self.q1, nulquat)
+        res = qarray.mult(np.tile(self.q1, 10).reshape((-1, 4)), nulquat)
+        np.testing.assert_array_almost_equal(res, np.tile(check, 10).reshape((-1, 4)))
+
 
     def test_rotate_onequaternion(self):
         my_rot_result = qarray.rotate(self.q1, self.vec)
@@ -77,7 +71,7 @@ class QarrayTest(MPITestCase):
         
     def test_rotate_qarray(self):
         my_rot_result = qarray.rotate(np.vstack([self.q1,self.q2]), self.vec)
-        np.testing.assert_array_almost_equal(my_rot_result , np.vstack([self.rot_by_q1, self.rot_by_q2]))
+        np.testing.assert_array_almost_equal(my_rot_result , np.vstack([self.rot_by_q1, self.rot_by_q2]).reshape((2, 3)))
 
 
     def test_slerp(self):
@@ -177,7 +171,7 @@ class QarrayTest(MPITestCase):
 
             detdir = qarray.rotate(detrot, zaxis)
 
-            check = np.dot(detdir, zaxis)[0]
+            check = np.dot(detdir, zaxis)
 
             np.testing.assert_almost_equal(check, np.cos(radius))
 

toast/tod/qarray.py
@@ -8,186 +8,208 @@ import numpy as np
 from . import _qarray as qc
 
 
-def arraylist_dot(a, b):
-    '''Dot product of a lists of arrays, returns a column array'''
-    na = 1
-    if a.ndim > 1:
-        na = a.shape[0]
-    nb = 1
-    if b.ndim > 1:
-        nb = b.shape[0]
-    if (na > 1) and (nb > 1) and (na != nb):
-        raise RuntimeError("quaternion arrays must have length one or matching lengths.")
-    n = np.max([na, nb])
-
-    aa = a
-    if a.ndim == 1:
-        aa = np.tile(a, n).reshape((-1, a.shape[0]))
-    bb = b
-    if b.ndim == 1:
-        bb = np.tile(b, n).reshape((-1, b.shape[0]))
-
-    return qc.arraylist_dot(aa, bb)
-
-
 def inv(q):
     """Inverse of quaternion array q"""
-    qq = q
+    nq = None
     if q.ndim == 1:
-        qq = q.reshape((1, q.shape[0]))
-    res = qc.inv(qq)
-    if q.ndim == 1:
-        return res.flatten()
+        nq = 1
     else:
-        return res
+        nq = q.shape[0]
+    return qc.inv(nq, q.flatten())
 
 
 def amplitude(v):
-    vv = v
+    """Amplitude of a vector array"""
+    nv = None
+    nm = None
     if v.ndim == 1:
-        vv = v.reshape((1, v.shape[0]))
-    res = qc.amplitude(vv)
-    if v.ndim == 1:
-        return res.flatten()
+        nv = 1
+        nm = v.shape[0]
     else:
-        return res
+        nv = v.shape[0]
+        nm = v.shape[1]
+    return qc.amplitude(nv, nm, v.flatten())
 
 
 def norm(q):
-    """Normalize quaternion array q or array list to unit quaternions"""
-    qq = q
+    """Normalize quaternion array to unit quaternions"""
+    nq = None
     if q.ndim == 1:
-        qq = q.reshape((1, q.shape[0]))
-    res = qc.norm(qq)
-    if q.ndim == 1:
-        return res.flatten()
+        nq = 1
     else:
-        return res
+        nq = q.shape[0]
+    return qc.norm(nq, q.flatten())
 
 
 def rotate(q, v):
     """
     Use a quaternion or array of quaternions (q) to rotate a vector or 
-    array of vectors (v).  If the dimensions of both q and v are 2, then
-    they must have the same leading dimension.
+    array of vectors (v).  If the number of dimensions of both q and v 
+    are 2, then they must have the same leading dimension.
     """
-    nq = 1
-    if q.ndim > 1:
+    nq = None
+    if q.ndim == 1:
+        nq = 1
+    else:
         nq = q.shape[0]
-    nv = 1
-    if v.ndim > 1:
+    nv = None
+    if v.ndim == 1:
+        nv = 1
+    else:
         nv = v.shape[0]
+
     if (nq > 1) and (nv > 1) and (nq != nv):
         raise RuntimeError("quaternion and vector arrays must have length one or matching lengths.")
     n = np.max([nq, nv])
 
     vv = v
-    if v.ndim == 1:
-        vv = np.tile(v, n).reshape((-1, 3))
+    if nv != n:
+        vv = np.tile(v, n)
     qq = q
-    if q.ndim == 1:
-        qq = np.tile(q, n).reshape((-1, 4))
+    if nq != n:
+        qq = np.tile(q, n)
 
-    return qc.rotate(qq, vv)
+    return qc.rotate(n, qq.flatten(), vv.flatten())
 
 
 def mult(p, q):
     """Multiply arrays of quaternions, see:
     http://en.wikipedia.org/wiki/Quaternions#Quaternions_and_the_geometry_of_R3
     """
-    qn = 1
-    if q.ndim > 1:
-        qn = q.shape[0]
-    pn = 1
-    if p.ndim > 1:
+    nq = None
+    if q.ndim == 1:
+        nq = 1
+    else:
+        nq = q.shape[0]
+    pn = None
+    if p.ndim == 1:
+        pn = 1
+    else:
         pn = p.shape[0]
-    if (qn > 1) and (pn > 1) and (qn != pn):
+
+    if (nq > 1) and (pn > 1) and (nq != pn):
         raise RuntimeError("quaternion arrays must have length one or matching lengths.")
-    n = np.max([qn, pn])
+    n = np.max([nq, pn])
 
     pp = p
-    if p.ndim == 1:
-        pp = np.tile(p, n).reshape((-1, 4))
+    if pn != n:
+        pp = np.tile(p, n)
     qq = q
-    if q.ndim == 1:
-        qq = np.tile(q, n).reshape((-1, 4))
+    if nq != n:
+        qq = np.tile(q, n)
 
-    return qc.mult(pp, qq)
+    return qc.mult(n, pp.flatten(), qq.flatten())
 
 
 def slerp(targettime, time, q):
     """Slerp, q quaternion array interpolated from time to targettime"""
-    return qc.slerp(targettime, time, q)
+    return qc.slerp(targettime, time, q.flatten())
 
 
 def exp(q):
     """Exponential of a quaternion array"""
-    qq = q
+    nq = None
     if q.ndim == 1:
-        qq = q.reshape((1, q.shape[0]))
-    print(qq)
-    return qc.exp(qq)
+        nq = 1
+    else:
+        nq = q.shape[0]    
+    return qc.exp(nq, q.flatten())
 
 
 def ln(q):
     """Natural logarithm of a quaternion array"""
-    qq = q
+    nq = None
     if q.ndim == 1:
-        qq = q.reshape((1, q.shape[0]))
-    return qc.ln(qq)
+        nq = 1
+    else:
+        nq = q.shape[0]    
+    return qc.ln(nq, q.flatten())
 
 
 def pow(q, p):
     """Real power of a quaternion array"""
-    qq = q
+    nq = None
     if q.ndim == 1:
-        qq = q.reshape((1, q.shape[0]))
-    pp = p
-    if not isinstance(p, np.ndarray):
-        pp = np.tile(p, qq.shape[0])
-    return qc.pow(qq, pp)
+        nq = 1
+    else:
+        nq = q.shape[0]
+    pn = None
+    if isinstance(p, np.ndarray):
+        pn = p.shape[0]
+    else:
+        pn = 1
+
+    if (nq > 1) and (pn > 1) and (nq != pn):
+        raise RuntimeError("quaternion array and power must have length one or matching lengths.")
+    n = np.max([nq, pn])
+
+    pp = None
+    if isinstance(p, np.ndarray):
+        if pn != n:
+            pp = np.tile(p, n)
+        else:
+            pp = p
+    else:
+        pp = np.tile(p, n)
+
+    qq = q
+    if nq != n:
+        qq = np.tile(q, n)
+
+    return qc.pow(qq.flatten(), pp.flatten())
 
     
 def rotation(axis, angle):
     """Rotation quaternions of angles [rad] around axes [already normalized]"""
-    nax = 1
-    if axis.ndim > 1:
+    nax = None
+    if axis.ndim == 1:
+        nax = 1
+    else:
         nax = axis.shape[0]
-    nang = 1
+    nang = None
     if isinstance(angle, np.ndarray):
-        # we have more than one angle
         nang = angle.shape[0]
+    else:
+        nang = 1
+
     if (nax > 1) and (nang > 1) and (nax != nang):
         raise RuntimeError("axis and angle arrays must have length one or matching lengths.")
     n = np.max([nax, nang])
 
     ax = axis
-    if axis.ndim == 1:
-        ax = np.tile(axis, n).reshape((-1, 3))
-    ang = angle
-    if nang == 1:
-        ang = np.tile(angle, n)
+    if nax != n:
+        ax = np.tile(axis, n)
 
-    res = qc.rotation(ax, ang)
-    if n == 1:
-        return res.flatten()
+    ang = None
+    if isinstance(angle, np.ndarray):
+        if nang != n:
+            ang = np.tile(angle, n)
+        else:
+            ang = angle
     else:
-        return res
+        ang = np.tile(angle, n) 
+
+    return qc.rotation(ax.flatten(), ang.flatten())
 
 
 def to_axisangle(q):
-    return qc.to_axisangle(q)
+    nq = None
+    if q.ndim == 1:
+        nq = 1
+    else:
+        nq = q.shape[0]
+    return qc.to_axisangle(nq, q.flatten())
 
 
 def to_rotmat(q):
     """Rotation matrix"""
-    return qc.to_rotmat(q)
+    return qc.to_rotmat(q.flatten())
                                 
 
 def from_rotmat(rotmat):
-    return qc.from_rotmat(rotmat)
+    return qc.from_rotmat(rotmat.flatten())
 
 
 def from_vectors(v1, v2):
-    return qc.from_vectors(v1, v2)
+    return qc.from_vectors(v1.flatten(), v2.flatten())
 