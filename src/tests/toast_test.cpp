b338587c41bef14f4a06e4896c4647dba63fbe69
Theodore Kisner <mail@theodorekisner.com>
2016-11-12 21:27:38 -0800

    Begin layout of compiled libraries and autotools build system.

---
+++ src/tests/toast_test.cpp
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2015-2017 by the parties listed in the AUTHORS file.
+All rights reserved.  Use of this source code is governed by 
+a BSD-style license that can be found in the LICENSE file.
+*/
+
+
+#include <toast_test.hpp>
+
+
+using namespace std;
+using namespace toast;
+
+
+int main ( int argc, char *argv[] ) {
+
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+
+}
+
+
---
+++ src/tests/toast_test.hpp
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2015-2017 by the parties listed in the AUTHORS file.
+All rights reserved.  Use of this source code is governed by 
+a BSD-style license that can be found in the LICENSE file.
+*/
+
+
+#ifndef TOAST_TEST_HPP
+#define TOAST_TEST_HPP
+
+//#include <toast_internal.hpp>
+
+#include <gtest/gtest.h>
+
+// #include <ctoast.h>
+
+
+
+
+
+#endif
