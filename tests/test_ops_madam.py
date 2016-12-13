f4d4889284939bfec9d19dc93d024fd16d748a7e
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2016-12-13 13:25:33 -0800

    Add a unit test for Madam output. Force Madam to return destriped timelines when OpMadam is returning timelines. Fix off-by-one error in OpMadam array packing.

--- tests/test_ops_madam.py
+++ tests/test_ops_madam.py
@@ -108,6 +108,7 @@ class OpMadamTest(MPITestCase):
         pars[ 'kfilter' ] = 'F'
         pars[ 'run_submap_test' ] = 'F'
         pars[ 'path_output' ] = self.mapdir
+        pars[ 'info' ] = 0
 
         madam = OpMadam(params=pars, name='grad', dets=self.dets)
         if madam.available:
@@ -118,3 +119,59 @@ class OpMadamTest(MPITestCase):
         else:
             print("libmadam not available, skipping tests")
 
+    def test_madam_output(self):
+        start = MPI.Wtime()
+
+        # add simple sky gradient signal
+        grad = OpSimGradient(nside=self.sim_nside)
+        grad.exec(self.data)
+
+        # make a simple pointing matrix
+        pointing = OpPointingHpix(nside=self.map_nside, nest=True)
+        pointing.exec(self.data)
+
+        handle = None
+        if self.comm.rank == 0:
+            handle = open(os.path.join(self.outdir,"out_test_madam_info"), "w")
+        self.data.info(handle)
+        if self.comm.rank == 0:
+            handle.close()
+
+        pars = {}
+        pars[ 'kfirst' ] = 'T'
+        pars[ 'iter_max' ] = 100
+        pars[ 'base_first' ] = 1.0
+        pars[ 'fsample' ] = self.rate
+        pars[ 'nside_map' ] = self.map_nside
+        pars[ 'nside_cross' ] = self.map_nside
+        pars[ 'nside_submap' ] = self.map_nside
+        pars[ 'write_map' ] = 'F'
+        pars[ 'write_binmap' ] = 'T'
+        pars[ 'write_matrix' ] = 'F'
+        pars[ 'write_wcov' ] = 'F'
+        pars[ 'write_hits' ] = 'T'
+        pars[ 'kfilter' ] = 'F'
+        pars[ 'run_submap_test' ] = 'F'
+        pars[ 'path_output' ] = self.mapdir
+        pars[ 'info' ] = 0
+
+        madam = OpMadam(params=pars, name='grad', name_out='destriped', dets=self.dets)
+        if madam.available:
+            tod = self.data.obs[0]['tod']
+            det = 'bore'
+            ref_in = tod.cache.reference('grad_'+det)
+            rms0 = np.std(ref_in)
+            ref_in[ref_in.size//2:] += 1e6 # Add an offset
+            rms1 = np.std(ref_in)
+
+            madam.exec(self.data)
+            stop = MPI.Wtime()
+            elapsed = stop - start
+            self.print_in_turns("Madam test took {:.3f} s".format(elapsed))
+
+            ref_out = tod.cache.reference('destriped_'+det)
+            rms2 = np.std(ref_out)
+            if rms1 < 0.9*rms2:
+                raise Exception('Destriped TOD does not have lower RMS')
+        else:
+            print("libmadam not available, skipping tests")
