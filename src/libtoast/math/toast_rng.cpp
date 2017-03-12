7349e0a88a90fdb9361953de4924d43392a31990
Theodore Kisner <mail@theodorekisner.com>
2017-03-12 15:59:17 -0700

    Add threading to RNG functions.

--- src/libtoast/math/toast_rng.cpp
+++ src/libtoast/math/toast_rng.cpp
@@ -22,17 +22,25 @@ typedef r123::Threefry2x64 RNG;
 // Native unsigned 64bit integer randoms.
 
 void toast::rng::dist_uint64 ( size_t n, uint64_t key1, uint64_t key2, uint64_t counter1, uint64_t counter2, uint64_t * data ) {
-    RNG rng;
-    RNG::ctr_type c = {{ counter1, counter2 }};
-    RNG::ukey_type uk = {{ key1, key2 }};
-    RNG::key_type k = uk;
+    
+    #pragma omp parallel default(shared)
+    {
+        RNG rng;
+        RNG::ctr_type c;
+        RNG::ukey_type uk = {{ key1, key2 }};
+        RNG::key_type k = uk;
+        RNG::ctr_type r;
 
-    RNG::ctr_type r;
-    for ( size_t i = 0; i < n; ++i ) {
-        r = rng ( c, k );
-        data[i] = r[0];
-        c[1] += 1;
+        c[0] = counter1;
+
+        #pragma omp parallel for schedule(static)
+        for ( size_t i = 0; i < n; ++i ) {
+            c[1] = counter2 + i;
+            r = rng ( c, k );
+            data[i] = r[0];
+        }
     }
+
     return;
 }
 
@@ -40,17 +48,25 @@ void toast::rng::dist_uint64 ( size_t n, uint64_t key1, uint64_t key2, uint64_t 
 // Uniform double precision values on [0.0, 1.0]
 
 void toast::rng::dist_uniform_01 ( size_t n, uint64_t key1, uint64_t key2, uint64_t counter1, uint64_t counter2, double * data ) {
-    RNG rng;
-    RNG::ctr_type c = {{ counter1, counter2 }};
-    RNG::ukey_type uk = {{ key1, key2 }};
-    RNG::key_type k = uk;
 
-    RNG::ctr_type r;
-    for ( size_t i = 0; i < n; ++i ) {
-        r = rng ( c, k );
-        data[i] = r123::u01 < double, uint64_t > ( r[0] );
-        c[1] += 1;
+    #pragma omp parallel default(shared)
+    {
+        RNG rng;
+        RNG::ctr_type c;
+        RNG::ukey_type uk = {{ key1, key2 }};
+        RNG::key_type k = uk;
+        RNG::ctr_type r;
+
+        c[0] = counter1;
+
+        #pragma omp parallel for schedule(static)
+        for ( size_t i = 0; i < n; ++i ) {
+            c[1] = counter2 + i;
+            r = rng ( c, k );
+            data[i] = r123::u01 < double, uint64_t > ( r[0] );
+        }
     }
+
     return;
 }
 
@@ -58,17 +74,25 @@ void toast::rng::dist_uniform_01 ( size_t n, uint64_t key1, uint64_t key2, uint6
 // Uniform double precision values on [-1.0, 1.0]
 
 void toast::rng::dist_uniform_11 ( size_t n, uint64_t key1, uint64_t key2, uint64_t counter1, uint64_t counter2, double * data ) {
-    RNG rng;
-    RNG::ctr_type c = {{ counter1, counter2 }};
-    RNG::ukey_type uk = {{ key1, key2 }};
-    RNG::key_type k = uk;
 
-    RNG::ctr_type r;
-    for ( size_t i = 0; i < n; ++i ) {
-        r = rng ( c, k );
-        data[i] = r123::uneg11 < double, uint64_t > ( r[0] );
-        c[1] += 1;
+    #pragma omp parallel default(shared)
+    {
+        RNG rng;
+        RNG::ctr_type c;
+        RNG::ukey_type uk = {{ key1, key2 }};
+        RNG::key_type k = uk;
+        RNG::ctr_type r;
+
+        c[0] = counter1;
+
+        #pragma omp parallel for schedule(static)
+        for ( size_t i = 0; i < n; ++i ) {
+            c[1] = counter2 + i;
+            r = rng ( c, k );
+            data[i] = r123::uneg11 < double, uint64_t > ( r[0] );
+        }
     }
+
     return;
 }
 
@@ -76,17 +100,25 @@ void toast::rng::dist_uniform_11 ( size_t n, uint64_t key1, uint64_t key2, uint6
 // Normal distribution.
 
 void toast::rng::dist_normal ( size_t n, uint64_t key1, uint64_t key2, uint64_t counter1, uint64_t counter2, double * data ) {
-    RNG rng;
-    RNG::ctr_type c = {{ counter1, counter2 }};
-    RNG::ukey_type uk = {{ key1, key2 }};
-    RNG::key_type k = uk;
 
-    RNG::ctr_type r;
-    for ( size_t i = 0; i < n; ++i ) {
-        r = rng ( c, k );
-        data[i] = ::sqrt ( -2.0 * ::log ( r123::u01 < double, uint64_t > ( r[0] ) ) ) * ::cos ( TWOPI * r123::u01 < double, uint64_t > ( r[1] ) );
-        c[1] += 1;
+    #pragma omp parallel default(shared)
+    {
+        RNG rng;
+        RNG::ctr_type c;
+        RNG::ukey_type uk = {{ key1, key2 }};
+        RNG::key_type k = uk;
+        RNG::ctr_type r;
+
+        c[0] = counter1;
+
+        #pragma omp parallel for schedule(static)
+        for ( size_t i = 0; i < n; ++i ) {
+            c[1] = counter2 + i;
+            r = rng ( c, k );
+            data[i] = ::sqrt ( -2.0 * ::log ( r123::u01 < double, uint64_t > ( r[0] ) ) ) * ::cos ( TWOPI * r123::u01 < double, uint64_t > ( r[1] ) );
+        }
     }
+
     return;
 }
 
