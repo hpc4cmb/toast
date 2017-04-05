dff2f37c31887c6ce0c35ccec32cc55599bbb905
Theodore Kisner <mail@theodorekisner.com>
2017-04-05 16:56:23 -0700

    Install pipeline scripts.  Add simulated focalplane layout functions for both hexagon and rhombus shapes.  Add script to generate a single large hexagonal focalplane.

--- pipelines/toast_satellite_sim.py
+++ pipelines/toast_satellite_sim.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2015-2017 by the parties listed in the AUTHORS file.
+# All rights reserved.  Use of this source code is governed by 
+# a BSD-style license that can be found in the LICENSE file.
+
 from toast.mpi import MPI
 
 import os
