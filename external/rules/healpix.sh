e55954817ecde4b718bfcdebecf00317cdb692b6
Theodore Kisner <tskisner.public@gmail.com>
2017-05-04 08:46:35 -0700

    Update healpix version in external dependencies.

--- external/rules/healpix.sh
+++ external/rules/healpix.sh
@@ -1,7 +1,7 @@
-curl -SL https://github.com/tskisner/healpix-autotools/releases/download/v3.31.1/healpix-autotools-3.31.1.tar.gz \
-    -o healpix-autotools-3.31.1.tar.gz \
-    && tar -xzf healpix-autotools-3.31.1.tar.gz \
-    && cd healpix-autotools-3.31.1 \
+curl -SL https://github.com/tskisner/healpix-autotools/releases/download/v3.31.2/healpix-autotools-3.31.2.tar.gz \
+    -o healpix-autotools-3.31.2.tar.gz \
+    && tar -xzf healpix-autotools-3.31.2.tar.gz \
+    && cd healpix-autotools-3.31.2 \
     && CC="@CC@" CXX="@CXX@" FC="@FC@" \
     CFLAGS="@CFLAGS@ -std=gnu99" CXXFLAGS="@CXXFLAGS@ -std=c++98" FCFLAGS="@FCFLAGS@" \
     ./configure @CROSS@ --with-cfitsio="@AUX_PREFIX@" --prefix="@AUX_PREFIX@" \
