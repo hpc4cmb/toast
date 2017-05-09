f0a944b38f9b777fa056bd33591aa6328160a276
Theodore Kisner <tskisner.public@gmail.com>
2017-05-08 19:23:00 -0500

    Add configs for stampede KNL.  When configuring madam, also pass the MPI C compiler.

--- external/rules/madam.sh
+++ external/rules/madam.sh
@@ -3,6 +3,7 @@ curl -SL https://github.com/hpc4cmb/libmadam/releases/download/0.2.2/libmadam-0.
     && tar -xjf libmadam-0.2.2.tar.bz2 \
     && cd libmadam-0.2.2 \
     && FC="@MPIFC@" MPIFC="@MPIFC@" FCFLAGS="@FCFLAGS@" \
+    && CC="@MPICC@" MPICC="@MPICC@" CFLAGS="@CFLAGS@" \
     ./configure @CROSS@ --with-cfitsio="@AUX_PREFIX@" \
     --with-blas="@BLAS@" --with-lapack="@LAPACK@" \
     --with-fftw="@AUX_PREFIX@" --prefix="@AUX_PREFIX@" \
