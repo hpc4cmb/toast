Use the TOAST random number generator. Use the correct elemental method for applying covariance.

pipelines/toast_ground_sim.py
@@ -88,7 +88,8 @@ def main():
     comm = toast.Comm()
 
     if comm.comm_world.rank == 0:
-        print("Running with {} processes".format(comm.comm_world.size))
+        print("Running with {} processes".format(comm.comm_world.size),
+              flush=True)
 
     global_start = MPI.Wtime()
 
@@ -140,10 +141,12 @@ def main():
                         required=False, default=0.0, type=np.float,
                         help='The rate (in RPM) of the HWP rotation')
     parser.add_argument('--hwpstep', required=False, default=None,
-                        help='For stepped HWP, the angle in degrees of each step')
+                        help='For stepped HWP, the angle in degrees '
+                        'of each step')
     parser.add_argument('--hwpsteptime',
                         required=False, default=0.0, type=np.float,
-                        help='For stepped HWP, the the time in seconds between steps')
+                        help='For stepped HWP, the the time in seconds '
+                        'between steps')
     parser.add_argument('--CES_start',
                         required=False, default=0, type=np.float,
                         help='Start time of the CES')
@@ -187,9 +190,6 @@ def main():
     parser.add_argument('--atm_fnear',
                         required=False, default=0.1, type=np.float,
                         help='multiplier for the near field simulation')
-    parser.add_argument('--atm_fixed_r',
-                        required=False, default=-1, type=np.int,
-                        help='positive number for start of integration')
     parser.add_argument('--atm_w_center',
                         required=False, default=10.0, type=np.float,
                         help='central value of the wind speed distribution')
@@ -320,7 +320,8 @@ def main():
     stop = MPI.Wtime()
     elapsed = stop - start
     if comm.comm_world.rank == 0:
-        print('Create focalplane:  {:.2f} seconds'.format(stop-start))
+        print('Create focalplane:  {:.2f} seconds'.format(stop-start),
+              flush=True)
     start = stop
 
     if args.debug:
@@ -369,7 +370,7 @@ def main():
             allow_sun_up=args.allow_sun_up,
             sampsizes=None)
     except RuntimeError as e:
-        print('Failed to create the CES scan: {}'.format(e))
+        print('Failed to create the CES scan: {}'.format(e), flush=True)
         return
 
     # Create the noise model for this observation
@@ -405,21 +406,22 @@ def main():
     elapsed = stop - start
     if comm.comm_world.rank == 0:
         print('Read parameters, compute data distribution:  {:.2f} seconds'
-              ''.format(stop-start))
+              ''.format(stop-start), flush=True)
     start = stop
 
     # Simulate the atmosphere signal
 
-    atm = tt.OpSimAtmosphere(out='signal', lmin_center=args.atm_lmin_center, 
-        lmin_sigma=args.atm_lmin_sigma, lmax_center=args.atm_lmax_center, 
-        lmax_sigma=args.atm_lmax_sigma, zatm=args.atm_zatm, zmax=args.atm_zmax, 
-        xstep=args.atm_xstep, ystep=args.atm_ystep, zstep=args.atm_zstep, 
-        nelem_sim_max=args.atm_nelem_sim_max, verbosity=int(args.debug), 
-        gangsize=args.atm_gangsize, fnear=args.atm_fnear, 
-        fixed_r=args.atm_fixed_r, w_center=args.atm_w_center, 
-        w_sigma=args.atm_w_sigma, wdir_center=args.atm_wdir_center, 
-        wdir_sigma=args.atm_wdir_sigma, z0_center=args.atm_z0_center, 
-        z0_sigma=args.atm_z0_sigma, T0_center=args.atm_T0_center, 
+    atm = tt.OpSimAtmosphere(
+        out='signal', lmin_center=args.atm_lmin_center,
+        lmin_sigma=args.atm_lmin_sigma, lmax_center=args.atm_lmax_center,
+        lmax_sigma=args.atm_lmax_sigma, zatm=args.atm_zatm, zmax=args.atm_zmax,
+        xstep=args.atm_xstep, ystep=args.atm_ystep, zstep=args.atm_zstep,
+        nelem_sim_max=args.atm_nelem_sim_max, verbosity=int(args.debug),
+        gangsize=args.atm_gangsize, fnear=args.atm_fnear,
+        w_center=args.atm_w_center,
+        w_sigma=args.atm_w_sigma, wdir_center=args.atm_wdir_center,
+        wdir_sigma=args.atm_wdir_sigma, z0_center=args.atm_z0_center,
+        z0_sigma=args.atm_z0_sigma, T0_center=args.atm_T0_center,
         T0_sigma=args.atm_T0_sigma)
 
     atm.exec(data)
@@ -428,7 +430,7 @@ def main():
     stop = MPI.Wtime()
     elapsed = stop - start
     if comm.comm_world.rank == 0:
-        print('Atmosphere simulation took {:.3f} s'.format(elapsed))
+        print('Atmosphere simulation took {:.3f} s'.format(elapsed), flush=True)
     start = stop
 
     # We could also scan from a map and accumulate to 'signal' here...
@@ -445,7 +447,7 @@ def main():
     stop = MPI.Wtime()
     elapsed = stop - start
     if comm.comm_world.rank == 0:
-        print('Pointing generation took {:.3f} s'.format(elapsed))
+        print('Pointing generation took {:.3f} s'.format(elapsed), flush=True)
     start = stop
 
     # Operator for signal copying, used in each MC iteration
@@ -463,7 +465,7 @@ def main():
 
     if not args.madam:
         if comm.comm_world.rank == 0:
-            print('Not using Madam, will only make a binned map!')
+            print('Not using Madam, will only make a binned map!', flush=True)
 
         subnside = 16
         if subnside > nside:
@@ -506,7 +508,8 @@ def main():
         stop = MPI.Wtime()
         elapsed = stop - start
         if comm.comm_world.rank == 0:
-            print('Building hits and N_pp^-1 took {:.3f} s'.format(elapsed))
+            print('Building hits and N_pp^-1 took {:.3f} s'.format(elapsed),
+                  flush=True)
         start = stop
 
         hits.write_healpix_fits('{}_hits.fits'.format(args.outdir))
@@ -516,7 +519,8 @@ def main():
         stop = MPI.Wtime()
         elapsed = stop - start
         if comm.comm_world.rank == 0:
-            print('Writing hits and N_pp^-1 took {:.3f} s'.format(elapsed))
+            print('Writing hits and N_pp^-1 took {:.3f} s'.format(elapsed),
+                  flush=True)
         start = stop
 
         # invert it
@@ -526,7 +530,8 @@ def main():
         stop = MPI.Wtime()
         elapsed = stop - start
         if comm.comm_world.rank == 0:
-            print('Inverting N_pp^-1 took {:.3f} s'.format(elapsed))
+            print('Inverting N_pp^-1 took {:.3f} s'.format(elapsed),
+                  flush=True)
         start = stop
 
         invnpp.write_healpix_fits('{}_npp.fits'.format(args.outdir))
@@ -535,9 +540,11 @@ def main():
         stop = MPI.Wtime()
         elapsed = stop - start
         if comm.comm_world.rank == 0:
-            print('Writing N_pp took {:.3f} s'.format(elapsed))
+            print('Writing N_pp took {:.3f} s'.format(elapsed),
+                  flush=True)
         start = stop
 
+        """
         # in debug mode, print out data distribution information
         if args.debug:
             handle = None
@@ -554,6 +561,7 @@ def main():
                 print('Dumping debug data distribution took {:.3f} s'
                       ''.format(elapsed))
             start = stop
+        """
 
         mcstart = start
 
@@ -574,7 +582,7 @@ def main():
             elapsed = stop - start
             if comm.comm_world.rank == 0:
                 print('Creating output dir {:04d} took {:.3f} s'
-                      ''.format(mc, elapsed))
+                      ''.format(mc, elapsed), flush=True)
             start = stop
 
             # Copy the signal timestreams to the total ones before
@@ -592,7 +600,7 @@ def main():
             elapsed = stop - start
             if comm.comm_world.rank == 0:
                 print('  Noise simulation {:04d} took {:.3f} s'
-                      ''.format(mc, elapsed))
+                      ''.format(mc, elapsed), flush=True)
             start = stop
 
             zmap.data.fill(0.0)
@@ -605,7 +613,7 @@ def main():
             elapsed = stop - start
             if comm.comm_world.rank == 0:
                 print('  Building noise weighted map {:04d} took {:.3f} s'
-                      ''.format(mc, elapsed))
+                      ''.format(mc, elapsed), flush=True)
             start = stop
 
             tm.covariance_apply(invnpp, zmap)
@@ -615,7 +623,7 @@ def main():
             elapsed = stop - start
             if comm.comm_world.rank == 0:
                 print('  Computing binned map {:04d} took {:.3f} s'
-                      ''.format(mc, elapsed))
+                      ''.format(mc, elapsed), flush=True)
             start = stop
         
             zmap.write_healpix_fits(os.path.join(outpath, 'binned.fits'))
@@ -625,10 +633,11 @@ def main():
             elapsed = stop - start
             if comm.comm_world.rank == 0:
                 print('  Writing binned map {:04d} took {:.3f} s'
-                      ''.format(mc, elapsed))
+                      ''.format(mc, elapsed), flush=True)
             elapsed = stop - mcstart
             if comm.comm_world.rank == 0:
-                print('  Mapmaking {:04d} took {:.3f} s'.format(mc, elapsed))
+                print('  Mapmaking {:04d} took {:.3f} s'.format(mc, elapsed),
+                      flush=True)
             start = stop
 
     else:
@@ -695,7 +704,8 @@ def main():
             stop = MPI.Wtime()
             elapsed = stop - start
             if comm.comm_world.rank == 0:
-                print('Noise simulation took {:.3f} s'.format(elapsed))
+                print('Noise simulation took {:.3f} s'.format(elapsed),
+                      flush=True)
             start = stop
 
             # create output directory for this realization
@@ -704,6 +714,7 @@ def main():
                 if not os.path.isdir(pars['path_output']):
                     os.makedirs(pars['path_output'])
 
+            """
             # in debug mode, print out data distribution information
             if args.debug:
                 handle = None
@@ -713,6 +724,7 @@ def main():
                 data.info(handle)
                 if comm.comm_world.rank == 0:
                     handle.close()
+            """
 
             madam = tm.OpMadam(params=pars, detweights=detweights, name='total',
                                common_flag_mask=args.common_flag_mask)
@@ -722,13 +734,13 @@ def main():
             stop = MPI.Wtime()
             elapsed = stop - start
             if comm.comm_world.rank == 0:
-                print('Mapmaking took {:.3f} s'.format(elapsed))
+                print('Mapmaking took {:.3f} s'.format(elapsed), flush=True)
 
     comm.comm_world.barrier()
     stop = MPI.Wtime()
     elapsed = stop - global_start
     if comm.comm_world.rank == 0:
-        print('Total Time:  {:.2f} seconds'.format(elapsed))
+        print('Total Time:  {:.2f} seconds'.format(elapsed), flush=True)
 
 
 if __name__ == '__main__':

src/python/tod/sim_det_atm.py
@@ -41,6 +41,9 @@ class OpSimAtmosphere(Operator):
     Args:
         out (str): accumulate data to the cache with name <out>_<detector>.
             If the named cache objects do not exist, then they are created.
+        realization (int): if simulating multiple realizations, the realization
+            index.
+        component (int): the component index to use for this noise simulation.
         lmin_center (float): Kolmogorov turbulence dissipation scale center.
         lmin_sigma (float): Kolmogorov turbulence dissipation scale sigma.
         lmax_center (float): Kolmogorov turbulence injection scale center.
@@ -54,7 +57,6 @@ class OpSimAtmosphere(Operator):
         verbosity (int): more information is printed for values > 0.
         gangsize (int): size of the gangs that create slices.
         fnear (float): multiplier for the near field simulation.
-        fixed_r (float): positive number for start of integration.
         w_center (float):  central value of the wind speed distribution.
         w_sigma (float):  sigma of the wind speed distribution.
         wdir_center (float):  central value of the wind direction distribution.
@@ -65,16 +67,21 @@ class OpSimAtmosphere(Operator):
         T0_sigma (float):  sigma of the temperature distribution.
 
     """
-    def __init__(self, out='atm', lmin_center=0.01, lmin_sigma=0.001,
-        lmax_center=10, lmax_sigma=10, zatm=40000.0, zmax=2000.0, xstep=100.0,
-        ystep=100.0, zstep=100.0, nelem_sim_max=10000, verbosity=0, gangsize=-1,
-        fnear=0.1, fixed_r=-1, w_center=10, w_sigma=1, wdir_center=0, wdir_sigma=100,
-        z0_center=2000, z0_sigma=0, T0_center=280, T0_sigma=10):
+    def __init__(
+            self, out='atm', realization=0, component=123456,
+            lmin_center=0.01, lmin_sigma=0.001,
+            lmax_center=10, lmax_sigma=10, zatm=40000.0, zmax=2000.0,
+            xstep=100.0, ystep=100.0, zstep=100.0, nelem_sim_max=10000,
+            verbosity=0, gangsize=-1,
+            fnear=0.1, w_center=10, w_sigma=1, wdir_center=0, wdir_sigma=100,
+            z0_center=2000, z0_sigma=0, T0_center=280, T0_sigma=10):
 
         # We call the parent class constructor, which currently does nothing
         super().__init__()
 
         self._out = out
+        self._realization = realization
+        self._component = component
         self._lmin_center = lmin_center
         self._lmin_sigma = lmin_sigma
         self._lmax_center = lmax_center
@@ -88,7 +95,6 @@ class OpSimAtmosphere(Operator):
         self._verbosity = verbosity
         self._gangsize = gangsize
         self._fnear = fnear
-        self._fixed_r = fixed_r
 
         # FIXME: eventually these will come from the TOD object
         # for each obs...
@@ -114,6 +120,31 @@ class OpSimAtmosphere(Operator):
         """
 
         for obs in data.obs:
+            obsindx = 0
+            if 'id' in obs:
+                obsindx = obs['id']
+            else:
+                print("Warning: observation ID is not set, using zero!")
+
+            telescope = 0
+            if 'telescope' in obs:
+                telescope = obs['telescope']
+
+            """
+            The random number generator accepts a key and a counter,
+            each made of two 64bit integers.
+            Following tod_math.py we set
+            key1 = realization * 2^32 + telescope * 2^16 + component
+            key2 = obsindx * 2^32
+            counter1 = currently unused (0)
+            counter2 = sample in stream (incremented internally in the atm code)
+            """
+            key1 = self._realization * 2**32 + telescope * 2**16 \
+                   + self._component
+            key2 = obsindx
+            counter1 = 0
+            counter2 = 0
+
             tod = obs['tod']
             comm = tod.mpicomm
 
@@ -156,7 +187,7 @@ class OpSimAtmosphere(Operator):
                               + (detdir[t][1] - detmeany)**2)
                       for t in range(len(detdir))]
 
-            fp_radius = np.arcsin(np.max(detrad)) + 1 * arcmin
+            fp_radius = np.arcsin(np.max(detrad)) + 30 * arcmin
 
             #print("fp_radius = {}", fp_radius)
 
@@ -165,22 +196,73 @@ class OpSimAtmosphere(Operator):
             elmin = min_el_bore - fp_radius
             elmax = max_el_bore + fp_radius
 
+            azmin = comm.allreduce(azmin, op=MPI.MIN)
+            azmax = comm.allreduce(azmax, op=MPI.MAX)
+            elmin = comm.allreduce(elmin, op=MPI.MIN)
+            elmax = comm.allreduce(elmax, op=MPI.MAX)
+
             #print("patch = {}, {}, {}, {}".format(azmin, azmax, elmin, elmax))
 
             # Get the timestamps
 
             times = tod.read_times()
 
-            sim = atm_sim_alloc(azmin, azmax, elmin, elmax, times[0],
-                times[-1], self._lmin_center, self._lmin_sigma,
+            tmin = times[0]
+            tmax = times[-1]
+            tmin = comm.allreduce(tmin, op=MPI.MIN)
+            tmax = comm.allreduce(tmax, op=MPI.MAX)
+
+            sim = atm_sim_alloc(
+                azmin, azmax, elmin, elmax, tmin, tmax,
+                self._lmin_center, self._lmin_sigma,
                 self._lmax_center, self._lmax_sigma, self._w_center,
                 self._w_sigma, self._wdir_center, self._wdir_sigma,
                 self._z0_center, self._z0_sigma, self._T0_center,
                 self._T0_sigma, self._zatm, self._zmax, self._xstep,
                 self._ystep, self._zstep, self._nelem_sim_max,
-                self._verbosity, comm, self._gangsize, self._fnear)
+                self._verbosity, comm, self._gangsize, self._fnear,
+                key1, key2, counter1, counter2)
 
             atm_sim_simulate(sim, 0)
+
+            if self._verbosity > 0:
+                # Create snapshots of the atmosphere
+                import matplotlib.pyplot as plt
+                import sys
+                azelstep = .01 * degree
+                azgrid = np.linspace( azmin, azmax, (azmax-azmin)//azelstep+1 )
+                elgrid = np.linspace( elmin, elmax, (elmax-elmin)//azelstep+1 )
+                AZ, EL = np.meshgrid( azgrid, elgrid )
+                nn = AZ.size
+                az = AZ.ravel()
+                el = EL.ravel()
+                atmdata = np.zeros(nn, dtype=np.float64)
+                atmtimes = np.zeros(nn, dtype=np.float64)
+
+                rank = comm.rank
+                ntask = comm.size
+                r = 0
+                t = 0
+                #for i, r in enumerate(np.linspace(1, 5000, 100)):
+                for i, t in enumerate(np.linspace(tmin, tmax, 100)):
+                    if i % ntask != rank:
+                        continue
+                    atm_sim_observe(sim, atmtimes+t, az, el, atmdata, nn, r)
+                    atmdata2d = atmdata.reshape(AZ.shape)
+                    plt.figure()
+                    plt.imshow(atmdata2d, interpolation='nearest',
+                               origin='lower', extent=np.array(
+                                   [azmin, azmax, elmin, elmax])/degree,
+                               cmap=plt.get_cmap('Blues'))
+                    plt.colorbar()
+                    ax = plt.gca()
+                    ax.set_title('t = {:15.1f} s, r = {:15.1f} m'.format(t, r))
+                    ax.set_xlabel('az [deg]')
+                    ax.set_ylabel('el [deg]')
+                    ax.set_yticks([elmin/degree, elmax/degree])
+                    plt.savefig('atm_t_{:04}_r_{:04}.png'.format(
+                        int(t), int(r)))
+                    plt.close()
 
             nsamp = tod.local_samples[1]
 
@@ -190,8 +272,8 @@ class OpSimAtmosphere(Operator):
 
                 atmdata = np.zeros(nsamp, dtype=np.float64)
 
-                # Convert Az/El quaternion of the detector back into angles for
-                # the simulation.
+                # Convert Az/El quaternion of the detector back into
+                # angles for the simulation.
 
                 theta, phi, pa = qa.to_angles(azelquat)
                 az = phi
@@ -213,7 +295,7 @@ class OpSimAtmosphere(Operator):
                 # Integrate detector signal
 
                 atm_sim_observe(
-                    sim, times, az, el, atmdata, nsamp, self._fixed_r)
+                    sim, times, az, el, atmdata, nsamp, 0)
 
                 # write to cache
 

src/python/tod/tod_math.py
@@ -343,7 +343,7 @@ class OpCacheCopy(Operator):
                         outref = tod.cache.create(outname, np.float64,
                                     (tod.local_samples[1],))
 
-                    outref[local_offset:local_offset+chunk_samp] = inref[local_offset:local_offset+chunk_samp]
+                    outref[:] = inref[:]
 
                     del outref
                     del inref