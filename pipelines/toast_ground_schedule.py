320017f594f4a55c3ec343c54da8c8c936b6a85b
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2017-05-30 15:41:05 -0700

    Add Moon avoidance to the scheduler

--- pipelines/toast_ground_schedule.py
+++ pipelines/toast_ground_schedule.py
@@ -72,8 +72,12 @@ def main():
                         help='Solar elevation above which to apply '
                         'sun_angle_min [deg]')
     parser.add_argument('--sun_angle_min',
-                        required=False, default=90.0, type=np.float,
-                        help='Minimum azimuthal distance between the Sun and '
+                        required=False, default=30.0, type=np.float,
+                        help='Minimum distance between the Sun and '
+                        'the bore sight [deg]')
+    parser.add_argument('--moon_angle_min',
+                        required=False, default=10.0, type=np.float,
+                        help='Minimum distance between the Moon and '
                         'the bore sight [deg]')
     parser.add_argument('--sun_el_max',
                         required=False, default=90.0, type=np.float,
@@ -177,7 +181,8 @@ def main():
     t = start_timestamp
     gap = args.gap
     sun = ephem.Sun()
-    tstep = 60
+    moon = ephem.Moon()
+    tstep = 600
     fout = open('schedule.txt', 'w')
 
     fout.write('#{:15} {:15} {:15} {:15}\n'.format(
@@ -185,18 +190,25 @@ def main():
     fout.write(' {:15} {:15} {:15} {:15.6f}\n'.format(
         args.site_name, args.site_lat, args.site_lon, args.site_alt))
 
-    fout_fmt0 = '#{:20} {:20} {:14} {:14} {:15} ' \
-                '{:8} {:8} {:8} {:5} {:8} {:8} {:8} {:8} {:5}\n'
+    fout_fmt0 = '#{:20} {:20} {:14} {:14} ' \
+                '{:15} {:8} {:8} {:8} {:5} ' \
+                '{:8} {:8} {:8} {:8} ' \
+                '{:8} {:8} {:8} {:8} {:5} ' \
+                '{:5}\n'
                 
-    fout_fmt = ' {:20} {:20} {:14.6f} {:14.6f} {:15} ' \
-               '{:8.2f} {:8.2f} {:8.2f} {:5} ' \
-               '{:8.2f} {:8.2f} {:8.2f} {:8.2f} {:5}\n'
+    fout_fmt = ' {:20} {:20} {:14.6f} {:14.6f} ' \
+               '{:15} {:8.2f} {:8.2f} {:8.2f} {:5} ' \
+               '{:8.2f} {:8.2f} {:8.2f} {:8.2f} ' \
+               '{:8.2f} {:8.2f} {:8.2f} {:8.2f} {:5.2f} ' \
+               '{:5}\n'
 
     fout.write(
         fout_fmt0.format(
             'Start time UTC', 'Stop time UTC', 'Start MJD', 'Stop MJD',
-            'Patch name', 'Az min', 'Az max', 'El',
-            'R/S', 'Sun el 1', 'Sun az 1', 'Sun el 2', 'Sun az 2', 'Pass'))
+            'Patch name', 'Az min', 'Az max', 'El', 'R/S',
+            'Sun el1', 'Sun az1', 'Sun el2', 'Sun az2',
+            'Moon el1', 'Moon az1', 'Moon el2', 'Moon az2', 'Phase',
+            'Pass'))
 
     while t < stop_timestamp:
         # Determine which patches are visible
@@ -207,7 +219,12 @@ def main():
         if sun_el > args.sun_el_max:
             t += tstep
             continue
+        moon.compute(observer)
+        moon_el = moon.alt / degree
+        moon_az = moon.az / degree
+        moon_phase = moon.moon_phase
         visible = []
+        not_visible = []
         for (name, weight, corners) in patches:
             # Reject all patches that have even one corner too close
             # to the sun, all setting patches that are not completely
@@ -226,38 +243,54 @@ def main():
                 corner.compute(observer)
                 if not rising and corner.alt / degree < args.el_min:
                     # At least one corner is too low
+                    not_visible.append((
+                        name, 'Too low {:.2f}'.format(corner.alt / degree)))
                     in_view = False
                     break
                 if rising and corner.alt / degree > args.el_max:
                     # At least one corner is too high
+                    not_visible.append((
+                        name, 'Too high {:.2f}'.format(corner.alt / degree)))
                     in_view = False
                     break
                 els[i] = corner.alt
                 if sun.alt > args.sun_avoidance_angle * degree:
                     # Sun is high enough to apply sun_angle_min check
-                    angle = corner.az - sun.az
-                    if angle < -2*np.pi:
-                        angle += 2*np.pi
-                    if angle > 2*np.pi:
-                        angle -= 2*np.pi
-                    angle = np.abs(angle) / degree
+                    angle = ephem.separation(sun, corner) / degree
                     if angle < args.sun_angle_min:
-                        # Patch is too close to the sun
+                        # Patch is too close to the Sun
+                        not_visible.append((
+                            name, 'Too close to Sun {:.2f}'.format(angle)))
                         in_view = False
                         break
+                angle = ephem.separation(moon, corner) / degree
+                if angle < args.moon_angle_min:
+                    # Patch is too close to the Moon
+                    not_visible.append((
+                        name, 'Too close to Moon {:.2f}'.format(angle)))
+                    in_view = False
+                    break
             if rising and in_view:
                 elmax = np.amax(els) / degree
                 if elmax < args.el_min:
                     # All corners are too low
+                    not_visible.append((
+                        name, 'Too low {:.2f}'.format(corner.alt / degree)))
                     in_view = False
             if not rising and in_view:
                 elmin = np.amin(els) / degree
                 if elmin > args.el_max:
                     # All corners are too high
+                    not_visible.append((
+                        name, 'Too high {:.2f}'.format(corner.alt / degree)))
                     in_view = False
             if in_view:
                 visible.append((name, weight, corners))
         if len(visible) == 0:
+            if args.debug:
+                tstring = datetime.utcfromtimestamp(t).strftime(
+                    '%Y-%m-%d %H:%M:%S %Z')
+                print('No patches visible at {}: {}'.format(tstring, not_visible))
             t += tstep
             continue
 
@@ -290,6 +323,7 @@ def main():
             if ambiguous:
                 # This patch is rotating.  It is unlikely all corners
                 # would make across a CES line.
+                not_visible.append((name, 'Rotating'))
                 continue
             observer.date = to_DJD(t)
             for corner in corners:
@@ -314,34 +348,41 @@ def main():
             old_el = els.copy()
             old_to_cross = to_cross.copy()
             while True:
-                tstop += tstep
+                tstop += tstep / 10
                 if tstop > stop_timestamp:
+                    not_visible.append((name, 'Ran out of time'))
                     break
                 observer.date = to_DJD(tstop)
                 sun.compute(observer)
                 if sun.alt / degree > args.sun_el_max:
+                    not_visible.append((
+                        name, 'Sun too high {:.2f}'.format(sun.alt / degree)))
                     break
+                moon.compute(observer)
+                sun_too_close = False
+                moon_too_close = False
                 for i, corner in enumerate(corners):
                     corner.compute(observer)
                     azs[i] = corner.az
                     els[i] = corner.alt
-                if np.any(azs < 0):
-                    raise RuntimeError('Negative az')
-                #azmin = min(azmin, np.amin(azs))
-                #azmax = max(azmax, np.amax(azs))
-                # Check if the sun has moved too close
-                sun_too_close = False
-                if sun.alt > args.sun_avoidance_angle * degree:
-                    for az in [azmin, azmax]:
-                        angle = az - sun.az
-                        if angle < -2*np.pi:
-                            angle += 2*np.pi
-                        if angle > 2*np.pi:
-                            angle -= 2*np.pi
-                        if np.abs(angle) < args.sun_angle_min * degree:
+                    if sun.alt > args.sun_avoidance_angle * degree:
+                        # Check if the sun has moved too close
+                        angle = ephem.separation(sun, corner) / degree
+                        if angle < args.sun_angle_min:
+                            # Patch is too close to the Sun
+                            not_visible.append((
+                                name, 'Too close to Sun {:.2f}'.format(angle)))
                             sun_too_close = True
                             break
-                if sun_too_close:
+                    # Check if the sun has moved too close
+                    angle = ephem.separation(moon, corner) / degree
+                    if angle < args.moon_angle_min:
+                        # Patch is too close to the Moon
+                        not_visible.append((
+                            name, 'Too close to Moon {:.2f}'.format(angle)))
+                        moon_too_close = True
+                        break
+                if sun_too_close or moon_too_close:
                     break
 
                 # The patch may change direction without all corners
@@ -351,6 +392,7 @@ def main():
                 if ambiguous or rising != rising_now:
                     # The patch changed direction without crossing the
                     # CES line.  Move on to the next target
+                    not_visible.append((name, 'Changed direction'))
                     break
                 if rising:
                     to_cross[els > el + args.fp_radius * degree] = False
@@ -372,7 +414,7 @@ def main():
                 old_el = els.copy()
                 old_to_cross = to_cross.copy()
             if not success:
-                # CES failed due to the sun or patch changing direction.
+                # CES failed due to the Sun, Moon or patch changing direction.
                 # Try the next patch instead.
                 continue
             # Check if we are scanning across the zero meridian
@@ -396,7 +438,8 @@ def main():
                     azmin/degree, azmax/degree, el/degree,
                     rising_string,
                     sun_el, sun_az, sun.alt/degree, sun.az/degree,
-                    hits[name]))
+                    moon_el, moon_az, moon.alt/degree, moon.az/degree,
+                    moon_phase, hits[name]))
             # Advance the time
             t = tstop
             # Add the gap
@@ -404,6 +447,10 @@ def main():
             break
 
         if not success:
+            if args.debug:
+                tstring = datetime.utcfromtimestamp(t).strftime(
+                    '%Y-%m-%d %H:%M:%S %Z')
+                print('No patches visible at {}: {}'.format(tstring, not_visible))
             t += tstep
 
     fout.close()
