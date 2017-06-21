Fix issues in the in-cone calculation

src/python/tod/sim_det_atm.py
@@ -390,7 +390,8 @@ class OpSimAtmosphere(Operator):
                     my_snapshots = []
                     vmin = 1e30
                     vmax = -1e30
-                    for i, t in enumerate(np.arange(tmin, tmax, 1)):
+                    tstep = 1
+                    for i, t in enumerate(np.arange(tmin, tmax, tstep)):
                         if i % ntask != rank:
                             continue
                         atm_sim_observe(sim, atmtimes+t, az, el, atmdata, nn, r)