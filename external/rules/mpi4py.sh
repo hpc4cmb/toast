6c7a410f650cfa405ba9077fc8078137ac1ca5fb
Kolen Cheung <christian.kolen@gmail.com>
2017-08-09 16:52:13 -0700

    mpi4py.sh: use POSIX grammar rule to make it cleaner
    http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_10_02

--- external/rules/mpi4py.sh
+++ external/rules/mpi4py.sh
@@ -1,7 +1,7 @@
 curl -SL https://pypi.python.org/packages/ee/b8/f443e1de0b6495479fc73c5863b7b5272a4ece5122e3589db6cd3bb57eeb/mpi4py-2.0.0.tar.gz#md5=4f7d8126d7367c239fd67615680990e3 \
-    | tar xzf - \
-    && cd mpi4py-2.0.0 \
-    && cat << EOF > mpi.cfg &&
+    | tar xzf - &&
+    cd mpi4py-2.0.0 &&
+    cat << EOF > mpi.cfg &&
 [toast]
 mpicc = @MPICC@
 mpicxx = @MPICXX@
@@ -12,7 +12,7 @@ libraries = @MPI_LIB@
 extra_compile_args = @MPI_EXTRA_COMP@
 extra_link_args = @MPI_EXTRA_LINK@
 EOF
-    python setup.py build --mpi=toast \
-    && python setup.py install --prefix=@CONDA_PREFIX@ \
-    && cd .. \
-    && rm -rf mpi4py*
+    python setup.py build --mpi=toast &&
+    python setup.py install --prefix=@CONDA_PREFIX@ &&
+    cd .. &&
+    rm -rf mpi4py*
