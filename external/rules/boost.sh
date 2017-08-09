00ccb47831a6995cf0cbae264c6b5b09bb25456b
Theodore Kisner <tskisner.public@gmail.com>
2017-08-09 09:15:39 -0700

    Add boost to install script to support legacy toast as well.

---
+++ external/rules/boost.sh
@@ -0,0 +1,16 @@
+curl -SL https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.bz2/download \
+    -o boost_1_63_0.tar.bz2 \
+    && tar xjf boost_1_63_0.tar.bz2 \
+    && cd boost_1_63_0 \
+    && echo "" > tools/build/user-config.jam \
+    && echo 'using @BOOSTCHAIN@ : : @CXX@ : <cflags>"@CFLAGS@" <cxxflags>"@CXXFLAGS@" ;' >> tools/build/user-config.jam \
+    && echo 'using mpi : @MPICXX@ : <include>"@MPI_CPPFLAGS@" <library-path>"@MPI_LDFLAGS@" <find-shared-library>"@MPI_CXXLIB@" <find-shared-library>"@MPI_LIB@" ;' >> tools/build/user-config.jam \
+    && BOOST_BUILD_USER_CONFIG=tools/build/user-config.jam ./bootstrap.sh \
+    --with-toolset=@BOOSTCHAIN@ \
+    --with-python=python@PYVERSION@ \
+    --prefix=@AUX_PREFIX@ \
+    && ./b2 --layout=tagged \
+    $(python@PYVERSION@-config --includes | sed -e 's/-I//g' -e 's/\([^[:space:]]\+\)/ include=\1/g') \
+    variant=release threading=multi link=shared runtime-link=shared install \
+    && cd .. \
+    && rm -rf boost*
