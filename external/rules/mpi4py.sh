f70cd458f7717e72f3bbc8275215a670e4e28fbd
Kolen Cheung <christian.kolen@gmail.com>
2017-08-09 16:46:13 -0700

    mpi4py.sh: pipe through w/o saving the tar.gz

--- external/rules/mpi4py.sh
+++ external/rules/mpi4py.sh
@@ -1,6 +1,5 @@
 curl -SL https://pypi.python.org/packages/ee/b8/f443e1de0b6495479fc73c5863b7b5272a4ece5122e3589db6cd3bb57eeb/mpi4py-2.0.0.tar.gz#md5=4f7d8126d7367c239fd67615680990e3 \
-    -o mpi4py-2.0.0.tar.gz \
-    && tar xzf mpi4py-2.0.0.tar.gz \
+    | tar xzf - \
     && cd mpi4py-2.0.0 \
     && echo $'\n\
 [toast]\n\
