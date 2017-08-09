c6dd0d43d39c55722baa60e52e6bfbb055c8b7b2
Kolen Cheung <christian.kolen@gmail.com>
2017-08-09 16:47:52 -0700

    mpi4py.sh: improve the content to mpi.cfg
    Before this patch, the `mpi.cfg` would have a lot of `\` lines in between. Moreover, using the heredoc makes it clearer to see what's happening.

--- external/rules/mpi4py.sh
+++ external/rules/mpi4py.sh
@@ -1,18 +1,18 @@
 curl -SL https://pypi.python.org/packages/ee/b8/f443e1de0b6495479fc73c5863b7b5272a4ece5122e3589db6cd3bb57eeb/mpi4py-2.0.0.tar.gz#md5=4f7d8126d7367c239fd67615680990e3 \
     | tar xzf - \
     && cd mpi4py-2.0.0 \
-    && echo $'\n\
-[toast]\n\
-mpicc = @MPICC@\n\
-mpicxx = @MPICXX@\n\
-include_dirs = @MPI_CPPFLAGS@\n\
-library_dirs = @MPI_LDFLAGS@\n\
-runtime_library_dirs = @MPI_LDFLAGS@\n\
-libraries = @MPI_LIB@\n\
-extra_compile_args = @MPI_EXTRA_COMP@\n\
-extra_link_args = @MPI_EXTRA_LINK@\n\
-' > mpi.cfg \
-    && python setup.py build --mpi=toast \
+    && cat << EOF > mpi.cfg &&
+[toast]
+mpicc = @MPICC@
+mpicxx = @MPICXX@
+include_dirs = @MPI_CPPFLAGS@
+library_dirs = @MPI_LDFLAGS@
+runtime_library_dirs = @MPI_LDFLAGS@
+libraries = @MPI_LIB@
+extra_compile_args = @MPI_EXTRA_COMP@
+extra_link_args = @MPI_EXTRA_LINK@
+EOF
+    python setup.py build --mpi=toast \
     && python setup.py install --prefix=@CONDA_PREFIX@ \
     && cd .. \
     && rm -rf mpi4py*
