00c5e9afcdac457a3d8b7210456e4684ccae102d
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2017-09-12 23:25:32 -0400

    Address a corner case where the gain is exactly at the time stamp.

--- src/python/tod/tod_math.py
+++ src/python/tod/tod_math.py
@@ -17,19 +17,20 @@ from .. import fft as fft
 from ..op import Operator
 
 
-def calibrate( toitimes, toi, gaintimes, gains, order=0, inplace=False ):
+def calibrate(toitimes, toi, gaintimes, gains, order=0, inplace=False):
     """
     Interpolate the gains to TOI samples and apply them.
 
     Args:
-        toitimes (float):  Increasing TOI sample times in same units as gaintimes
-        toi (float):  TOI samples to calibrate
-        gaintimes (float):  Increasing timestamps of the gain values in same units
-                            as toitimes
-        gains (float):  Multiplicative gains
-        order (int):  Gain interpolation order. 0 means steps at the gain times,
-                      all other are polynomial interpolations.
-        inplace (bool):  Overwrite input TOI.
+        toitimes (float): Increasing TOI sample times in same units as
+            gaintimes
+        toi (float): TOI samples to calibrate
+        gaintimes (float): Increasing timestamps of the gain values in
+            same units as toitimes
+        gains (float): Multiplicative gains
+        order (int): Gain interpolation order. 0 means steps at the gain
+            times, all other are polynomial interpolations.
+        inplace (bool): Overwrite input TOI.
 
     Returns:
         calibrated timestream.
@@ -39,14 +40,13 @@ def calibrate( toitimes, toi, gaintimes, gains, order=0, inplace=False ):
         g = gains
     else:
         if order == 0:
-            ind = np.searchsorted( gaintimes, toitimes )
-            ind[ind > 0] -= 1
+            ind = np.searchsorted(gaintimes, toitimes, side='right') - 1
             g = gains[ind]
         else:
             if len(gaintimes) <= order:
                 order = len(gaintimes) - 1
-            p = np.polyfit( gaintimes, gains, order )
-            g = np.polyval( p, toitimes )
+            p = np.polyfit(gaintimes, gains, order)
+            g = np.polyval(p, toitimes)
 
     if inplace:
         toi_out = toi
