836af1b8a2e3f55c64e04bd1211cbcc4d130375a
Theodore Kisner <tskisner.public@gmail.com>
2017-09-15 09:52:36 -0700

    Add cfitsio tools to external install.

--- external/rules/cfitsio.sh
+++ external/rules/cfitsio.sh
@@ -1,7 +1,11 @@
 curl -SL http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3410.tar.gz \
     | tar xzf - \
     && cd cfitsio \
-    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ --prefix="@AUX_PREFIX@" \
-    && make -j 4 && make shared && make install \
+    && CC="@CC@" CFLAGS="@CFLAGS@" ./configure @CROSS@ \
+    --prefix="@AUX_PREFIX@" --enable-reentrant \
+    && make stand_alone \
+    && make utils \
+    && make shared \
+    && make install \
     && cd .. \
     && rm -rf cfitsio*
