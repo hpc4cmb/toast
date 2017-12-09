3ccebdca27d7f2f9f951337081c1761bfc3704a9
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2017-12-08 19:49:14 -0800

    Add aatm as an external package

---
+++ external/rules/aatm.sh
@@ -0,0 +1,14 @@
+curl -SL https://launchpad.net/aatm/trunk/0.5/+download/aatm-0.5.tar.gz \
+    | tar xvf - \
+    && cd aatm-0.5 \
+    && chmod -R u+w . \
+    && patch -p1 < ../rules/patch_aatm \
+    && autoreconf \
+    && CC="@CC@" CFLAGS="@CFLAGS@" \
+    CPPFLAGS="-I@AUX_PREFIX@/include" \
+    LDFLAGS="-L@AUX_PREFIX@/lib" \
+    ./configure @CROSS@ \
+    --prefix="@AUX_PREFIX@" \
+    && make -j 4 && make install \
+    && cd .. \
+    && rm -rf aatm*
