15f1b24cf7024794e258e5f6af37af3e6695afde
Andrea Zonca <code@andreazonca.com>
2018-03-20 13:11:06 -0700

    write log to stdout

--- examples/run_tiny_tests.sh
+++ examples/run_tiny_tests.sh
@@ -1,6 +1,7 @@
 bash generate_shell.sh
 sed -i "s/512/64/g" tiny*
 sed -i "s/mpirun -n 1//g" tiny*
+sed -i 's/eval \${run} \${com}.*$/eval \${run} \${com}/' tiny*
 wget http://portal.nersc.gov/project/cmb/toast_data/ref_out_tiny_satellite.tgz
 tar xzf ref_out_tiny_satellite.tgz
 bash tiny_satellite_shell.sh && python check_maps.py
