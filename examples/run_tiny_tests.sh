4a3aa263ade67c99659ae4b4c19e5f378507615b
Andrea Zonca <code@andreazonca.com>
2018-03-20 12:32:58 -0700

    run focalplane without mpi

--- examples/run_tiny_tests.sh
+++ examples/run_tiny_tests.sh
@@ -1,6 +1,6 @@
 bash generate_shell.sh
 sed -i "s/512/64/g" tiny*
+sed -i "s/mpirun -n 1//g" tiny*
 wget http://portal.nersc.gov/project/cmb/toast_data/ref_out_tiny_satellite.tgz
 tar xzf ref_out_tiny_satellite.tgz
-bash tiny_satellite_shell.sh
-python check_maps.py
+bash tiny_satellite_shell.sh && python check_maps.py
