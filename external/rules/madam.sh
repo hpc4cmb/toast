3c28f2c6bcd2d9a0639531e604a133102d425d5d
Theodore Kisner <tskisner.public@gmail.com>
2018-03-22 07:21:31 -0700

    Forgot to commit this update to madam build rule.

--- external/rules/madam.sh
+++ external/rules/madam.sh
@@ -1,6 +1,6 @@
-curl -SL https://github.com/hpc4cmb/libmadam/releases/download/0.2.7/libmadam-0.2.7.tar.bz2 \
+curl -SL https://github.com/hpc4cmb/libmadam/releases/download/0.2.9/libmadam-0.2.9.tar.bz2 \
     | tar -xjf - \
-    && cd libmadam-0.2.7 \
+    && cd libmadam-0.2.9 \
     && FC="@MPIFC@" MPIFC="@MPIFC@" FCFLAGS="@FCFLAGS@" \
     CC="@MPICC@" MPICC="@MPICC@" CFLAGS="@CFLAGS@" \
     ./configure @CROSS@ --with-cfitsio="@AUX_PREFIX@" \
