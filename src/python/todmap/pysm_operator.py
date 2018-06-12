1dae0a0e09e6a56c7c55a8502ae442c45311b10a
Andrea Zonca <code@andreazonca.com>
2018-06-12 11:24:57 -0700

    better error message if healpy is not 1.12

--- src/python/todmap/pysm_operator.py
+++ src/python/todmap/pysm_operator.py
@@ -191,7 +191,12 @@ class OpSimPySM(Operator):
                 # PySM is always in Galactic, make rotation to Ecliptic or Equatorial
                 rot = hp.Rotator(coord = ["G", self.coord])
                 # this requires healpy 1.12
-                full_map_rank0 = rot.rotate_map(full_map_rank0)
+                try:
+                    full_map_rank0 = rot.rotate_map(full_map_rank0)
+                except AttributeError:
+                    print('PySM coordinate conversion from G to another reference frame requires'
+                          'healpy.Rotator.rotate_map available since healpy 1.12')
+                    raise
             if self.comm.rank == 0 and self._nest:
                 # PySM is RING, toast is NEST
                 full_map_rank0 = hp.reorder(full_map_rank0, r2n=True)
