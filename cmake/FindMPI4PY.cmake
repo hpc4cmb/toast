68240305c1259e7e868bc944421ec215ffb0aff4
Theodore Kisner <tskisner.public@gmail.com>
2018-11-30 12:56:19 -0800

    Remove some autotools files, move old cmake things out of the way.

---
+++ cmake/FindMPI4PY.cmake
@@ -0,0 +1,29 @@
+# - FindMPI4PY
+# Find mpi4py includes and library
+# This module defines:
+# MPI4PY_INCLUDE_DIR, where to find mpi4py.h, etc.
+# MPI4PY_FOUND
+
+# Roughly based on a file in a branch of the Dolfin project.
+
+if(NOT MPI4PY_INCLUDE_DIR)
+    execute_process(
+        COMMAND "${PYTHON_EXECUTABLE}" "${CMAKE_SOURCE_DIR}/cmake/test_mpi4py.py"
+        OUTPUT_VARIABLE MPI4PY_INCLUDE_DIR
+        RESULT_VARIABLE MPI4PY_COMMAND_RESULT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    message(${MPI4PY_INCLUDE_DIR})
+    if(MPI4PY_COMMAND_RESULT STREQUAL "0")
+        message("mpi4py found")
+        set(MPI4PY_FOUND TRUE)
+        set(MPI4PY_INCLUDE_DIR ${MPI4PY_INCLUDE_DIR}
+            CACHE STRING "mpi4py include path")
+    else(MPI4PY_COMMAND_RESULT STREQUAL "0")
+        message("mpi4py not found")
+        set(MPI4PY_FOUND FALSE)
+    endif(MPI4PY_COMMAND_RESULT STREQUAL "0")
+else(NOT MPI4PY_INCLUDE_DIR)
+    message("using manually specified mpi4py includes")
+    set(MPI4PY_FOUND TRUE)
+endif(NOT MPI4PY_INCLUDE_DIR)
