496037dab3cff59b664e30bc2af96c88742f5aa3
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-07-15 21:52:37 -0700

    Import the atmospheric utility methods

--- pipelines/toast_ground_sim.py
+++ pipelines/toast_ground_sim.py
@@ -60,6 +60,11 @@ from toast.tod import (
     OpGroundFilter,
     OpSimAtmosphere,
 )
+from toast.tod.atm import (
+    atm_atmospheric_loading,
+    atm_absorption_coefficient,
+    atm_absorption_coefficient_vec,
+)
 
 # FIXME: put these back into the import statement above after porting.
 tidas_available = False
@@ -1342,7 +1347,7 @@ def scale_atmosphere_by_frequency(args, comm, data, freq, totalname_freq, mc):
         if my_nfreq > 0:
             my_freqs = freqmin + np.arange(my_ifreq_min, my_ifreq_max) * freqstep
             my_absorption = np.zeros(my_nfreq)
-            err = atm_get_absorption_coefficient_vec(
+            err = atm_absorption_coefficient_vec(
                 altitude,
                 air_temperature,
                 surface_pressure,
@@ -1359,8 +1364,7 @@ def scale_atmosphere_by_frequency(args, comm, data, freq, totalname_freq, mc):
             my_absorption = np.array([])
         freqs = np.hstack(todcomm.allgather(my_freqs))
         absorption = np.hstack(todcomm.allgather(my_absorption))
-        # loading = toast.ctoast.atm_get_atmospheric_loading(
-        #    altitude, pwv, freq)
+        # loading = atm_atmospheric_loading(altitude, pwv, freq)
         for det in tod.local_dets:
             try:
                 # Use detector bandpass from the focalplane
@@ -1409,7 +1413,7 @@ def update_atmospheric_noise_weights(args, comm, data, freq, mc):
             start_time = obs["start_time"]
             weather.set(site_id, mc, start_time)
             altitude = obs["altitude"]
-            absorption = atm_get_absorption_coefficient(
+            absorption = atm_absorption_coefficient(
                 altitude,
                 weather.air_temperature,
                 weather.surface_pressure,
