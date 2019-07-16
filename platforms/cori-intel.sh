615e0b8d2377b21d93bfd07a7701e4e69f98bdba
Theodore Kisner <tskisner.public@gmail.com>
2019-07-16 11:48:21 -0700

    Add OpenMP compile / link to python extension.  Update cori Intel config.

--- platforms/cori-intel.sh
+++ platforms/cori-intel.sh
@@ -10,9 +10,13 @@ cmake \
     -DCMAKE_C_FLAGS="-O3 -g -fPIC -xcore-avx2 -axmic-avx512 -pthread" \
     -DCMAKE_CXX_FLAGS="-O3 -g -fPIC -xcore-avx2 -axmic-avx512 -pthread -std=c++11" \
     -DPYTHON_EXECUTABLE:FILEPATH=$(which python3) \
+    -DBLAS_LIBRARIES=$MKLROOT/lib/intel64/libmkl_rt.so \
+    -DLAPACK_LIBRARIES=$MKLROOT/lib/intel64/libmkl_rt.so \
+    -DFFTW_ROOT=$CMBENV_AUX_ROOT \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -DSUITESPARSE_INCLUDE_DIR_HINTS="${CMBENV_AUX_ROOT}/include" \
     -DSUITESPARSE_LIBRARY_DIR_HINTS="${CMBENV_AUX_ROOT}/lib" \
     ${opts} \
     ..
+
 
