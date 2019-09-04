92cad2bf99c267272f38bea103fa777579438cf5
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-09-04 17:23:58 -0400

    disable tests with pysm

--- travis/travis_run.sh
+++ travis/travis_run.sh
@@ -45,7 +45,10 @@ mpirun -np 2 python -c "import toast.tests; toast.tests.run()"
 #===========================================================================
 
 cd examples/
-bash run_tiny_tests.sh
+# Disable tests with PySM until https://github.com/astropy/astropy/pull/9190
+# is merged or otherwise resolved
+#bash run_tiny_tests.sh
+TYPES="ground ground_simple" bash run_tiny_tests.sh
 cd ..
 
 #===========================================================================
