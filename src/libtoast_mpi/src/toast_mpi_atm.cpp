18804d37be600d822f6e5c36fd322e203fda2249
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-10-03 20:34:37 -0700

    temporarily enable debug statements in atmosphere

--- src/libtoast_mpi/src/toast_mpi_atm.cpp
+++ src/libtoast_mpi/src/toast_mpi_atm.cpp
@@ -3,9 +3,9 @@
 // All rights reserved.  Use of this source code is governed by
 // a BSD-style license that can be found in the LICENSE file.
 
-// #if !defined(DEBUG)
-// #   define DEBUG
-// #endif
+#if !defined(DEBUG)
+#   define DEBUG
+#endif
 
 #include <toast_mpi_internal.hpp>
 
