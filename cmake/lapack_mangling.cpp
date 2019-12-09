e54c84def7ccc1823f4676b8bd1b0cd68e82312b
Theodore Kisner <tskisner.public@gmail.com>
2019-12-09 10:15:14 -0800

    Cleanup and fix detection of BLAS/LAPACK and name mangling.  Now works on OS X with both accelerate framework and openblas.

--- cmake/lapack_mangling.cpp
+++ cmake/lapack_mangling.cpp
@@ -3,21 +3,21 @@
 // in the LAPACK library without actually using the fortran compiler.
 
 #if defined LAPACK_UPPER
-# define ilaver ILAVER
+# define func ILAVER
 #elif defined LAPACK_LOWER
-# define ilaver ilaver
+# define func ilaver
 #elif defined LAPACK_UBACK
-# define ilaver ilaver_
+# define func ilaver_
 #elif defined LAPACK_UFRONT
-# define ilaver _ilaver
+# define func _ilaver
 #endif // if defined LAPACK_UP
 extern "C" {
-void ilaver(int * major, int * minor, int * patch);
+void func(int * major, int * minor, int * patch);
 }
 int main(int argc, char ** argv) {
     int major = 0;
     int minor = 0;
     int patch = 0;
-    ilaver(&major, &minor, &patch);
+    func(&major, &minor, &patch);
     return 0;
 }
