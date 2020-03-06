78461d694bf5553f750d1816f75da0fb3f9d8b37
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2020-03-06 13:12:10 -0800

    and more

--- tutorial/03_Simulated_Sky_Signal/.simsky_mapdomain_sol.py
+++ tutorial/03_Simulated_Sky_Signal/.simsky_mapdomain_sol.py
@@ -1,7 +1,10 @@
-pysm_sky_cmb = PySMSky(
-    comm=None, pixel_indices=None, nside=NSIDE, units="uK_CMB", pysm_sky_config=["c1"]
+pysm_sky_cmb = PySMSky(comm=None,
+       pixel_indices=None,
+       nside=NSIDE,
+       units="uK_CMB",
+       pysm_sky_config=["c1"]
 )
 
-pysm_sky_cmb.exec(local_maps, out="cmb", bandpasses={"ch0": (100, 1), "ch1": (200, 1)})
+pysm_sky_cmb.exec(local_maps, out="cmb", bandpasses={"ch0":(100,1),"ch1":(200,1)})
 
-hp.mollview(local_maps["cmb_ch0"][0] - local_maps["cmb_ch1"][0])
+hp.mollview(local_maps["cmb_ch0"][0]-local_maps["cmb_ch1"][0])
