Many small fixes.  Noise covariance unit tests all passing.

src/toast/covariance.py
@@ -56,8 +56,9 @@ def create_local_invert(n_pix_submap, mapnnz, threshold, rcond, invert=False):
             )
             for lc in locs:
                 receive[lc : lc + n_submap_value] = reduce_buf
-                if rcond is not None:
-                    rcond.receive[lc : lc + (n_pix_submap * mapnnz)] = rcond.reduce_buf
+            if rcond is not None:
+                for lc in rcond._recv_locations[sm]:
+                    rcond.receive[lc : lc + n_pix_submap] = rcond.reduce_buf
 
     return local_invert
 
@@ -86,22 +87,16 @@ def covariance_invert(npp, threshold, rcond=None, use_alltoallv=False):
     """
     mapnnz = int(((np.sqrt(8 * npp.n_value) - 1) / 2) + 0.5)
     nppdata = npp.raw
-    if nppdata is None:
-        nppdata = np.empty(shape=0, dtype=np.float64)
     if rcond is not None:
-        if rcond.distribution.n_pix != npp.distribution.n_pix:
+        if npp.distribution != rcond.distribution:
             raise RuntimeError(
-                "covariance matrix and condition number map must have same number "
-                "of pixels"
-            )
-        if rcond.distribution.n_pix_submap != npp.distribution.n_pix_submap:
-            raise RuntimeError(
-                "covariance matrix and condition number map must have same submap size"
+                "covariance matrix and rcond must have same pixel distribution"
             )
         if rcond.n_value != 1:
             raise RuntimeError("condition number map should have n_value = 1")
 
     if use_alltoallv:
+        myp = npp.distribution.comm.rank
         if rcond is not None:
             # Stage data to receive buffer
             rcond.forward_alltoallv()
@@ -116,10 +111,10 @@ def covariance_invert(npp, threshold, rcond=None, use_alltoallv=False):
         else:
             rdata = rcond.raw
         cov_eigendecompose_diag(
-            1,
-            n_pix_submap,
+            npp.distribution.n_local_submap,
+            npp.distribution.n_pix_submap,
             mapnnz,
-            reduce_buf,
+            nppdata,
             rdata,
             threshold,
             True,
@@ -177,10 +172,8 @@ def covariance_multiply(npp1, npp2, use_alltoallv=False):
     """
     mapnnz = int(((np.sqrt(8 * npp1.n_value) - 1) / 2) + 0.5)
 
-    if npp1.n_pix != npp2.n_pix:
-        raise RuntimeError("covariance matrices must have same number of pixels")
-    if npp1.n_pix_submap != npp2.n_pix_submap:
-        raise RuntimeError("covariance matrices must have same submap size")
+    if npp1.distribution != npp2.distribution:
+        raise RuntimeError("covariance matrices must have same pixel distribution")
     if npp1.n_value != npp2.n_value:
         raise RuntimeError("covariance matrices must have same n_values")
 
@@ -189,7 +182,9 @@ def covariance_multiply(npp1, npp2, use_alltoallv=False):
         lmultiply = create_local_multiply(npp1.distribution.n_pix_submap, mapnnz, npp2)
         npp1.sync_alltoallv(local_func=lmultiply)
     else:
-        cov_mult_diag(npp1.n_submap, npp1.n_pix_submap, mapnnz, npp1data, npp2data)
+        cov_mult_diag(
+            npp1.n_local_submap, npp1.n_pix_submap, mapnnz, npp1data, npp2data
+        )
 
     return
 
@@ -218,7 +213,7 @@ def create_local_apply(n_pix_submap, mapnnz, m):
 
             cov_apply_diag(1, n_pix_submap, mapnnz, reduce_buf, m_buf)
 
-            for lc in locs:
+            for lc in m._recv_locations[sm]:
                 m.receive[lc : lc + (n_pix_submap * mapnnz)] = m.reduce_buf
 
     return local_apply
@@ -244,10 +239,10 @@ def covariance_apply(npp, m, use_alltoallv=False):
     """
     mapnnz = int(((np.sqrt(8 * npp.n_value) - 1) / 2) + 0.5)
 
-    if m.n_pix != npp.n_pix:
-        raise RuntimeError("covariance matrix and map must have same number of pixels")
-    if m.n_pix_submap != npp.n_pix_submap:
-        raise RuntimeError("covariance matrix and map must have same submap size")
+    if npp.distribution != m.distribution:
+        raise RuntimeError(
+            "covariance matrix and map must have same pixel distribution"
+        )
     if m.n_value != mapnnz:
         raise RuntimeError("covariance matrix and map have incompatible NNZ values")
 
@@ -258,7 +253,13 @@ def covariance_apply(npp, m, use_alltoallv=False):
     else:
         nppdata = npp.raw
         mdata = m.raw
-        cov_apply_diag(npp.n_submap, npp.n_pix_submap, mapnnz, nppdata, mdata)
+        cov_apply_diag(
+            npp.distribution.n_local_submap,
+            npp.distribution.n_pix_submap,
+            mapnnz,
+            nppdata,
+            mdata,
+        )
     return
 
 
@@ -294,10 +295,10 @@ def covariance_rcond(npp, use_alltoallv=False):
         nppdata = npp.raw
         rdata = rcond.raw
         cov_eigendecompose_diag(
-            1,
-            n_pix_submap,
+            npp.distribution.n_local_submap,
+            npp.distribution.n_pix_submap,
             mapnnz,
-            reduce_buf,
+            nppdata,
             rdata,
             threshold,
             False,

src/toast/future_ops/mapmaker_utils.py
@@ -128,7 +128,7 @@ class BuildHitMap(Operator):
                     local_pix[flags != 0] = -1
 
                 cov_accum_diag_hits(
-                    dist.n_submap,
+                    dist.n_local_submap,
                     dist.n_pix_submap,
                     1,
                     local_sm.astype(np.int64),
@@ -313,7 +313,7 @@ class BuildInverseCovariance(Operator):
 
                 # Accumulate
                 cov_accum_diag_invnpp(
-                    dist.n_submap,
+                    dist.n_local_submap,
                     dist.n_pix_submap,
                     weight_nnz,
                     local_sm.astype(np.int64),
@@ -503,7 +503,7 @@ class BuildNoiseWeighted(Operator):
 
                 # Accumulate
                 cov_accum_zmap(
-                    dist.n_submap,
+                    dist.n_local_submap,
                     dist.n_pix_submap,
                     self._zmap.n_value,
                     local_sm.astype(np.int64),

src/toast/noise.py
@@ -165,7 +165,7 @@ class Noise(object):
                 noisevar = np.median(psd[ind])
                 for det in self.detectors:
                     wt = self.weight(det, k)
-                    if wt > 0.0:
+                    if wt != 0.0:
                         self._detweights[det] += wt * (1.0 / noisevar)
         return self._detweights[det]
 

src/toast/pixels.py
@@ -73,6 +73,31 @@ class PixelDistribution(object):
         self._owned_submaps = None
         self._alltoallv_info = None
 
+    def __eq__(self, other):
+        local_eq = True
+        if self._n_pix != other._n_pix:
+            local_eq = False
+        if self._n_submap != other._n_submap:
+            local_eq = False
+        if self._n_pix_submap != other._n_pix_submap:
+            local_eq = False
+        if not np.array_equal(self._local_submaps, other._local_submaps):
+            local_eq = False
+        if self._comm is None and other._comm is not None:
+            local_eq = False
+        if self._comm is not None and other._comm is None:
+            local_eq = False
+        if self._comm is not None:
+            comp = MPI.Comm.Compare(self._comm, other._comm)
+            if comp not in (MPI.IDENT, MPI.CONGRUENT):
+                local_eq = False
+        if self._comm is not None:
+            local_eq = self._comm.allreduce(local_eq, op=MPI.LAND)
+        return local_eq
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def clear(self):
         """Delete the underlying memory.
 
@@ -171,8 +196,8 @@ class PixelDistribution(object):
         local_sm, pixels = libtoast_global_to_local(
             gl, self._n_pix_submap, self._glob2loc
         )
-        local_sm[:] *= self._n_pix_submap
-        pixels[:] += local_sm
+        local_sm *= self._n_pix_submap
+        pixels += local_sm
         return pixels
 
     def __repr__(self):
@@ -204,16 +229,6 @@ class PixelDistribution(object):
             # Need to compute it.
             local_hit_submaps = np.zeros(self._n_submap, dtype=np.uint8)
             local_hit_submaps[self._local_submaps] = 1
-            # print(
-            #     "rank {} local_submaps = {}".format(
-            #         self._comm.rank, self._local_submaps[:]
-            #     )
-            # )
-            # print(
-            #     "rank {} local_hit_submaps = {}".format(
-            #         self._comm.rank, local_hit_submaps[:]
-            #     )
-            # )
 
             hit_submaps = None
             if self._comm.rank == 0:
@@ -223,9 +238,6 @@ class PixelDistribution(object):
             del local_hit_submaps
 
             if self._comm.rank == 0:
-                # print(
-                #     "rank {} hit_submaps = {}".format(self._comm.rank, hit_submaps[:])
-                # )
                 total_hit_submaps = np.sum(hit_submaps.astype(np.int32))
                 tdist = distribute_uniform(total_hit_submaps, self._comm.size)
 
@@ -247,8 +259,6 @@ class PixelDistribution(object):
                 del hit_submaps
 
             self._comm.Bcast(self._submap_owners, root=0)
-            # if self._comm.rank == 0:
-            #     print("submap owners = {}".format(self._submap_owners[:]))
         return self._submap_owners
 
     @property
@@ -261,7 +271,6 @@ class PixelDistribution(object):
         self._owned_submaps = np.array(
             [x for x, y in enumerate(owners) if y == self._comm.rank], dtype=np.int32
         )
-        # print("rank {} owns submaps {}".format(self._comm.rank, self._owned_submaps[:]))
         return self._owned_submaps
 
     @property
@@ -506,6 +515,18 @@ class PixelData(object):
         )
         return val
 
+    def duplicate(self):
+        """Create a copy of the data with the same distribution.
+
+        Returns:
+            (PixelData):  A duplicate of the instance with copied data but the same
+                distribution.
+
+        """
+        dup = PixelData(self.distribution, self.dtype, n_value=self.n_value)
+        dup.raw[:] = self.raw
+        return dup
+
     def comm_nsubmap(self, bytes):
         """Given a buffer size, compute the number of submaps to communicate.
 
@@ -607,6 +628,7 @@ class PixelData(object):
     def setup_alltoallv(self):
         """Check that alltoallv buffers exist and create them if needed."""
         if self._send_counts is None:
+            log = Logger.get()
             # Get the parameters in terms of submaps.
             (
                 send_counts,
@@ -634,27 +656,41 @@ class PixelData(object):
             self._reduce_buf_raw = self.storage_class.zeros(self._n_submap_value)
             self.reduce_buf = self._reduce_buf_raw.array()
 
-            if self._dist.comm is None:
-                # For this case, point the receive member to the original data.  This
-                # will allow codes processing locally owned submaps to work
-                # transparently in the serial case.
-                self.receive = self.data
-            else:
-                # Check that our send and receive buffers do not exceed 32bit indices
-                # required by MPI
-                max_int = 2147483647
-                if scale * (self._recv_displ[-1] + self._recv_counts[-1]) > max_int:
-                    msg = "Alltoallv receive buffer size exceeds max 32bit integer"
-                    raise RuntimeError(msg)
-                if len(self.raw) > max_int:
-                    msg = "Alltoallv send buffer size exceeds max 32bit integer"
-                    raise RuntimeError(msg)
+            buf_check_fail = False
+            try:
+                if self._dist.comm is None:
+                    # For this case, point the receive member to the original data.
+                    # This will allow codes processing locally owned submaps to work
+                    # transparently in the serial case.
+                    self.receive = self.data
+                else:
+                    # Check that our send and receive buffers do not exceed 32bit
+                    # indices required by MPI
+                    max_int = 2147483647
+                    recv_buf_size = self._recv_displ[-1] + self._recv_counts[-1]
+                    if recv_buf_size > max_int:
+                        msg = "Proc {} Alltoallv receive buffer size exceeds max 32bit integer".format(
+                            self._dist.comm.rank
+                        )
+                        log.error(msg)
+                        buf_check_fail = True
+                    if len(self.raw) > max_int:
+                        msg = "Proc {} Alltoallv send buffer size exceeds max 32bit integer".format(
+                            self._dist.comm.rank
+                        )
+                        log.error(msg)
+                        buf_check_fail = True
 
-                # Allocate a persistent receive buffer
-                self._receive_raw = self.storage_class.zeros(
-                    self._recv_displ[-1] + self._recv_counts[-1]
-                )
-                self.receive = self._receive_raw.array()
+                    # Allocate a persistent receive buffer
+                    self._receive_raw = self.storage_class.zeros(recv_buf_size)
+                    self.receive = self._receive_raw.array()
+            except:
+                buf_check_fail = True
+            if self._dist.comm is not None:
+                buf_check_fail = self._dist.comm.allreduce(buf_check_fail, op=MPI.LOR)
+            if buf_check_fail:
+                msg = "alltoallv buffer setup failed on one or more processes"
+                raise RuntimeError(msg)
 
     @function_timer
     def forward_alltoallv(self):
@@ -668,7 +704,10 @@ class PixelData(object):
             None.
 
         """
+        myp = self.distribution.comm.rank
+
         self.setup_alltoallv()
+
         if self._dist.comm is None:
             # No communication needed
             return
@@ -720,6 +759,7 @@ class PixelData(object):
             None.
 
         """
+        myp = self.distribution.comm.rank
         self.forward_alltoallv()
 
         if local_func is None:

src/toast/tests/ops_mapmaker_utils.py
@@ -5,20 +5,17 @@
 import os
 
 import numpy as np
+import numpy.testing as nt
 
 from astropy import units as u
 
 from .mpi import MPITestCase
 
-from ..vis import set_matplotlib_backend
-
-from .. import rng as rng
-
 from ..noise import Noise
 
 from .. import future_ops as ops
 
-from ..future_ops.sim_tod_noise import sim_noise_timestream
+from ..pixels import PixelDistribution, PixelData
 
 from ._helpers import create_outdir, create_satellite_data
 
@@ -27,6 +24,24 @@ class MapmakerUtilsTest(MPITestCase):
     def setUp(self):
         fixture_name = os.path.splitext(os.path.basename(__file__))[0]
         self.outdir = create_outdir(self.comm, fixture_name)
+        np.random.seed(123456)
+        self.mix_coeff = np.random.uniform(low=-1.0, high=1.0, size=1000)
+
+    def create_corr_noise(self, dets, nse):
+        corr_freqs = {"noise_{}".format(i): nse.freq(x) for i, x in enumerate(dets)}
+        corr_psds = {"noise_{}".format(i): nse.psd(x) for i, x in enumerate(dets)}
+        corr_indices = {"noise_{}".format(i): 100 + i for i, x in enumerate(dets)}
+        corr_mix = dict()
+        for i, x in enumerate(dets):
+            dmix = self.mix_coeff[: len(dets)]
+            corr_mix[x] = {"noise_{}".format(y): dmix[y] for y in range(len(dets))}
+        return Noise(
+            detectors=dets,
+            freqs=corr_freqs,
+            psds=corr_psds,
+            mixmatrix=corr_mix,
+            indices=corr_indices,
+        )
 
     def test_hits(self):
         # Create a fake satellite data set for testing
@@ -40,6 +55,20 @@ class MapmakerUtilsTest(MPITestCase):
 
         build_hits = ops.BuildHitMap(pixel_dist="pixel_dist")
         hits = build_hits.apply(data)
+
+        # Manual check
+        check_hits = PixelData(data["pixel_dist"], np.int64, n_value=1)
+        for ob in data.obs:
+            for det in ob.local_detectors:
+                local_sm, local_pix = data["pixel_dist"].global_pixel_to_submap(
+                    ob.detdata["pixels"][det]
+                )
+                for i in range(ob.n_local_samples):
+                    if local_pix[i] >= 0:
+                        check_hits.data[local_sm[i], local_pix[i], 0] += 1
+        check_hits.sync_allreduce()
+
+        nt.assert_equal(hits.data, check_hits.data)
 
         del data
         return
@@ -62,33 +91,7 @@ class MapmakerUtilsTest(MPITestCase):
         # observation.
         for ob in data.obs:
             nse = ob[default_model.noise_model]
-            corr_freqs = {
-                "noise_{}".format(i): nse.freq(x)
-                for i, x in enumerate(ob.local_detectors)
-            }
-            corr_psds = {
-                "noise_{}".format(i): nse.psd(x)
-                for i, x in enumerate(ob.local_detectors)
-            }
-            corr_indices = {
-                "noise_{}".format(i): 100 + i for i, x in enumerate(ob.local_detectors)
-            }
-            corr_mix = dict()
-            for i, x in enumerate(ob.local_detectors):
-                dmix = np.random.uniform(
-                    low=-1.0, high=1.0, size=len(ob.local_detectors)
-                )
-                corr_mix[x] = {
-                    "noise_{}".format(y): dmix[y]
-                    for y in range(len(ob.local_detectors))
-                }
-            ob["noise_model_corr"] = Noise(
-                detectors=ob.local_detectors,
-                freqs=corr_freqs,
-                psds=corr_psds,
-                mixmatrix=corr_mix,
-                indices=corr_indices,
-            )
+            ob["noise_model_corr"] = self.create_corr_noise(ob.local_detectors, nse)
 
         # Simulate noise using both models
 
@@ -110,6 +113,50 @@ class MapmakerUtilsTest(MPITestCase):
         )
         invnpp_corr = build_invnpp_corr.apply(data)
 
+        # Manual check
+
+        check_invnpp = PixelData(data["pixel_dist"], np.float64, n_value=6)
+        check_invnpp_corr = PixelData(data["pixel_dist"], np.float64, n_value=6)
+
+        for ob in data.obs:
+            noise = ob["noise_model"]
+            noise_corr = ob["noise_model_corr"]
+
+            for det in ob.local_detectors:
+                detweight = noise.detector_weight(det)
+                detweight_corr = noise_corr.detector_weight(det)
+
+                wt = ob.detdata["weights"][det]
+                local_sm, local_pix = data["pixel_dist"].global_pixel_to_submap(
+                    ob.detdata["pixels"][det]
+                )
+                for i in range(ob.n_local_samples):
+                    if local_pix[i] < 0:
+                        continue
+                    off = 0
+                    for j in range(3):
+                        for k in range(j, 3):
+                            check_invnpp.data[local_sm[i], local_pix[i], off] += (
+                                detweight * wt[i, j] * wt[i, k]
+                            )
+                            check_invnpp_corr.data[local_sm[i], local_pix[i], off] += (
+                                detweight_corr * wt[i, j] * wt[i, k]
+                            )
+                            off += 1
+
+        check_invnpp.sync_allreduce()
+        check_invnpp_corr.sync_allreduce()
+
+        for sm in range(invnpp.distribution.n_local_submap):
+            for px in range(invnpp.distribution.n_pix_submap):
+                if invnpp.data[sm, px, 0] != 0:
+                    nt.assert_almost_equal(
+                        invnpp.data[sm, px], check_invnpp.data[sm, px]
+                    )
+                if invnpp_corr.data[sm, px, 0] != 0:
+                    nt.assert_almost_equal(
+                        invnpp_corr.data[sm, px], check_invnpp_corr.data[sm, px]
+                    )
         del data
         return
 
@@ -131,33 +178,7 @@ class MapmakerUtilsTest(MPITestCase):
         # observation.
         for ob in data.obs:
             nse = ob[default_model.noise_model]
-            corr_freqs = {
-                "noise_{}".format(i): nse.freq(x)
-                for i, x in enumerate(ob.local_detectors)
-            }
-            corr_psds = {
-                "noise_{}".format(i): nse.psd(x)
-                for i, x in enumerate(ob.local_detectors)
-            }
-            corr_indices = {
-                "noise_{}".format(i): 100 + i for i, x in enumerate(ob.local_detectors)
-            }
-            corr_mix = dict()
-            for i, x in enumerate(ob.local_detectors):
-                dmix = np.random.uniform(
-                    low=-1.0, high=1.0, size=len(ob.local_detectors)
-                )
-                corr_mix[x] = {
-                    "noise_{}".format(y): dmix[y]
-                    for y in range(len(ob.local_detectors))
-                }
-            ob["noise_model_corr"] = Noise(
-                detectors=ob.local_detectors,
-                freqs=corr_freqs,
-                psds=corr_psds,
-                mixmatrix=corr_mix,
-                indices=corr_indices,
-            )
+            ob["noise_model_corr"] = self.create_corr_noise(ob.local_detectors, nse)
 
         # Simulate noise using both models
 
@@ -180,6 +201,41 @@ class MapmakerUtilsTest(MPITestCase):
             det_data="noise_corr",
         )
         zmap_corr = build_zmap_corr.apply(data)
+
+        # Manual check
+
+        check_zmap = PixelData(data["pixel_dist"], np.float64, n_value=3)
+        check_zmap_corr = PixelData(data["pixel_dist"], np.float64, n_value=3)
+
+        for ob in data.obs:
+            noise = ob["noise_model"]
+            noise_corr = ob["noise_model_corr"]
+            for det in ob.local_detectors:
+                wt = ob.detdata["weights"][det]
+                local_sm, local_pix = data["pixel_dist"].global_pixel_to_submap(
+                    ob.detdata["pixels"][det]
+                )
+
+                for i in range(ob.n_local_samples):
+                    if local_pix[i] < 0:
+                        continue
+                    for j in range(3):
+                        check_zmap.data[local_sm[i], local_pix[i], j] += (
+                            noise.detector_weight(det)
+                            * ob.detdata["noise"][det, i]
+                            * wt[i, j]
+                        )
+                        check_zmap_corr.data[local_sm[i], local_pix[i], j] += (
+                            noise_corr.detector_weight(det)
+                            * ob.detdata["noise_corr"][det, i]
+                            * wt[i, j]
+                        )
+
+        check_zmap.sync_allreduce()
+        check_zmap_corr.sync_allreduce()
+
+        np.testing.assert_almost_equal(zmap.data, check_zmap.data)
+        np.testing.assert_almost_equal(zmap_corr.data, check_zmap_corr.data)
 
         del data
         return