87264192fe6cdfb35fbbd6f682f48aad91e2c4d7
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-02-22 21:09:28 -0800

    For the few pathological matrices, just use tthe diagonal

--- src/libtoast/src/toast_atm_sim.cpp
+++ src/libtoast/src/toast_atm_sim.cpp
@@ -722,13 +722,12 @@ cholmod_sparse * toast::atm_sim_sqrt_sparse_covariance(
                 // Extract band diagonal of the matrix and try
                 // factorizing again
                 // int ndiag = ntry - itry - 1;
-                int ndiag = nelem - nelem * (itry + 1) / ntry;
-                if (ndiag < 3) ndiag = 3;
+                int ndiag = nelem - nelem * (itry + 1) / (ntry - 1);
+                if (ndiag < 1) ndiag = 1;
                 int iupper = ndiag - 1;
                 int ilower = -iupper;
                 if (atm_verbose()) {
                     cholmod_print_sparse(cov, "Covariance matrix", chol.chcommon);
-
                     // DEBUG begin
                     if (itry == ntry - 2) {
                         FILE * covfile = fopen("failed_covmat.mtx", "w");
@@ -736,7 +735,6 @@ cholmod_sparse * toast::atm_sim_sqrt_sparse_covariance(
                                              chol.chcommon);
                         fclose(covfile);
                     }
-
                     // DEBUG end
                     o.str("");
                     o << rank
@@ -749,6 +747,17 @@ cholmod_sparse * toast::atm_sim_sqrt_sparse_covariance(
 
                 if (chol.chcommon->status != CHOLMOD_OK) {
                     throw std::runtime_error("cholmod_band_inplace failed.");
+                }
+                if (atm_verbose()) {
+                    cholmod_print_sparse(cov, "Covariance matrix", chol.chcommon);
+                    // DEBUG begin
+                    if (itry == ntry - 2) {
+                        FILE * covfile = fopen("band_covmat.mtx", "w");
+                        cholmod_write_sparse(covfile, cov, NULL, NULL,
+                                             chol.chcommon);
+                        fclose(covfile);
+                    }
+                    // DEBUG end
                 }
             } else throw std::runtime_error("cholmod_factorize failed.");
         } else {
