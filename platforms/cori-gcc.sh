93aebeaed0a453003e4360358899597aa1554da2
Theodore Kisner <tskisner.public@gmail.com>
2021-06-01 14:24:28 -0700

    Fix cori-gcc platform example to use static linking of dependencies.

--- platforms/cori-gcc.sh
+++ platforms/cori-gcc.sh
@@ -7,13 +7,16 @@ cmake \
     -DCMAKE_CXX_COMPILER="g++" \
     -DCMAKE_C_FLAGS="-O3 -g -fPIC -pthread" \
     -DCMAKE_CXX_FLAGS="-O3 -g -fPIC -pthread" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-lgfortran" \
+    -DCMAKE_EXE_LINKER_FLAGS="-lgfortran" \
+    -DCMAKE_MODULE_LINKER_FLAGS="-lgfortran" \
     -DPYTHON_EXECUTABLE:FILEPATH=$(which python3) \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -DMKL_DISABLED=TRUE \
-    -DBLAS_LIBRARIES=$CMBENV_AUX_ROOT/lib/libopenblas.so \
-    -DLAPACK_LIBRARIES=$CMBENV_AUX_ROOT/lib/libopenblas.so \
+    -DTOAST_STATIC_DEPS:BOOL=ON \
     -DFFTW_ROOT=$CMBENV_AUX_ROOT \
     -DSUITESPARSE_INCLUDE_DIR_HINTS="${CMBENV_AUX_ROOT}/include" \
     -DSUITESPARSE_LIBRARY_DIR_HINTS="${CMBENV_AUX_ROOT}/lib" \
     ${opts} \
     ..
+
