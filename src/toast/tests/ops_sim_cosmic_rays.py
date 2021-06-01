9156bb021dbe57fdcc53074be218eb3b097920d6
Giuseppe Puglisi <giuse.puglisi@gmail.com>
2021-05-31 18:49:46 -0700

    improving tests

--- src/toast/tests/ops_sim_cosmic_rays.py
+++ src/toast/tests/ops_sim_cosmic_rays.py
@@ -31,20 +31,34 @@ class SimCosmicRayTest(MPITestCase):
         fixture_name = os.path.splitext(os.path.basename(__file__))[0]
         self.outdir = create_outdir(self.comm, fixture_name)
 
-    def test_cosmic_rays(self):
+    def make_mock_cosmic_ray_data(self, data , crfile ):
+        # so far we hardcode typical values
+        low_noise= [-3e-17 , 1e-17 ]
+        for obs  in  (data.obs ):
+            for kk, det in enumerate(obs.local_detectors) :
+                direct_hits =np.zeros((400,3 ))
+
+                direct_hits [:,0]= 5e-17* np.random.randn(400) + 3e-17 #C1, Watts
+                direct_hits [:,1]=1e-15* np.random.randn(400) + 2e-15 #C2, watts
+                direct_hits [:,2]=2* np.random.randn(400) +  4   #time constant, msec
+                filename =  crfile.replace("detector", f"det{kk}")
+                np.savez(filename, low_noise=low_noise ,
+                            direct_hits= direct_hits,
+                            sampling_rate= [156]     )
+        return
+
+    def test_cosmic_rays_wafer_noise(self):
         # Create a fake satellite data set for testing
         data = create_satellite_data(
             self.comm,
         )
+        crfile=f"{self.outdir}/cosmic_ray_glitches_detector.npz"
+        self.make_mock_cosmic_ray_data( data, crfile )
 
-        # Create a noise model from focalplane detector properties
-        default_model = ops.DefaultNoiseModel()
-        default_model.apply(data)
         # Simulate noise using this model
         key = "my_signal"
-        dir="/Users/peppe/work/satellite_sims/cosmic_rays/"
         sim_cosmic_rays = ops.InjectCosmicRays(det_data=key,
-                        crfile=f"{dir}/cosmic_ray_glitches_detector.npz",
+                        crfile=crfile ,
                     )
         sim_cosmic_rays.apply(data)
         for obs  in  (data.obs ):
@@ -54,4 +68,50 @@ class SimCosmicRayTest(MPITestCase):
 
             for det in obs.local_detectors:
                 detindx = focalplane[det]["uid"]
-                print(obs.detdata[key][det]  )
+                assert (obs.detdata[key][det].sum() !=0.   )
+
+    def test_cosmic_rays_glitches(self):
+        # Create a fake satellite data set for testing
+        data = create_satellite_data(
+            self.comm,
+        )
+        crfile=f"{self.outdir}/cosmic_ray_glitches_detector.npz"
+        self.make_mock_cosmic_ray_data( data, crfile )
+
+        # Simulate noise using this model
+        key = "my_signal"
+        sim_cosmic_rays = ops.InjectCosmicRays(det_data=key,
+                        crfile=crfile ,inject_direct_hits=True
+                    )
+        sim_cosmic_rays.apply(data)
+        for obs  in  (data.obs ):
+            telescope = obs.telescope.uid
+            focalplane = obs.telescope.focalplane
+            obsindx = obs.uid
+            for det in obs.local_detectors:
+                detindx = focalplane[det]["uid"]
+                assert (obs.detdata[key][det].sum() !=0.   )
+
+
+    def test_cosmic_rays_commonmode(self):
+        # Create a fake satellite data set for testing
+        data = create_satellite_data(
+            self.comm,
+        )
+        crfile=f"{self.outdir}/cosmic_ray_glitches_detector.npz"
+        self.make_mock_cosmic_ray_data( data, crfile )
+
+        # Simulate noise using this model
+        key = "my_signal"
+        sim_cosmic_rays = ops.InjectCosmicRays(det_data=key,
+                        crfile=crfile ,include_common_mode=True
+                    )
+        sim_cosmic_rays.apply(data)
+        for obs  in  (data.obs ):
+            telescope = obs.telescope.uid
+            focalplane = obs.telescope.focalplane
+            obsindx = obs.uid
+
+            for det in obs.local_detectors:
+                detindx = focalplane[det]["uid"]
+                assert (obs.detdata[key][det].sum() !=0.   )
