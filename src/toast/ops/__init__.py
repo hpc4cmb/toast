fb7dfdda76a5afd7641512c005db9e89245b3d08
Giuseppe Puglisi <giuse.puglisi@gmail.com>
2021-05-31 17:48:44 -0700

    implemented the operator to inject cosmic rays

--- src/toast/ops/__init__.py
+++ src/toast/ops/__init__.py
@@ -63,5 +63,6 @@ from .madam import Madam
 from .conviqt import SimConviqt, SimWeightedConviqt
 
 from .sim_gaindrifts import GainDrifter
+from .sim_cosmic_rays import InjectCosmicRays
 
 from .totalconvolve import SimTotalconvolve
