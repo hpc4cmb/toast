53ca1a2ef4480166d3d476a3c1dba4126854881b
Kolen Cheung <christian.kolen@gmail.com>
2021-06-03 18:29:44 -0700

    tap into toast's test framework instead of pytest

--- src/toast/tests/ops_crosstalk.py
+++ src/toast/tests/ops_crosstalk.py
@@ -3,15 +3,17 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+# import os
 import numpy as np
 from numpy.random import default_rng
-import pytest
 
 import toast
 from toast.mpi import get_world
 from toast.utils import Logger
 
-from toast.tod.crosstalk import SimpleCrosstalkMatrix, OpCrosstalk
+from .mpi import MPITestCase
+from ..tod.crosstalk import SimpleCrosstalkMatrix, OpCrosstalk
+# from ._helpers import create_outdir, create_satellite_data
 
 if TYPE_CHECKING:
     from typing import List
@@ -29,6 +31,8 @@ detrankses = (1,) if procs == 1 else (1, 2, 4)
 
 names_str = [f'A{i}' for i in range(n_detectors)]
 names = np.array(names_str, dtype='S')
+names_2_str = [f'B{i}' for i in range(n_detectors)]
+names_2 = np.array(names_str, dtype='S')
 
 tod_array = np.arange(n_detectors * n_samples, dtype=np.float64).reshape((n_detectors, n_samples))
 crosstalk_data = np.arange(n_detectors * n_detectors, dtype=np.float64).reshape((n_detectors, n_detectors))
@@ -48,116 +52,123 @@ class FakeData:
     obs: List[dict]
 
 
-cases = []
-for detranks in detrankses:
-    cases += [
-        (
-            tod_array,
-            crosstalk_data,
-            tod_crosstalked,
-            detranks,
-        ),
-        (
-            tod_array_random,
-            crosstalk_data_2,
-            tod_crosstalked_random,
-            detranks,
-        ),
-    ]
+class OpCrosstalkTest(MPITestCase):
 
+    # def setUp(self):
+    #     fixture_name = os.path.splitext(os.path.basename(__file__))[0]
+    #     self.outdir = create_outdir(self.comm, fixture_name)
 
-@pytest.mark.parametrize("tod_array,crosstalk_data,tod_crosstalked,detranks", cases)
-def test_op_crosstalk(
-    tod_array: np.ndarray[np.float64],
-    crosstalk_data: np.ndarray[np.float64],
-    tod_crosstalked: np.ndarray[np.float64],
-    detranks: int,
-):
-    if rank == 0:
-        crosstalk_matrices = [SimpleCrosstalkMatrix(names, crosstalk_data)]
-    else:
-        crosstalk_matrices = []
-    op_crosstalk = OpCrosstalk(1, crosstalk_matrices)
+    @staticmethod
+    def _tset_op_crosstalk(
+        tod_array: np.ndarray[np.float64],
+        crosstalk_data: np.ndarray[np.float64],
+        tod_crosstalked: np.ndarray[np.float64],
+        detranks: int,
+    ):
+        if rank == 0:
+            crosstalk_matrices = [SimpleCrosstalkMatrix(names, crosstalk_data)]
+        else:
+            crosstalk_matrices = []
+        op_crosstalk = OpCrosstalk(1, crosstalk_matrices)
 
-    tod = toast.tod.TODCache(mpiworld, names_str, n_samples, detranks=detranks)
+        tod = toast.tod.TODCache(mpiworld, names_str, n_samples, detranks=detranks)
 
-    # write local detectors
-    start, length = tod.local_samples
-    local_dets_set = set(tod.local_dets)
-    for i, name in enumerate(names_str):
-        if name in local_dets_set:
-            tod.write(detector=name, data=tod_array[i, start:start + length])
-    data = FakeData([{
-        'tod': tod,
-    }])
-    op_crosstalk.exec(data, signal_name=signal_name)
-
-    # check output local detectors
-    # half float64 precision (float64 has 53-bit precison)
-    # will not produce bit-identical output as mat-mul
-    # is sensitive to the sum-ordering
-    ulp_max = 2**(53 / 2)
-    for i, name in enumerate(names_str):
-        if name in local_dets_set:
-            output = tod.cache.reference(f"{signal_name}_{name}")
-            answer = tod_crosstalked[i, start:start + length]
-            ulp = np.testing.assert_array_max_ulp(answer, output, ulp_max)
-            log.info(f'Reproducing mat-mul for detector {name} with max ULP: {ulp.max():e}')
-
-
-names_2_str = [f'B{i}' for i in range(n_detectors)]
-names_2 = np.array(names_str, dtype='S')
-cases = []
-for detranks in detrankses:
-    cases.append(
-        (
-            [names, names_2],
-            [names_str, names_2_str],
-            [tod_array, tod_array_random],
-            [crosstalk_data, crosstalk_data_2],
-            [tod_crosstalked, tod_crosstalked_random],
-            detranks,
-        ),
-    )
-
-
-@pytest.mark.parametrize("names,names_strs,tods_array,crosstalk_datas,tods_crosstalked,detranks", cases)
-def test_op_crosstalk_multiple_matrices(
-    names: List[np.ndarray['S']],
-    names_strs: List[List[str]],
-    tods_array: List[np.ndarray[np.float64]],
-    crosstalk_datas: List[np.ndarray[np.float64]],
-    tods_crosstalked: List[np.ndarray[np.float64]],
-    detranks: int,
-):
-    n_crosstalk_matrices = len(names)
-    idxs_per_rank = range(rank, n_crosstalk_matrices, procs)
-    crosstalk_matrices = [SimpleCrosstalkMatrix(names[i], crosstalk_datas[i]) for i in idxs_per_rank]
-    op_crosstalk = OpCrosstalk(n_crosstalk_matrices, crosstalk_matrices)
-
-    tod = toast.tod.TODCache(mpiworld, sum(names_strs, []), n_samples, detranks=detranks)
-
-    # write local detectors
-    start, length = tod.local_samples
-    local_dets_set = set(tod.local_dets)
-    for names_str, tod_array in zip(names_strs, tods_array):
+        # write local detectors
+        start, length = tod.local_samples
+        local_dets_set = set(tod.local_dets)
         for i, name in enumerate(names_str):
             if name in local_dets_set:
                 tod.write(detector=name, data=tod_array[i, start:start + length])
-    data = FakeData([{
-        'tod': tod,
-    }])
-    op_crosstalk.exec(data, signal_name=signal_name)
+        data = FakeData([{
+            'tod': tod,
+        }])
+        op_crosstalk.exec(data, signal_name=signal_name)
 
-    # check output local detectors
-    # half float64 precision (float64 has 53-bit precison)
-    # will not produce bit-identical output as mat-mul
-    # is sensitive to the sum-ordering
-    ulp_max = 2**(53 / 2)
-    for names_str, tod_crosstalked in zip(names_strs, tods_crosstalked):
+        # check output local detectors
+        # half float64 precision (float64 has 53-bit precison)
+        # will not produce bit-identical output as mat-mul
+        # is sensitive to the sum-ordering
+        ulp_max = 2**(53 / 2)
         for i, name in enumerate(names_str):
             if name in local_dets_set:
                 output = tod.cache.reference(f"{signal_name}_{name}")
                 answer = tod_crosstalked[i, start:start + length]
                 ulp = np.testing.assert_array_max_ulp(answer, output, ulp_max)
                 log.info(f'Reproducing mat-mul for detector {name} with max ULP: {ulp.max():e}')
+
+    def test_op_crosstalk(self):
+        cases = []
+        for detranks in detrankses:
+            cases += [
+                (
+                    tod_array,
+                    crosstalk_data,
+                    tod_crosstalked,
+                    detranks,
+                ),
+                (
+                    tod_array_random,
+                    crosstalk_data_2,
+                    tod_crosstalked_random,
+                    detranks,
+                ),
+            ]
+        for case in cases:
+            self._tset_op_crosstalk(*case)
+
+    @staticmethod
+    def _tset_op_crosstalk_multiple_matrices(
+        names: List[np.ndarray['S']],
+        names_strs: List[List[str]],
+        tods_array: List[np.ndarray[np.float64]],
+        crosstalk_datas: List[np.ndarray[np.float64]],
+        tods_crosstalked: List[np.ndarray[np.float64]],
+        detranks: int,
+    ):
+        n_crosstalk_matrices = len(names)
+        idxs_per_rank = range(rank, n_crosstalk_matrices, procs)
+        crosstalk_matrices = [SimpleCrosstalkMatrix(names[i], crosstalk_datas[i]) for i in idxs_per_rank]
+        op_crosstalk = OpCrosstalk(n_crosstalk_matrices, crosstalk_matrices)
+
+        tod = toast.tod.TODCache(mpiworld, sum(names_strs, []), n_samples, detranks=detranks)
+
+        # write local detectors
+        start, length = tod.local_samples
+        local_dets_set = set(tod.local_dets)
+        for names_str, tod_array in zip(names_strs, tods_array):
+            for i, name in enumerate(names_str):
+                if name in local_dets_set:
+                    tod.write(detector=name, data=tod_array[i, start:start + length])
+        data = FakeData([{
+            'tod': tod,
+        }])
+        op_crosstalk.exec(data, signal_name=signal_name)
+
+        # check output local detectors
+        # half float64 precision (float64 has 53-bit precison)
+        # will not produce bit-identical output as mat-mul
+        # is sensitive to the sum-ordering
+        ulp_max = 2**(53 / 2)
+        for names_str, tod_crosstalked in zip(names_strs, tods_crosstalked):
+            for i, name in enumerate(names_str):
+                if name in local_dets_set:
+                    output = tod.cache.reference(f"{signal_name}_{name}")
+                    answer = tod_crosstalked[i, start:start + length]
+                    ulp = np.testing.assert_array_max_ulp(answer, output, ulp_max)
+                    log.info(f'Reproducing mat-mul for detector {name} with max ULP: {ulp.max():e}')
+
+    def test_op_crosstalk_multiple_matrices(self):
+        cases = []
+        for detranks in detrankses:
+            cases.append(
+                (
+                    [names, names_2],
+                    [names_str, names_2_str],
+                    [tod_array, tod_array_random],
+                    [crosstalk_data, crosstalk_data_2],
+                    [tod_crosstalked, tod_crosstalked_random],
+                    detranks,
+                ),
+            )
+        for case in cases:
+            self._tset_op_crosstalk_multiple_matrices(*case)
