5da7ae745e1807072f55e8bd0b327509ff9739f3
Kolen Cheung <christian.kolen@gmail.com>
2021-07-02 20:38:49 -0700

    minor change (remove an empty line)

--- src/toast/tod/__init__.py
+++ src/toast/tod/__init__.py
@@ -39,7 +39,6 @@ from .polyfilter import OpPolyFilter, OpPolyFilter2D
 
 from .gainscrambler import OpGainScrambler
 from .applygain import OpApplyGain, write_calibration_file
-
 from .crosstalk import OpCrosstalk, SimpleCrosstalkMatrix
 
 from .memorycounter import OpMemoryCounter
