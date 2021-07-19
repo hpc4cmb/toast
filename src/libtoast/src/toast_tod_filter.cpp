b2d98ede4d8e01f4ffa5de1eeec84340705bda33
nestordemeure <ndemeure@lbl.gov>
2021-07-19 16:05:39 -0700

    removed dgelss from tod filter

--- src/libtoast/src/toast_tod_filter.cpp
+++ src/libtoast/src/toast_tod_filter.cpp
@@ -140,24 +140,11 @@ void toast::filter_polynomial(int64_t order, size_t n, uint8_t * flags,
         // minimize the norm of the difference and the solution
         // vector.
 
-        // DGELSS will overwrite proj with the fitting
-        // coefficients.  invcov is overwritten with
-        // singular vectors.
-        int rank, info;
-        double rcond_limit = 1e-3;
-        int lwork = std::max(10 * (norder + nsignal), 1000000);
-        toast::AlignedVector <double> work(lwork);
-        toast::AlignedVector <double> singular_values(norder);
-        toast::LinearAlgebra::gelss(norder, norder, nsignal,
-                                    invcov.data(), norder,
-                                    proj.data(), norder,
-                                    singular_values.data(), rcond_limit,
-                                    &rank, work.data(), lwork, &info);
-        // TODO how does this impact speed and precision?
-        /*int info;
+        // DGELS will overwrite proj with the fitting coefficients.
+        int info;
         toast::LinearAlgebra::gels(norder, norder, nsignal,
                                     invcov.data(), norder,
-                                    proj.data(), norder, &info);*/
+                                    proj.data(), norder, &info);
 
         for (int iorder = 0; iorder < norder; ++iorder) {
             double * temp = &full_templates[iorder * scanlen];
