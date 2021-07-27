67972cdc96cfef37bd6b6220a3014f613d34141a
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-07-27 15:16:24 -0700

    Fix unit test failure at higher concurrency

--- src/toast/tests/_helpers.py
+++ src/toast/tests/_helpers.py
@@ -570,9 +570,8 @@ def fake_flags(data, shared_name="flags", shared_val=1, det_name="flags", det_va
         if ob.comm_col_rank == 0:
             fshared = np.zeros(ob.n_local_samples, dtype=np.uint8)
             fshared[::100] = shared_val
-        ob.shared[shared_name].set(
-            ob.shared[shared_name].data | fshared, offset=(0,), fromrank=0
-        )
+            fshared |= ob.shared[shared_name].data
+        ob.shared[shared_name].set(fshared, offset=(0,), fromrank=0)
         for det in ob.local_detectors:
             ob.detdata[det_name][det, :half] |= det_val
 
