Fix polarization angle calculation (#424)
* Replace the erroneous and untested method to extract polarization sensitive angles from quaternions

* Adopt a much cleaner implementation suggested by @mhasself

* More intuitive labeling

* Fix typo

src/toast/instrument.py
@@ -370,12 +370,9 @@ class Focalplane(object):
             )
             for row in self.detector_data:
                 quat = row["quat"]
-                theta, phi = qarray.to_position(quat)
-                yrot = qarray.rotation(self.YAXIS, -theta)
-                zrot = qarray.rotation(self.ZAXIS, -phi)
-                rot = qarray.norm(qarray.mult(yrot, zrot))
-                pol_rot = qarray.mult(rot, quat)
-                pol_angle = qarray.to_angles(pol_rot)[2]
+                a = quat[3]
+                d = quat[2]
+                pol_angle = np.arctan2(2 * a * d, a ** 2 - d ** 2) % np.pi
                 row["pol_angle"] = pol_angle * u.radian
 
     def _get_pol_efficiency(self):