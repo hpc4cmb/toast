c985608772c43cf231d4bf9f0f9908ac5d90c433
Theodore Kisner <tskisner.public@gmail.com>
2021-08-10 15:44:46 -0700

    Attempt to make the benchmark problem configuration more realistic.

--- src/toast/instrument_sim.py
+++ src/toast/instrument_sim.py
@@ -573,8 +573,11 @@ def fake_hexagon_focalplane(
     quat_A = hex_layout(n_pix, width_deg, "D", "A", pol_A)
     quat_B = hex_layout(n_pix, width_deg, "D", "B", pol_B)
 
-    det_data = dict(quat_A)
-    det_data.update(quat_B)
+    temp_data = dict(quat_A)
+    temp_data.update(quat_B)
+
+    # Sort by detector name so that detector pairs are together
+    det_data = {x: temp_data[x] for x in sorted(temp_data.keys())}
 
     nrings = hex_nring(n_pix)
 
