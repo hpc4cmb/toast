666d174c0a19d9d52eb37c89e7ae138fc285e609
Theodore Kisner <tskisner.public@gmail.com>
2021-12-02 12:16:06 -0800

    Fix detection of NVHPC

---
+++ platforms/cori-gpu-nvhpc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+opts="$@"
+
+cmake \
+    -DCMAKE_C_COMPILER="nvc" \
+    -DCMAKE_CXX_COMPILER="nvc++" \
+    -DCMAKE_C_FLAGS="-O3 -g -fPIC -pthread" \
+    -DCMAKE_CXX_FLAGS="-O3 -g -fPIC -pthread -std=c++11" \
+    -DMKL_DISABLED=TRUE \
+    -DPYTHON_EXECUTABLE:FILEPATH=$(which python3) \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DBLAS_LIBRARIES="-lopenblas -lgomp -lm" \
+    ${opts} \
+    ..
+
