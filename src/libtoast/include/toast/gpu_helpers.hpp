3761b3558dfa1d95055f9b78d29731f8fe828371
nestor <ndemeure@lbl.gov>
2021-12-01 16:27:11 -0800

    updated memory allocator

--- src/libtoast/include/toast/gpu_helpers.hpp
+++ src/libtoast/include/toast/gpu_helpers.hpp
@@ -17,12 +17,12 @@
 #include <cufft.h>
 
 // checks on various type of cuda error codes
-void checkCudaErrorCode(const cudaError errorCode, const std::string& functionName = "unknown");
-void checkCublasErrorCode(const cublasStatus_t errorCode, const std::string& functionName = "unknown");
-void checkCusolverErrorCode(const cusolverStatus_t errorCode, const std::string& functionName = "unknown");
-void checkCufftErrorCode(const cufftResult errorCode, const std::string& functionName = "unknown");
+void checkCudaErrorCode(const cudaError errorCode, const std::string &functionName = "unknown");
+void checkCublasErrorCode(const cublasStatus_t errorCode, const std::string &functionName = "unknown");
+void checkCusolverErrorCode(const cusolverStatus_t errorCode, const std::string &functionName = "unknown");
+void checkCufftErrorCode(const cufftResult errorCode, const std::string &functionName = "unknown");
 
-// fucntions to get a number of bbytes for an allocation
+// functions to get a number of bbytes for an allocation
 size_t GigagbytesToBytes(const size_t nbGB = 4);
 size_t FractionOfGPUMemory(const double fraction = 0.9);
 
@@ -30,11 +30,19 @@ size_t FractionOfGPUMemory(const double fraction = 0.9);
 class GPU_memory_block_t
 {
 public:
-    void* start;
-    void* end;
+    // pointers to the start and end of the gpu allocation represented by the block
+    void *start;
+    void *end;
+    // the size of the allocation in bytes
+    size_t size_bytes;
+    // whether that block has been freed (meaning it could be garbage collected later)
     bool isFree;
+    // cpu pointer that was used as a source for the allocation, might be nullptr if the data didn't come from cpu
+    void *cpu_ptr;
 
-    GPU_memory_block_t(void* ptr, size_t size);
+    // creating a new `GPU_memory_block_t`
+    // `cpu_ptr` is an, optional, pointer to the cpu memory that was moved into the block
+    GPU_memory_block_t(void *ptr, size_t size_bytes, void *cpu_ptr = nullptr);
 };
 
 // used to recycle GPU allocation
@@ -44,11 +52,12 @@ private:
     // used to make class threadsafe
     std::mutex alloc_mutex;
     // starting point of the current allocation
-    void* start;
-    // what is the total memory allocated here
-    size_t available_memory;
-    // memory blocks that have been allocated at some point
+    void *start;
+    // what is the total memory allocated here, in bytes
+    size_t available_memory_bytes;
+    // memory blocks that have been allocated but cannot be freed yet
     std::vector<GPU_memory_block_t> blocks;
+
 public:
     // reused handles for linear algebra (as they are very expensive to create)
     cublasHandle_t handleBlas = NULL;
@@ -57,40 +66,65 @@ public:
 
     GPU_memory_pool_t(size_t bytesPreallocated);
     ~GPU_memory_pool_t();
-    cudaError malloc(void** output_ptr, size_t size);
-    void free(void* ptr);
+    cudaError malloc(void **output_ptr, size_t size_bytes, void *cpu_ptr = nullptr);
+    void free(void *gpu_ptr);
 
     // allocates memory for the given number of elements and returns a pointer to the allocated memory
-    // this function is slightly higher level than malloc
-    template<typename T>
-    T* alloc(size_t size)
+    // this function is slightly higher level than malloc as it is typed and takes a number of elements rather than a number of bytes
+    template <typename T>
+    T *alloc(size_t nb_elements)
     {
-        T* output_ptr = NULL;
-        const cudaError errorCode = this->malloc((void**)&output_ptr, size*sizeof(T));
+        T *output_ptr = NULL;
+        const cudaError errorCode = this->malloc((void **)&output_ptr, nb_elements * sizeof(T));
         checkCudaErrorCode(errorCode, "GPU_memory_pool_t::alloc");
         return output_ptr;
     }
 
     // allocates gpu memory and returns a pointer to the memory after having copied the data there
-    template<typename T>
-    T* toDevice(T* data, size_t nbElements)
+    // stores the cpu_ptr for a potential later transfer back
+    template <typename T>
+    T *toDevice(T *data, size_t nb_elements)
     {
         // memory allocation
-        void* data_gpu = NULL;
-        const cudaError errorCodeMalloc = this->malloc(&data_gpu, nbElements*sizeof(T));
+        void *data_gpu = NULL;
+        const cudaError errorCodeMalloc = this->malloc(&data_gpu, nb_elements * sizeof(T), data);
         checkCudaErrorCode(errorCodeMalloc, "GPU_memory_pool_t::toDevice (malloc)");
         // data transfer
-        const cudaError errorCodeMemcpy = cudaMemcpy(data_gpu, data, nbElements*sizeof(T), cudaMemcpyHostToDevice);
+        const cudaError errorCodeMemcpy = cudaMemcpy(data_gpu, data, nb_elements * sizeof(T), cudaMemcpyHostToDevice);
         checkCudaErrorCode(errorCodeMemcpy, "GPU_memory_pool_t::toDevice (memcpy)");
-        return static_cast<T*>(data_gpu);
+        return static_cast<T *>(data_gpu);
     }
 
-    // gets data back from GPU and deallocates gpu memory
-    template<typename T>
-    void fromDevice(T* data_cpu, T* data_gpu, size_t nbElements)
+    // gets the given number of elements back from GPU
+    // put them in the given cpu memory
+    // deallocates gpu memory
+    template <typename T>
+    void fromDevice(T *data_cpu, T *data_gpu, size_t nb_elements)
     {
         // data transfer
-        const cudaError errorCodeMemcpy = cudaMemcpy(data_cpu, data_gpu, nbElements*sizeof(T), cudaMemcpyDeviceToHost);
+        const cudaError errorCodeMemcpy = cudaMemcpy(data_cpu, data_gpu, nb_elements * sizeof(T), cudaMemcpyDeviceToHost);
+        checkCudaErrorCode(errorCodeMemcpy, "GPU_memory_pool_t::fromDevice (memcpy)");
+        // deallocation
+        this->free(data_gpu);
+    }
+
+    // gets data back from GPU and into the pointer whence it came from
+    // deallocates gpu memory
+    template <typename T>
+    void fromDevice(T *data_cpu)
+    {
+        // gets index of cpu_ptr in blocks, starting from the end
+        int i = blocks.size() - 1;
+        while (blocks[i].cpu_ptr != data_cpu)
+        {
+            i--;
+        }
+        // extract the gpu_ptr and the size of the allocation in bytes
+        T *data_gpu = blocks[i].start;
+        const size_t size = blocks[i].size_bytes;
+
+        // data transfer
+        const cudaError errorCodeMemcpy = cudaMemcpy(data_cpu, data_gpu, size, cudaMemcpyDeviceToHost);
         checkCudaErrorCode(errorCodeMemcpy, "GPU_memory_pool_t::fromDevice (memcpy)");
         // deallocation
         this->free(data_gpu);
@@ -100,5 +134,5 @@ public:
 // global variable (one instance per thread) containing the pool
 extern GPU_memory_pool_t GPU_memory_pool;
 
-#endif //HAVE_CUDALIBS
-#endif //TOAST_GPU_HELPER_H
+#endif // HAVE_CUDALIBS
+#endif // TOAST_GPU_HELPER_H
--- src/libtoast/src/toast_gpu_helpers.cpp
+++ src/libtoast/src/toast_gpu_helpers.cpp
@@ -12,13 +12,14 @@
 // ERROR CODE CHECKING
 
 // displays an error message if the computation did not end in success
-void checkCudaErrorCode(const cudaError errorCode, const std::string& functionName)
+void checkCudaErrorCode(const cudaError errorCode, const std::string &functionName)
 {
     if (errorCode != cudaSuccess)
     {
         auto log = toast::Logger::get();
         std::string msg = "The CUDA Runtime threw a '" + std::string(cudaGetErrorString(errorCode)) + "' error code";
-        if (functionName != "unknown") msg += " in function '" + functionName + "'.";
+        if (functionName != "unknown")
+            msg += " in function '" + functionName + "'.";
         log.error(msg.c_str());
         throw std::runtime_error(msg.c_str());
     }
@@ -27,31 +28,42 @@ void checkCudaErrorCode(const cudaError errorCode, const std::string& functionNa
 // turns an error code into human readable text
 std::string cublasGetErrorString(const cublasStatus_t errorCode)
 {
-    switch(errorCode)
+    switch (errorCode)
     {
-        case CUBLAS_STATUS_SUCCESS: return "CUBLAS_STATUS_SUCCESS";
-        case CUBLAS_STATUS_NOT_INITIALIZED: return "CUBLAS_STATUS_NOT_INITIALIZED";
-        case CUBLAS_STATUS_ALLOC_FAILED: return "CUBLAS_STATUS_ALLOC_FAILED";
-        case CUBLAS_STATUS_INVALID_VALUE: return "CUBLAS_STATUS_INVALID_VALUE";
-        case CUBLAS_STATUS_ARCH_MISMATCH: return "CUBLAS_STATUS_ARCH_MISMATCH";
-        case CUBLAS_STATUS_LICENSE_ERROR: return "CUBLAS_STATUS_LICENSE_ERROR";
-        case CUBLAS_STATUS_MAPPING_ERROR: return "CUBLAS_STATUS_MAPPING_ERROR";
-        case CUBLAS_STATUS_NOT_SUPPORTED: return "CUBLAS_STATUS_NOT_SUPPORTED";
-        case CUBLAS_STATUS_EXECUTION_FAILED: return "CUBLAS_STATUS_EXECUTION_FAILED";
-        case CUBLAS_STATUS_INTERNAL_ERROR: return "CUBLAS_STATUS_INTERNAL_ERROR";
+    case CUBLAS_STATUS_SUCCESS:
+        return "CUBLAS_STATUS_SUCCESS";
+    case CUBLAS_STATUS_NOT_INITIALIZED:
+        return "CUBLAS_STATUS_NOT_INITIALIZED";
+    case CUBLAS_STATUS_ALLOC_FAILED:
+        return "CUBLAS_STATUS_ALLOC_FAILED";
+    case CUBLAS_STATUS_INVALID_VALUE:
+        return "CUBLAS_STATUS_INVALID_VALUE";
+    case CUBLAS_STATUS_ARCH_MISMATCH:
+        return "CUBLAS_STATUS_ARCH_MISMATCH";
+    case CUBLAS_STATUS_LICENSE_ERROR:
+        return "CUBLAS_STATUS_LICENSE_ERROR";
+    case CUBLAS_STATUS_MAPPING_ERROR:
+        return "CUBLAS_STATUS_MAPPING_ERROR";
+    case CUBLAS_STATUS_NOT_SUPPORTED:
+        return "CUBLAS_STATUS_NOT_SUPPORTED";
+    case CUBLAS_STATUS_EXECUTION_FAILED:
+        return "CUBLAS_STATUS_EXECUTION_FAILED";
+    case CUBLAS_STATUS_INTERNAL_ERROR:
+        return "CUBLAS_STATUS_INTERNAL_ERROR";
     }
 
     return "unknown";
 }
 
 // displays an error message if the computation did not end in sucess
-void checkCublasErrorCode(const cublasStatus_t errorCode, const std::string& functionName)
+void checkCublasErrorCode(const cublasStatus_t errorCode, const std::string &functionName)
 {
-    if(errorCode != CUBLAS_STATUS_SUCCESS)
+    if (errorCode != CUBLAS_STATUS_SUCCESS)
     {
         auto log = toast::Logger::get();
         std::string msg = "CUBLAS threw a '" + cublasGetErrorString(errorCode) + "' error code";
-        if (functionName != "unknown") msg += " in function '" + functionName + "'.";
+        if (functionName != "unknown")
+            msg += " in function '" + functionName + "'.";
         log.error(msg.c_str());
         throw std::runtime_error(msg.c_str());
     }
@@ -60,46 +72,72 @@ void checkCublasErrorCode(const cublasStatus_t errorCode, const std::string& fun
 // turns an error code into human readable text
 std::string cusolverGetErrorString(const cusolverStatus_t errorCode)
 {
-    switch(errorCode)
+    switch (errorCode)
     {
-        case CUSOLVER_STATUS_SUCCESS: return "CUSOLVER_STATUS_SUCCESS";
-        case CUSOLVER_STATUS_NOT_INITIALIZED: return "CUSOLVER_STATUS_NOT_INITIALIZED";
-        case CUSOLVER_STATUS_ALLOC_FAILED: return "CUSOLVER_STATUS_ALLOC_FAILED";
-        case CUSOLVER_STATUS_INVALID_VALUE: return "CUSOLVER_STATUS_INVALID_VALUE";
-        case CUSOLVER_STATUS_ARCH_MISMATCH: return "CUSOLVER_STATUS_ARCH_MISMATCH";
-        case CUSOLVER_STATUS_MAPPING_ERROR: return "CUSOLVER_STATUS_MAPPING_ERROR";
-        case CUSOLVER_STATUS_EXECUTION_FAILED: return "CUSOLVER_STATUS_EXECUTION_FAILED";
-        case CUSOLVER_STATUS_INTERNAL_ERROR: return "CUSOLVER_STATUS_INTERNAL_ERROR";
-        case CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED: return "CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED";
-        case CUSOLVER_STATUS_NOT_SUPPORTED: return "CUSOLVER_STATUS_NOT_SUPPORTED";
-        case CUSOLVER_STATUS_ZERO_PIVOT: return "CUSOLVER_STATUS_ZERO_PIVOT";
-        case CUSOLVER_STATUS_INVALID_LICENSE: return "CUSOLVER_STATUS_INVALID_LICENSE";
-        case CUSOLVER_STATUS_IRS_PARAMS_NOT_INITIALIZED: return "CUSOLVER_STATUS_IRS_PARAMS_NOT_INITIALIZED";
-        case CUSOLVER_STATUS_IRS_PARAMS_INVALID: return "CUSOLVER_STATUS_IRS_PARAMS_INVALID";
-        case CUSOLVER_STATUS_IRS_PARAMS_INVALID_PREC: return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_PREC";
-        case CUSOLVER_STATUS_IRS_PARAMS_INVALID_REFINE: return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_REFINE";
-        case CUSOLVER_STATUS_IRS_PARAMS_INVALID_MAXITER: return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_MAXITER";
-        case CUSOLVER_STATUS_IRS_INTERNAL_ERROR: return "CUSOLVER_STATUS_IRS_INTERNAL_ERROR";
-        case CUSOLVER_STATUS_IRS_NOT_SUPPORTED: return "CUSOLVER_STATUS_IRS_NOT_SUPPORTED";
-        case CUSOLVER_STATUS_IRS_OUT_OF_RANGE: return "CUSOLVER_STATUS_IRS_OUT_OF_RANGE";
-        case CUSOLVER_STATUS_IRS_NRHS_NOT_SUPPORTED_FOR_REFINE_GMRES: return "CUSOLVER_STATUS_IRS_NRHS_NOT_SUPPORTED_FOR_REFINE_GMRES";
-        case CUSOLVER_STATUS_IRS_INFOS_NOT_INITIALIZED: return "CUSOLVER_STATUS_IRS_INFOS_NOT_INITIALIZED";
-        case CUSOLVER_STATUS_IRS_INFOS_NOT_DESTROYED: return "CUSOLVER_STATUS_IRS_INFOS_NOT_DESTROYED";
-        case CUSOLVER_STATUS_IRS_MATRIX_SINGULAR: return "CUSOLVER_STATUS_IRS_MATRIX_SINGULAR";
-        case CUSOLVER_STATUS_INVALID_WORKSPACE: return "CUSOLVER_STATUS_INVALID_WORKSPACE";
+    case CUSOLVER_STATUS_SUCCESS:
+        return "CUSOLVER_STATUS_SUCCESS";
+    case CUSOLVER_STATUS_NOT_INITIALIZED:
+        return "CUSOLVER_STATUS_NOT_INITIALIZED";
+    case CUSOLVER_STATUS_ALLOC_FAILED:
+        return "CUSOLVER_STATUS_ALLOC_FAILED";
+    case CUSOLVER_STATUS_INVALID_VALUE:
+        return "CUSOLVER_STATUS_INVALID_VALUE";
+    case CUSOLVER_STATUS_ARCH_MISMATCH:
+        return "CUSOLVER_STATUS_ARCH_MISMATCH";
+    case CUSOLVER_STATUS_MAPPING_ERROR:
+        return "CUSOLVER_STATUS_MAPPING_ERROR";
+    case CUSOLVER_STATUS_EXECUTION_FAILED:
+        return "CUSOLVER_STATUS_EXECUTION_FAILED";
+    case CUSOLVER_STATUS_INTERNAL_ERROR:
+        return "CUSOLVER_STATUS_INTERNAL_ERROR";
+    case CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED:
+        return "CUSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED";
+    case CUSOLVER_STATUS_NOT_SUPPORTED:
+        return "CUSOLVER_STATUS_NOT_SUPPORTED";
+    case CUSOLVER_STATUS_ZERO_PIVOT:
+        return "CUSOLVER_STATUS_ZERO_PIVOT";
+    case CUSOLVER_STATUS_INVALID_LICENSE:
+        return "CUSOLVER_STATUS_INVALID_LICENSE";
+    case CUSOLVER_STATUS_IRS_PARAMS_NOT_INITIALIZED:
+        return "CUSOLVER_STATUS_IRS_PARAMS_NOT_INITIALIZED";
+    case CUSOLVER_STATUS_IRS_PARAMS_INVALID:
+        return "CUSOLVER_STATUS_IRS_PARAMS_INVALID";
+    case CUSOLVER_STATUS_IRS_PARAMS_INVALID_PREC:
+        return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_PREC";
+    case CUSOLVER_STATUS_IRS_PARAMS_INVALID_REFINE:
+        return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_REFINE";
+    case CUSOLVER_STATUS_IRS_PARAMS_INVALID_MAXITER:
+        return "CUSOLVER_STATUS_IRS_PARAMS_INVALID_MAXITER";
+    case CUSOLVER_STATUS_IRS_INTERNAL_ERROR:
+        return "CUSOLVER_STATUS_IRS_INTERNAL_ERROR";
+    case CUSOLVER_STATUS_IRS_NOT_SUPPORTED:
+        return "CUSOLVER_STATUS_IRS_NOT_SUPPORTED";
+    case CUSOLVER_STATUS_IRS_OUT_OF_RANGE:
+        return "CUSOLVER_STATUS_IRS_OUT_OF_RANGE";
+    case CUSOLVER_STATUS_IRS_NRHS_NOT_SUPPORTED_FOR_REFINE_GMRES:
+        return "CUSOLVER_STATUS_IRS_NRHS_NOT_SUPPORTED_FOR_REFINE_GMRES";
+    case CUSOLVER_STATUS_IRS_INFOS_NOT_INITIALIZED:
+        return "CUSOLVER_STATUS_IRS_INFOS_NOT_INITIALIZED";
+    case CUSOLVER_STATUS_IRS_INFOS_NOT_DESTROYED:
+        return "CUSOLVER_STATUS_IRS_INFOS_NOT_DESTROYED";
+    case CUSOLVER_STATUS_IRS_MATRIX_SINGULAR:
+        return "CUSOLVER_STATUS_IRS_MATRIX_SINGULAR";
+    case CUSOLVER_STATUS_INVALID_WORKSPACE:
+        return "CUSOLVER_STATUS_INVALID_WORKSPACE";
     }
 
     return "unknown";
 }
 
 // displays an error message if the computation did not end in sucess
-void checkCusolverErrorCode(const cusolverStatus_t errorCode, const std::string& functionName)
+void checkCusolverErrorCode(const cusolverStatus_t errorCode, const std::string &functionName)
 {
-    if(errorCode != CUSOLVER_STATUS_SUCCESS)
+    if (errorCode != CUSOLVER_STATUS_SUCCESS)
     {
         auto log = toast::Logger::get();
         std::string msg = "CUSOLVER threw a '" + cusolverGetErrorString(errorCode) + "' error code";
-        if (functionName != "unknown") msg += " in function '" + functionName + "'.";
+        if (functionName != "unknown")
+            msg += " in function '" + functionName + "'.";
         log.error(msg.c_str());
         throw std::runtime_error(msg.c_str());
     }
@@ -110,36 +148,54 @@ std::string cufftGetErrorString(cufftResult error)
 {
     switch (error)
     {
-        case CUFFT_SUCCESS: return "CUFFT_SUCCESS";
-        case CUFFT_INVALID_PLAN: return "CUFFT_INVALID_PLAN";
-        case CUFFT_ALLOC_FAILED: return "CUFFT_ALLOC_FAILED";
-        case CUFFT_INVALID_TYPE: return "CUFFT_INVALID_TYPE";
-        case CUFFT_INVALID_VALUE: return "CUFFT_INVALID_VALUE";
-        case CUFFT_INTERNAL_ERROR: return "CUFFT_INTERNAL_ERROR";
-        case CUFFT_EXEC_FAILED: return "CUFFT_EXEC_FAILED";
-        case CUFFT_SETUP_FAILED: return "CUFFT_SETUP_FAILED";
-        case CUFFT_INVALID_SIZE: return "CUFFT_INVALID_SIZE";
-        case CUFFT_UNALIGNED_DATA: return "CUFFT_UNALIGNED_DATA";
-        case CUFFT_INCOMPLETE_PARAMETER_LIST: return "CUFFT_INCOMPLETE_PARAMETER_LIST";
-        case CUFFT_INVALID_DEVICE: return "CUFFT_INVALID_DEVICE";
-        case CUFFT_PARSE_ERROR: return "CUFFT_PARSE_ERROR";
-        case CUFFT_NO_WORKSPACE: return "CUFFT_NO_WORKSPACE";
-        case CUFFT_NOT_IMPLEMENTED: return "CUFFT_NOT_IMPLEMENTED";
-        case CUFFT_LICENSE_ERROR: return "CUFFT_LICENSE_ERROR";
-        case CUFFT_NOT_SUPPORTED: return "CUFFT_NOT_SUPPORTED";
+    case CUFFT_SUCCESS:
+        return "CUFFT_SUCCESS";
+    case CUFFT_INVALID_PLAN:
+        return "CUFFT_INVALID_PLAN";
+    case CUFFT_ALLOC_FAILED:
+        return "CUFFT_ALLOC_FAILED";
+    case CUFFT_INVALID_TYPE:
+        return "CUFFT_INVALID_TYPE";
+    case CUFFT_INVALID_VALUE:
+        return "CUFFT_INVALID_VALUE";
+    case CUFFT_INTERNAL_ERROR:
+        return "CUFFT_INTERNAL_ERROR";
+    case CUFFT_EXEC_FAILED:
+        return "CUFFT_EXEC_FAILED";
+    case CUFFT_SETUP_FAILED:
+        return "CUFFT_SETUP_FAILED";
+    case CUFFT_INVALID_SIZE:
+        return "CUFFT_INVALID_SIZE";
+    case CUFFT_UNALIGNED_DATA:
+        return "CUFFT_UNALIGNED_DATA";
+    case CUFFT_INCOMPLETE_PARAMETER_LIST:
+        return "CUFFT_INCOMPLETE_PARAMETER_LIST";
+    case CUFFT_INVALID_DEVICE:
+        return "CUFFT_INVALID_DEVICE";
+    case CUFFT_PARSE_ERROR:
+        return "CUFFT_PARSE_ERROR";
+    case CUFFT_NO_WORKSPACE:
+        return "CUFFT_NO_WORKSPACE";
+    case CUFFT_NOT_IMPLEMENTED:
+        return "CUFFT_NOT_IMPLEMENTED";
+    case CUFFT_LICENSE_ERROR:
+        return "CUFFT_LICENSE_ERROR";
+    case CUFFT_NOT_SUPPORTED:
+        return "CUFFT_NOT_SUPPORTED";
     }
 
     return "unknown";
 }
 
 // displays an error message if the computation did not end in sucess
-void checkCufftErrorCode(const cufftResult errorCode, const std::string& functionName)
+void checkCufftErrorCode(const cufftResult errorCode, const std::string &functionName)
 {
-    if(errorCode != CUFFT_SUCCESS)
+    if (errorCode != CUFFT_SUCCESS)
     {
         auto log = toast::Logger::get();
         std::string msg = "CUFFT threw a '" + cufftGetErrorString(errorCode) + "' error code";
-        if (functionName != "unknown") msg += " in function '" + functionName + "'.";
+        if (functionName != "unknown")
+            msg += " in function '" + functionName + "'.";
         log.error(msg.c_str());
         throw std::runtime_error(msg.c_str());
     }
@@ -151,20 +207,29 @@ void checkCufftErrorCode(const cufftResult errorCode, const std::string& functio
 // constant used to make sure allocations are aligned
 const int ALIGNEMENT_SIZE = 512;
 
-GPU_memory_block_t::GPU_memory_block_t(void* ptr, size_t size)
+// creating a new `GPU_memory_block_t`
+// `cpu_ptr` is an, optional, pointer to the cpu memory that was moved into the block
+GPU_memory_block_t::GPU_memory_block_t(void *gpu_ptr, size_t size_bytes_arg, void *cpu_ptr_arg)
 {
+    // stores size of the allocation, in bytes
+    size_bytes = size_bytes_arg;
     // align size with 512
-    if(size % ALIGNEMENT_SIZE != 0) size += ALIGNEMENT_SIZE - (size % ALIGNEMENT_SIZE);
-    start = ptr;
+    if (size % ALIGNEMENT_SIZE != 0)
+        size += ALIGNEMENT_SIZE - (size % ALIGNEMENT_SIZE);
+    // defines the start and end gpu pointers
+    start = gpu_ptr;
     end = static_cast<char *>(start) + size;
+    // the block starts allocated (and not freed)
     isFree = false;
+    // stores cpu pointer (or nullptr)
+    cpu_ptr = cpu_ptr_arg;
 }
 
 // constructor, does the initial allocation
-GPU_memory_pool_t::GPU_memory_pool_t(size_t bytesPreallocated): available_memory(bytesPreallocated), blocks()
+GPU_memory_pool_t::GPU_memory_pool_t(size_t bytesPreallocated) : available_memory_bytes(bytesPreallocated), blocks()
 {
     // allocates the memory
-    const cudaError errorCode = cudaMalloc(&start, available_memory);
+    const cudaError errorCode = cudaMalloc(&start, available_memory_bytes);
     checkCudaErrorCode(errorCode, "GPU memory pre-allocation");
     // first block to mark the starting point
     GPU_memory_block_t initialBlock(start, 0);
@@ -197,23 +262,24 @@ GPU_memory_pool_t::~GPU_memory_pool_t()
 }
 
 // allocates memory starting from the end of the latest block
-cudaError GPU_memory_pool_t::malloc(void** output_ptr, size_t size)
+// input_ptr is an optional pointer to the cpu memory from which the data store into memory might come from
+cudaError GPU_memory_pool_t::malloc(void **output_ptr, size_t size_bytes, void *input_ptr)
 {
     // insure two threads cannot interfere
     const std::lock_guard<std::mutex> lock(alloc_mutex);
 
     // builds a block starting at the end of the existing blocks
     *output_ptr = blocks.back().end;
-    const GPU_memory_block_t memoryBlock(*output_ptr, size);
+    const GPU_memory_block_t memoryBlock(*output_ptr, size_bytes, input_ptr);
 
     // errors out if the allocation goes beyond the preallocated memory
     const size_t usedMemory = static_cast<char *>(memoryBlock.end) - static_cast<char *>(start);
-    if(usedMemory > available_memory)
+    if (usedMemory > available_memory_bytes)
     {
         std::cerr << "INSUFICIENT GPU MEMORY PREALOCATION"
                   << " memory that will be allocated:" << usedMemory
-                  << " total memory available:" << available_memory
-                  << " size requested:" << size << std::endl;
+                  << " total memory available:" << available_memory_bytes
+                  << " size requested:" << size_bytes << std::endl;
         *output_ptr = NULL;
         return cudaErrorMemoryAllocation;
     }
@@ -224,14 +290,14 @@ cudaError GPU_memory_pool_t::malloc(void** output_ptr, size_t size)
 }
 
 // frees memory by releasing the block
-void GPU_memory_pool_t::free(void* ptr)
+void GPU_memory_pool_t::free(void *gpu_ptr)
 {
     // insure two threads cannot interfere
     const std::lock_guard<std::mutex> lock(alloc_mutex);
 
     // gets index of ptr in block, starting from the end
     int i = blocks.size() - 1;
-    while(blocks[i].start != ptr)
+    while (blocks[i].start != gpu_ptr)
     {
         i--;
     }
@@ -240,9 +306,9 @@ void GPU_memory_pool_t::free(void* ptr)
     blocks[i].isFree = true;
 
     // if ptr was the last elements, frees a maximum of elements
-    if(i == blocks.size() - 1)
+    if (i == blocks.size() - 1)
     {
-        while(blocks.back().isFree)
+        while (blocks.back().isFree)
         {
             blocks.pop_back();
         }
