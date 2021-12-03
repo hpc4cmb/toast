2b8f7dedfdb10b3884f63b7100ca7bc572f4c2d2
Theodore Kisner <tskisner.public@gmail.com>
2021-12-02 18:01:59 -0800

    Disable CUDA by default

--- platforms/cori-gpu-nvhpc.sh
+++ platforms/cori-gpu-nvhpc.sh
@@ -8,6 +8,8 @@ cmake \
     -DCMAKE_C_FLAGS="-O3 -g -fPIC -pthread" \
     -DCMAKE_CXX_FLAGS="-O3 -g -fPIC -pthread -std=c++11" \
     -DMKL_DISABLED=TRUE \
+    -DUSE_CUDA=TRUE \
+    -DUSE_OPENACC=TRUE \
     -DPYTHON_EXECUTABLE:FILEPATH=$(which python3) \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
     -DBLAS_LIBRARIES="-lopenblas -lgomp -lm" \
