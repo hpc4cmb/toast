dbb0d68b154c4d90c43b63b74560f862d72c0f00
nestor <ndemeure@lbl.gov>
2022-02-23 15:47:45 -0800

    all operators set to jax as their default

--- src/toast/ops/jax_ops/__init__.py
+++ src/toast/ops/jax_ops/__init__.py
@@ -14,11 +14,11 @@ from .template_offset import template_offset_add_to_signal, template_offset_proj
 
 # dummy call to warm-up the jit for further JAX compilations
 # NOTE: we could remove this, it makes a 1 or 2s difference to the full runtime
-import numpy as np
-from .polyfilter1D import filter_polynomial_interval as jit_warmup_function
-dummy_order = 1
-dummy_scanlen = 2
-dummy_nsignal = 1
-dummy_flags = np.zeros(shape=(dummy_scanlen,))
-dummy_interval = np.zeros(shape=(dummy_scanlen,dummy_nsignal))
-jit_warmup_function(dummy_flags, dummy_interval, dummy_order)
+#import numpy as np
+#from .polyfilter1D import filter_polynomial_interval as jit_warmup_function
+#dummy_order = 1
+#dummy_scanlen = 2
+#dummy_nsignal = 1
+#dummy_flags = np.zeros(shape=(dummy_scanlen,))
+#dummy_interval = np.zeros(shape=(dummy_scanlen,dummy_nsignal))
+#jit_warmup_function(dummy_flags, dummy_interval, dummy_order)
