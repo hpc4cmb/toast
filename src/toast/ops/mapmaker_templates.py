Fix flagging and masking

src/toast/ops/mapmaker_templates.py
@@ -65,11 +65,11 @@ class TemplateMatrix(Operator):
         None, allow_none=True, help="Observation detdata key for the timestream data"
     )
 
-    flags = Unicode(
+    det_flags = Unicode(
         None, allow_none=True, help="Observation detdata key for solver flags to use"
     )
 
-    flag_mask = Int(0, help="Bit mask value for solver flags")
+    det_flag_mask = Int(0, help="Bit mask value for solver flags")
 
     @traitlets.validate("templates")
     def _check_templates(self, proposal):
@@ -104,8 +104,8 @@ class TemplateMatrix(Operator):
             transpose=self.transpose,
             view=self.view,
             det_data=self.det_data,
-            flags=self.flags,
-            flag_mask=self.flag_mask,
+            det_flags=self.det_flags,
+            det_flag_mask=self.det_flag_mask,
         )
         ret._initialized = self._initialized
         return ret
@@ -179,8 +179,8 @@ class TemplateMatrix(Operator):
         if not self._initialized:
             for tmpl in self.templates:
                 tmpl.view = self.view
-                tmpl.flags = self.flags
-                tmpl.flag_mask = self.flag_mask
+                tmpl.det_flags = self.det_flags
+                tmpl.det_flag_mask = self.det_flag_mask
                 # This next line will trigger calculation of the number
                 # of amplitudes within each template.
                 tmpl.data = data
@@ -606,8 +606,8 @@ class SolveAmplitudes(Operator):
             data[self.solver_rcond_mask_name] = PixelData(
                 data[self.binning.pixel_dist], dtype=np.uint8, n_value=1
             )
-            data[self.solver_rcond_mask_name].raw[
-                data[self.solver_rcond_name].raw.array() < self.solve_rcond_threshold
+            data[self.solver_rcond_mask_name].data[
+                data[self.solver_rcond_name].data < self.solve_rcond_threshold
             ] = 1
 
             memreport.prefix = "After constructing rcond mask"
@@ -664,6 +664,11 @@ class SolveAmplitudes(Operator):
             comm=comm,
         )
 
+        # First application of the template matrix will propagate flags and
+        # the flag mask to the templates
+        self.template_matrix.det_flags = self.solver_flags
+        self.template_matrix.det_flag_mask = 255
+
         # Set our binning operator to use only our new solver flags
         self.binning.shared_flag_mask = 0
         self.binning.det_flags = self.solver_flags
@@ -676,7 +681,7 @@ class SolveAmplitudes(Operator):
 
         self.template_matrix.amplitudes = self.solver_rhs
         rhs_calc = SolverRHS(
-            name="{}_rhs".format(self.name),
+            name=f"{self.name}_rhs",
             det_data=self.det_data,
             overwrite=False,
             binning=self.binning,