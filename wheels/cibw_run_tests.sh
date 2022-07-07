de0007f741ba11b99ff435521f22abce15bd173a
Theodore Kisner <tskisner.public@gmail.com>
2022-07-07 11:22:20 -0700

    Test building wheels with homebrew gcc on macos

--- wheels/cibw_run_tests.sh
+++ wheels/cibw_run_tests.sh
@@ -3,9 +3,11 @@
 #set -e
 
 echo "======================================================================="
-echo "Upgrade numpy / scipy to latest versions for runtime tests"
+echo "Running serial tests with MPI disabled"
 echo "======================================================================="
-pip install --upgrade numpy scipy
+com="MPI_DISABLE=1 python -c 'import toast.tests; toast.tests.run()'"
+echo ${com}
+eval ${com}
 
 echo "======================================================================="
 echo "Running MPI tests with $(which mpirun)"
@@ -15,12 +17,5 @@ echo "*** one process.  So we are not really testing much.  Change this to"
 echo "*** two processes once it works."
 echo "======================================================================="
 com="mpirun -np 1 python -c 'import toast.tests; toast.tests.run()'"
-echo ${com}
-eval ${com}
-
-echo "======================================================================="
-echo "Running serial tests with MPI disabled"
-echo "======================================================================="
-com="MPI_DISABLE=1 python -c 'import toast.tests; toast.tests.run()'"
 echo ${com}
 eval ${com}
--- wheels/install_deps_osx.sh
+++ wheels/install_deps_osx.sh
@@ -11,10 +11,6 @@ set -e
 
 arch=$1
 
-# If this arg is set to any value, do not install MPI or atmosphere
-# tools.  This is used when running quick unit tests on every PR.
-quick=$2
-
 # Cross compile option needed for autoconf builds.
 cross=""
 if [ "${arch}" = "macosx_arm64" ]; then
@@ -27,10 +23,12 @@ scriptdir=$(pwd)
 popd >/dev/null 2>&1
 echo "Wheel script directory = ${scriptdir}"
 
-# Build options.  If we use clang, then use accelerate framework.  Otherwise
-# build and use OpenBLAS.
+# Build options.  If we use clang, then use accelerate framework.  Note that
+# the Accelerate framework has known bugs that Apple refuses to fix.  Numpy and Scipy
+# have both abandoned Accelerate.  Use that here with caution.  Default uses
+# homebrew gcc to build OpenBLAS with OpenMP enabled.
 
-use_gcc=no
+use_gcc=yes
 
 if [ "x${use_gcc}" = "xyes" ]; then
     CC=gcc-11
@@ -57,56 +55,9 @@ MAKEJ=2
 
 PREFIX=/usr/local
 
-# Install OS packages with homebrew
-
-# Optionally install gcc
+# Optionally install gcc if we are using it
 if [ "x${use_gcc}" = "xyes" ]; then
     brew install gcc
-fi
-
-# NOTE:  this is disabled, since the homebrew version of
-# mpich requires gcc, and we might be building with clang.
-#brew install mpich
-
-# Install MPICH with our serial compiler.  Needed to install
-# mpi4py later for testing (MPI is not bundled with the wheel).
-# NOTE:  on arm64 we cannot run tests anyway (since we are
-# cross compiling).  So do not attempt to build mpich / mpi4py.
-
-if [ "${arch}" != "macosx_arm64" ] && [ "x${quick}" = "x" ]; then
-    mpich_version=3.4
-    mpich_dir=mpich-${mpich_version}
-    mpich_pkg=${mpich_dir}.tar.gz
-
-    echo "Fetching MPICH..."
-
-    if [ ! -e ${mpich_pkg} ]; then
-        curl -SL https://www.mpich.org/static/downloads/${mpich_version}/${mpich_pkg} -o ${mpich_pkg}
-    fi
-
-    echo "Building MPICH..."
-
-    rm -rf ${mpich_dir}
-    fcopt="--enable-fortran=all"
-    if [ "x${FC}" = "x" ]; then
-        fcopt="--disable-fortran"
-    fi
-    unset F90
-    unset F90FLAGS
-    tar xzf ${mpich_pkg} \
-        && cd ${mpich_dir} \
-        && CC="${CC}" CXX="${CXX}" FC="${FC}" F77="${FC}" \
-        CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" \
-        FFLAGS="${FCFLAGS}" FCFLAGS="${FCFLAGS}" \
-        MPICH_MPICC_CFLAGS="${CFLAGS}" \
-        MPICH_MPICXX_CXXFLAGS="${CXXFLAGS}" \
-        MPICH_MPIF77_FFLAGS="${FCFLAGS}" \
-        MPICH_MPIFORT_FCFLAGS="${FCFLAGS}" \
-        ./configure ${fcopt} \
-        --with-device=ch3 \
-        --prefix="${PREFIX}" \
-        && make -j ${MAKEJ} \
-        && make install
 fi
 
 # Update pip
@@ -133,15 +84,6 @@ fi
 
 # Install build requirements.
 CC="${CC}" CFLAGS="${CFLAGS}" pip install -v "numpy<${numpy_ver}" -r "${scriptdir}/build_requirements.txt"
-
-# Install mpi4py for running tests (only on x86_64)
-
-if [ "${arch}" != "macosx_arm64" ] && [ "x${quick}" = "x" ]; then
-    echo "mpicc = $(which mpicc)"
-    echo "mpicxx = $(which mpicxx)"
-    echo "mpirun = $(which mpirun)"
-    pip install mpi4py
-fi
 
 # Optionally Install Openblas
 
@@ -201,129 +143,184 @@ tar xzf ${fftw_pkg} \
     && make install \
     && popd >/dev/null 2>&1
 
-if [ "x${quick}" = "x" ]; then
-    # libgmp
+# libgmp
 
-    gmp_version=6.2.1
-    gmp_dir=gmp-${gmp_version}
-    gmp_pkg=${gmp_dir}.tar.xz
+gmp_version=6.2.1
+gmp_dir=gmp-${gmp_version}
+gmp_pkg=${gmp_dir}.tar.xz
 
-    echo "Fetching libgmp"
+echo "Fetching libgmp"
 
-    if [ ! -e ${gmp_pkg} ]; then
-        curl -SL https://ftp.gnu.org/gnu/gmp/${gmp_pkg} -o ${gmp_pkg}
+if [ ! -e ${gmp_pkg} ]; then
+    curl -SL https://ftp.gnu.org/gnu/gmp/${gmp_pkg} -o ${gmp_pkg}
+fi
+
+echo "Building libgmp..."
+
+rm -rf ${gmp_dir}
+tar xf ${gmp_pkg} \
+    && pushd ${gmp_dir} >/dev/null 2>&1 \
+    && CC="${CC}" CFLAGS="${CFLAGS}" \
+    && CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
+    ./configure ${cross} \
+    --enable-static \
+    --disable-shared \
+    --with-pic \
+    --prefix="${PREFIX}" \
+    && make -j ${MAKEJ} \
+    && make install \
+    && popd >/dev/null 2>&1
+
+# libmpfr
+
+mpfr_version=4.1.0
+mpfr_dir=mpfr-${mpfr_version}
+mpfr_pkg=${mpfr_dir}.tar.xz
+
+echo "Fetching libmpfr"
+
+if [ ! -e ${mpfr_pkg} ]; then
+    curl -SL https://www.mpfr.org/mpfr-current/${mpfr_pkg} -o ${mpfr_pkg}
+fi
+
+echo "Building libmpfr..."
+
+rm -rf ${mpfr_dir}
+tar xf ${mpfr_pkg} \
+    && pushd ${mpfr_dir} >/dev/null 2>&1 \
+    && CC="${CC}" CFLAGS="${CFLAGS}" \
+    ./configure ${cross} \
+    --enable-static \
+    --disable-shared \
+    --with-pic \
+    --with-gmp="${PREFIX}" \
+    --prefix="${PREFIX}" \
+    && make -j ${MAKEJ} \
+    && make install \
+    && popd >/dev/null 2>&1
+
+# Install libaatm
+
+aatm_version=1.0.9
+aatm_dir=libaatm-${aatm_version}
+aatm_pkg=${aatm_dir}.tar.gz
+
+echo "Fetching libaatm..."
+
+if [ ! -e ${aatm_pkg} ]; then
+    curl -SL "https://github.com/hpc4cmb/libaatm/archive/${aatm_version}.tar.gz" -o "${aatm_pkg}"
+fi
+
+echo "Building libaatm..."
+
+rm -rf ${aatm_dir}
+tar xzf ${aatm_pkg} \
+    && pushd ${aatm_dir} >/dev/null 2>&1 \
+    && mkdir -p build \
+    && pushd build >/dev/null 2>&1 \
+    && cmake \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCMAKE_C_FLAGS="${CFLAGS}" \
+    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    .. \
+    && make -j ${MAKEJ} install \
+    && popd >/dev/null 2>&1 \
+    && popd >/dev/null 2>&1
+
+# Install SuiteSparse
+
+ssparse_version=5.11.0
+ssparse_dir=SuiteSparse-${ssparse_version}
+ssparse_pkg=${ssparse_dir}.tar.gz
+
+echo "Fetching SuiteSparse..."
+
+if [ ! -e ${ssparse_pkg} ]; then
+    curl -SL https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v${ssparse_version}.tar.gz -o ${ssparse_pkg}
+fi
+
+echo "Building SuiteSparse..."
+
+blas_opt="-framework Accelerate"
+if [ "x${use_gcc}" = "xyes" ]; then
+    blas_opt="-lopenblas -fopenmp -lm"
+fi
+
+rm -rf ${ssparse_dir}
+tar xzf ${ssparse_pkg} \
+    && pushd ${ssparse_dir} >/dev/null 2>&1 \
+    && patch -p1 < "${scriptdir}/suitesparse.patch" \
+    && make library JOBS=${MAKEJ} \
+    CC="${CC}" CXX="${CXX}" \
+    CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" AUTOCC=no \
+    GPU_CONFIG="" BLAS="${blas_opt}" \
+    && make static JOBS=${MAKEJ} \
+    CC="${CC}" CXX="${CXX}" \
+    CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" AUTOCC=no \
+    GPU_CONFIG="" BLAS="${blas_opt}" \
+    && cp -a ./include/* "${PREFIX}/include/" \
+    && find . -name "*.a" -exec cp -a '{}' "${PREFIX}/lib/" \; \
+    && popd >/dev/null 2>&1
+
+# This line removed from above, since we are linking to static libs:
+# && cp -a ./lib/* "${PREFIX}/lib/" \
+
+# MPI for testing.  Although we do not bundle MPI with toast, we need
+# to install it in order to run mpi-enabled tests on the produced
+# wheel.
+
+if [ "x${use_gcc}" = "xyes" ]; then
+    # The homebrew version of mpich uses gcc too, so we can just
+    # install that.
+    brew install mpich
+else
+    # NOTE:  on arm64 we cannot run tests anyway (since we are
+    # cross compiling).  So do not attempt to build mpich / mpi4py.
+    if [ "${arch}" != "macosx_arm64" ]; then
+        mpich_version=3.4
+        mpich_dir=mpich-${mpich_version}
+        mpich_pkg=${mpich_dir}.tar.gz
+
+        echo "Fetching MPICH..."
+
+        if [ ! -e ${mpich_pkg} ]; then
+            curl -SL https://www.mpich.org/static/downloads/${mpich_version}/${mpich_pkg} -o ${mpich_pkg}
+        fi
+
+        echo "Building MPICH..."
+
+        rm -rf ${mpich_dir}
+        fcopt="--enable-fortran=all"
+        if [ "x${FC}" = "x" ]; then
+            fcopt="--disable-fortran"
+        fi
+        unset F90
+        unset F90FLAGS
+        tar xzf ${mpich_pkg} \
+            && cd ${mpich_dir} \
+            && CC="${CC}" CXX="${CXX}" FC="${FC}" F77="${FC}" \
+            CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" \
+            FFLAGS="${FCFLAGS}" FCFLAGS="${FCFLAGS}" \
+            MPICH_MPICC_CFLAGS="${CFLAGS}" \
+            MPICH_MPICXX_CXXFLAGS="${CXXFLAGS}" \
+            MPICH_MPIF77_FFLAGS="${FCFLAGS}" \
+            MPICH_MPIFORT_FCFLAGS="${FCFLAGS}" \
+            ./configure ${fcopt} \
+            --with-device=ch3 \
+            --prefix="${PREFIX}" \
+            && make -j ${MAKEJ} \
+            && make install
     fi
+fi
 
-    echo "Building libgmp..."
+# Install mpi4py for running tests (only on x86_64)
 
-    rm -rf ${gmp_dir}
-    tar xf ${gmp_pkg} \
-        && pushd ${gmp_dir} >/dev/null 2>&1 \
-        && CC="${CC}" CFLAGS="${CFLAGS}" \
-        && CXX="${CXX}" CXXFLAGS="${CXXFLAGS}" \
-        ./configure ${cross} \
-        --enable-static \
-        --disable-shared \
-        --with-pic \
-        --prefix="${PREFIX}" \
-        && make -j ${MAKEJ} \
-        && make install \
-        && popd >/dev/null 2>&1
-
-    # libmpfr
-
-    mpfr_version=4.1.0
-    mpfr_dir=mpfr-${mpfr_version}
-    mpfr_pkg=${mpfr_dir}.tar.xz
-
-    echo "Fetching libmpfr"
-
-    if [ ! -e ${mpfr_pkg} ]; then
-        curl -SL https://www.mpfr.org/mpfr-current/${mpfr_pkg} -o ${mpfr_pkg}
-    fi
-
-    echo "Building libmpfr..."
-
-    rm -rf ${mpfr_dir}
-    tar xf ${mpfr_pkg} \
-        && pushd ${mpfr_dir} >/dev/null 2>&1 \
-        && CC="${CC}" CFLAGS="${CFLAGS}" \
-        ./configure ${cross} \
-        --enable-static \
-        --disable-shared \
-        --with-pic \
-        --with-gmp="${PREFIX}" \
-        --prefix="${PREFIX}" \
-        && make -j ${MAKEJ} \
-        && make install \
-        && popd >/dev/null 2>&1
-
-    # Install libaatm
-
-    aatm_version=1.0.9
-    aatm_dir=libaatm-${aatm_version}
-    aatm_pkg=${aatm_dir}.tar.gz
-
-    echo "Fetching libaatm..."
-
-    if [ ! -e ${aatm_pkg} ]; then
-        curl -SL "https://github.com/hpc4cmb/libaatm/archive/${aatm_version}.tar.gz" -o "${aatm_pkg}"
-    fi
-
-    echo "Building libaatm..."
-
-    rm -rf ${aatm_dir}
-    tar xzf ${aatm_pkg} \
-        && pushd ${aatm_dir} >/dev/null 2>&1 \
-        && mkdir -p build \
-        && pushd build >/dev/null 2>&1 \
-        && cmake \
-        -DCMAKE_C_COMPILER="${CC}" \
-        -DCMAKE_CXX_COMPILER="${CXX}" \
-        -DCMAKE_C_FLAGS="${CFLAGS}" \
-        -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-        -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        .. \
-        && make -j ${MAKEJ} install \
-        && popd >/dev/null 2>&1 \
-        && popd >/dev/null 2>&1
-
-    # Install SuiteSparse
-
-    ssparse_version=5.11.0
-    ssparse_dir=SuiteSparse-${ssparse_version}
-    ssparse_pkg=${ssparse_dir}.tar.gz
-
-    echo "Fetching SuiteSparse..."
-
-    if [ ! -e ${ssparse_pkg} ]; then
-        curl -SL https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v${ssparse_version}.tar.gz -o ${ssparse_pkg}
-    fi
-
-    echo "Building SuiteSparse..."
-
-    blas_opt="-framework Accelerate"
-    if [ "x${use_gcc}" = "xyes" ]; then
-        blas_opt="-lopenblas -fopenmp -lm"
-    fi
-
-    rm -rf ${ssparse_dir}
-    tar xzf ${ssparse_pkg} \
-        && pushd ${ssparse_dir} >/dev/null 2>&1 \
-        && patch -p1 < "${scriptdir}/suitesparse.patch" \
-        && make library JOBS=${MAKEJ} \
-        CC="${CC}" CXX="${CXX}" \
-        CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" AUTOCC=no \
-        GPU_CONFIG="" BLAS="${blas_opt}" \
-        && make static JOBS=${MAKEJ} \
-        CC="${CC}" CXX="${CXX}" \
-        CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" AUTOCC=no \
-        GPU_CONFIG="" BLAS="${blas_opt}" \
-        && cp -a ./include/* "${PREFIX}/include/" \
-        && find . -name "*.a" -exec cp -a '{}' "${PREFIX}/lib/" \; \
-        && popd >/dev/null 2>&1
-
-    # This line removed from above, since we are linking to static libs:
-    # && cp -a ./lib/* "${PREFIX}/lib/" \
+if [ "${arch}" != "macosx_arm64" ]; then
+    echo "mpicc = $(which mpicc)"
+    echo "mpicxx = $(which mpicxx)"
+    echo "mpirun = $(which mpirun)"
+    pip install mpi4py
 fi
