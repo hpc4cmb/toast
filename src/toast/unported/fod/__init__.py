be2bc2b68cdf2c2bde2f7d705cd47e8b72d3b3bb
Theodore Kisner <tskisner.public@gmail.com>
2022-07-09 06:59:05 -0700

    Finally remove the unported directory.

--- src/toast/unported/fod/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2015-2020 by the parties listed in the AUTHORS file.
-# All rights reserved.  Use of this source code is governed by
-# a BSD-style license that can be found in the LICENSE file.
-
-from .noise_estimation import OpNoiseEstim
-from .psd_math import autocov_psd, crosscov_psd
