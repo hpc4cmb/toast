a1d7fd61c6a5630024a99fb178f35023de2cb9cd
Theodore Kisner <tskisner.public@gmail.com>
2022-10-24 09:00:28 -0700

    When building MacOS wheels, compiling with numpy 1.23 seems necessary to avoid a segfault in the unit tests.

--- wheels/debug_local_macos.sh
+++ wheels/debug_local_macos.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# This script builds against dependencies previously installed by the
-# "test_local_macos.sh".  It is useful for repeated build and debug
-# cycles against the same dependencies used by the deployed wheels.
+# This script tests the local use of setup.py on MacOS.  It assumes
+# that you have homebrew installed and have the "brew" command in
+# your PATH.
+#
+# This script takes one argument, which is the path to the virtualenv
+# to use for installation.  If that path does not exist, it will be
+# created.
 #
 
 set -e
@@ -21,7 +25,8 @@ fi
 venv_path=$1
 if [ "x${venv_path}" = "x" ]; then
     echo "Usage:  $0 <virtualenv path>"
-    echo "  The virtualenv must exist and will be activated."
+    echo "  If the path to the virtualenv exists, it will be activated."
+    echo "  Otherwise it will be created."
     exit 1
 fi
 
@@ -30,18 +35,33 @@ if [ "x$(type -t deactivate)" != "x" ]; then
     deactivate
 fi
 
+# Path to homebrew
+brew_root=$(dirname $(dirname ${brew_com}))
+echo "Using homebrew installation in ${brew_root}"
+
 # Export compiler information
 export CC=clang
 export CXX=clang++
 export CFLAGS="-O3 -fPIC"
 export CXXFLAGS="-O3 -fPIC -std=c++11 -stdlib=libc++"
 
+# Use homebrew python for development files
+brew_py="${brew_root}/opt/python@3/bin/python3"
+brew_py_ver=$(eval ${brew_py} --version | awk '{print $2}')
+echo "Using python ${brew_py}, version ${brew_py_ver}"
+
 if [ -d ${venv_path} ]; then
     echo "Virtualenv \"${venv_path}\" exists, activating"
     source "${venv_path}/bin/activate"
+    venv_py_ver=$(python3 --version | awk '{print $2}')
+    if [ "${venv_py_ver}" != "${brew_py_ver}" ]; then
+        echo "Virtualenv python version ${venv_py_ver} does not match"
+        echo "homebrew python version ${brew_py_ver}.  Remove this"
+        echo "virtualenv or use a different path."
+        exit 1
+    fi
 else
-    echo "Virtualenv \"${venv_path}\" does not exist"
-    exit 1
+    echo "Virtualenv \"${venv_path}\" does not exist, run test_local_macos.sh."
 fi
 
 # Look for our dependencies in the virtualenv
