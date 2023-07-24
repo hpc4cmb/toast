791f3ccb6bea466e2a62f43ed9cf7ece7f4804c6
Theodore Kisner <tskisner.public@gmail.com>
2023-07-24 09:52:10 -0700

    Remove debugging.

--- wheels/install_deps_linux.sh
+++ wheels/install_deps_linux.sh
@@ -87,36 +87,34 @@ fi
 # Install build requirements.
 CC="${CC}" CFLAGS="${CFLAGS}" pip install -v "numpy<${numpy_ver}" -r "${scriptdir}/build_requirements.txt"
 
-# # Install Openblas
+# Install Openblas
 
-# openblas_version=0.3.23
-# openblas_dir=OpenBLAS-${openblas_version}
-# openblas_pkg=${openblas_dir}.tar.gz
+openblas_version=0.3.23
+openblas_dir=OpenBLAS-${openblas_version}
+openblas_pkg=${openblas_dir}.tar.gz
 
-# if [ ! -e ${openblas_pkg} ]; then
-#     echo "Fetching OpenBLAS..."
-#     curl -SL https://github.com/xianyi/OpenBLAS/archive/v${openblas_version}.tar.gz -o ${openblas_pkg}
-# fi
+if [ ! -e ${openblas_pkg} ]; then
+    echo "Fetching OpenBLAS..."
+    curl -SL https://github.com/xianyi/OpenBLAS/archive/v${openblas_version}.tar.gz -o ${openblas_pkg}
+fi
 
-# echo "Building OpenBLAS..."
+echo "Building OpenBLAS..."
 
-# shr="NO_STATIC=1"
-# if [ "${static}" = "yes" ]; then
-#     shr="NO_SHARED=1"
-# fi
+shr="NO_STATIC=1"
+if [ "${static}" = "yes" ]; then
+    shr="NO_SHARED=1"
+fi
 
-# rm -rf ${openblas_dir}
-# tar xzf ${openblas_pkg} \
-#     && pushd ${openblas_dir} >/dev/null 2>&1 \
-#     && make USE_OPENMP=1 ${shr} \
-#     MAKE_NB_JOBS=${MAKEJ} \
-#     CC="${CC}" FC="${FC}" DYNAMIC_ARCH=1 TARGET=GENERIC \
-#     COMMON_OPT="${CFLAGS}" FCOMMON_OPT="${FCFLAGS}" \
-#     EXTRALIB="-fopenmp -lm ${FCLIBS}" all \
-#     && make ${shr} DYNAMIC_ARCH=1 TARGET=GENERIC PREFIX="${PREFIX}" install \
-#     && popd >/dev/null 2>&1
-
-# exit 0
+rm -rf ${openblas_dir}
+tar xzf ${openblas_pkg} \
+    && pushd ${openblas_dir} >/dev/null 2>&1 \
+    && make USE_OPENMP=1 ${shr} \
+    MAKE_NB_JOBS=${MAKEJ} \
+    CC="${CC}" FC="${FC}" DYNAMIC_ARCH=1 TARGET=GENERIC \
+    COMMON_OPT="${CFLAGS}" FCOMMON_OPT="${FCFLAGS}" \
+    EXTRALIB="-fopenmp -lm ${FCLIBS}" all \
+    && make ${shr} DYNAMIC_ARCH=1 TARGET=GENERIC PREFIX="${PREFIX}" install \
+    && popd >/dev/null 2>&1
 
 # Install FFTW
 
