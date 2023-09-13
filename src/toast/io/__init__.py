108a6a71ee6e54d77f53ab8c24fbdfd37ecfb653
Theodore Kisner <tskisner.public@gmail.com>
2023-09-13 14:58:16 -0700

    Extract HDF5 observation metadata loading into a separate function

--- src/toast/io/__init__.py
+++ src/toast/io/__init__.py
@@ -6,5 +6,5 @@
 
 from .compression import compress_detdata, decompress_detdata
 from .hdf_utils import H5File, have_hdf5_parallel, hdf5_config, hdf5_open
-from .observation_hdf_load import load_hdf5
+from .observation_hdf_load import load_hdf5, load_hdf5_obs_meta
 from .observation_hdf_save import save_hdf5
