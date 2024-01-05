93339cdf8ee875c511fc2f198af877be458b5874
keskitalo <reijo.keskitalo@gmail.com>
2024-01-05 14:07:10 -0800

    Add --sample_rate and --fsample to toast_sim_satellite.py

--- src/toast/instrument.py
+++ src/toast/instrument.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2023 by the parties listed in the AUTHORS file.
+# Copyright (c) 2019-2024 by the parties listed in the AUTHORS file.
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
 
