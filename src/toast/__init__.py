f2688a725ff40cbaccd0bce2ce43bb9c8a9dd4db
keskitalo <reijo.keskitalo@gmail.com>
2024-02-05 14:20:18 -0800

    Sort imports

--- src/toast/__init__.py
+++ src/toast/__init__.py
@@ -93,8 +93,8 @@ from .intervals import IntervalList, interval_dtype
 from .job import job_group_size
 from .mpi import Comm, get_world
 from .observation import Observation
+from .ops import ObsMat
 from .pixels import PixelData, PixelDistribution
 from .timing import GlobalTimers, Timer
 from .traits import create_from_config
 from .weather import Weather
-from .ops import ObsMat
