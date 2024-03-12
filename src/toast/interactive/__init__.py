2883b17da12d9e01aadc704accac8c9220899c25
Theodore Kisner <tskisner.public@gmail.com>
2024-03-11 22:52:29 -0700

    Fix stokes sign in docs.  Add start of interactive submodule

---
+++ src/toast/interactive/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024-2024 by the parties listed in the AUTHORS file.
+# All rights reserved.  Use of this source code is governed by
+# a BSD-style license that can be found in the LICENSE file.
+
+# Namespace imports
+from .startup import start_parallel
+from .magic import load_ipython_extension
