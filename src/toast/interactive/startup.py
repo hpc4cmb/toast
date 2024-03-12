9a8b9cc574a655def02359228c6a4c98c77e1e59
Theodore Kisner <tskisner.public@gmail.com>
2024-03-11 22:52:29 -0700

    Fix stokes sign in docs.  Add start of interactive submodule

---
+++ src/toast/interactive/startup.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2024-2024 by the parties listed in the AUTHORS file.
+# All rights reserved.  Use of this source code is governed by
+# a BSD-style license that can be found in the LICENSE file.
+"""Tools for starting up interactive sessions.
+"""
+
+import os
+from IPython.core.extensions import ExtensionManager
+
+
+def start_parallel(procs=1, threads=1, nice=True, auto_mpi=False):
+    """Attempt to start up ipyparallel with mpi4py and OpenMP.
+
+    Args:
+        procs (int):  The number of processes to use.
+        threads (int):  The number of OpenMP threads per process.
+        nice (bool):  If True, "nice" the processes to a lower
+            priority.
+
+    Returns:
+        (int):  The number of processes started.
+
+    """
+    cores = procs * threads
+    print(f"Using {procs} processes with {threads} threads each.")
+
+    os.environ["OMP_NUM_THREADS"] = f"{threads}"
+
+    if procs > 1:
+        try:
+            import ipyparallel as ipp
+            ipp.bind_kernel()
+            cluster = ipp.Cluster(engines="mpi", n=procs)
+            client = cluster.start_and_connect_sync()
+            client.block = True
+            if nice:
+                # Optionally nice the individual processes if running on a 
+                # shared node.
+                if procs > 1:
+                    import psutil
+                    psutil.Process().nice(
+                        20 if psutil.POSIX else psutil.IDLE_PRIORITY_CLASS
+                    )
+            # Optionally enable automatic use of MPI
+            if auto_mpi:
+                pass
+            # We can turn on automatic use of MPI with:
+            # %autopx
+        except:
+            procs = 1
+            print(f"Failed to start ipyparallel cluster, using one process.")
+    extmanager = ExtensionManager()
+    _ = extmanager.load_extension("wurlitzer")
+    return procs
+
