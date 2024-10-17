925abfbdda374cd2c5f695ae66915a5634aa5dbb
Theodore Kisner <tskisner.public@gmail.com>
2024-10-16 20:23:40 -0700

    Fix typo, enable debugging on lapack name mangling check

--- packaging/wheels/install_deps_osx.sh
+++ packaging/wheels/install_deps_osx.sh
@@ -126,7 +126,7 @@ export CLEANUP=yes
 
 export BLAS_LIBRARIES="/usr/local/lib/libscipy_openblas.dylib"
 export LAPACK_LIBRARIES="/usr/local/lib/libscipy_openblas.dylib"
-export BLA_OPTIONS="-DBLA_PREFER_PKGCONFIG=1 -DBLA_PKGCONFIG_BLAS=scipy-openblas -DBLAS_NO_UNDERSCORE"
+export BLA_OPTIONS="-DBLA_PREFER_PKGCONFIG=1 -DBLA_PKGCONFIG_BLAS=scipy-openblas -DBLAS_NO_UNDERSCORE=1"
 
 for pkg in fftw libflac suitesparse libaatm; do
     source "${depsdir}/${pkg}.sh"
