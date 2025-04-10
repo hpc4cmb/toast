e991424dd434ac8f4d2bd0f0d855593ccd3c57d7
keskitalo <reijo.keskitalo@gmail.com>
2025-04-10 13:22:33 -0700

    Upgrade to match new module name

--- src/toast/tests/ops_scan_healpix_detector.py
+++ src/toast/tests/ops_scan_healpix_detector.py
@@ -12,15 +12,20 @@ from .. import ops as ops
 from ..observation import default_values as defaults
 from ..pixels import PixelData
 from ..pixels_io_healpix import write_healpix_fits, write_healpix_hdf5
-from ._helpers import (close_data, create_fake_healpix_scanned_tod,
-                       create_fake_mask, create_outdir, create_satellite_data)
+from .helpers import (
+    close_data,
+    create_fake_healpix_scanned_tod,
+    create_fake_mask,
+    create_outdir,
+    create_satellite_data,
+)
 from .mpi import MPITestCase
 
 
 class ScanHealpixTest(MPITestCase):
     def setUp(self):
         fixture_name = os.path.splitext(os.path.basename(__file__))[0]
-        self.outdir = create_outdir(self.comm, fixture_name)
+        self.outdir = create_outdir(self.comm, subdir=fixture_name)
         np.random.seed(123456)
 
     def test_healpix_fits(self):
