Add initial unit test.  Fix normalization.

src/toast/ops/noise_filter.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2025-2025 by the parties listed in the AUTHORS file.
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
+import os
 
 import numpy as np
 from astropy import units as u
@@ -8,7 +9,8 @@ from astropy import units as u
 from ..fft import convolve
 from ..observation import default_values as defaults
 from ..timing import function_timer
-from ..traits import Int, Unicode, trait_docs
+from ..traits import Int, Unicode, Quantity, trait_docs
+from .noise_model import estimate_net
 from .operator import Operator
 
 
@@ -70,6 +72,18 @@ class NoiseFilter(Operator):
         defaults.noise_model, help="Observation key containing the noise model"
     )
 
+    white_noise_min = Quantity(
+        None,
+        allow_none=True,
+        help="The minimum frequency to consider for the white noise plateau",
+    )
+
+    white_noise_max = Quantity(
+        None,
+        allow_none=True,
+        help="The maximum frequency to consider for the white noise plateau",
+    )
+
     debug = Unicode(
         None,
         allow_none=True,
@@ -81,6 +95,12 @@ class NoiseFilter(Operator):
 
     @function_timer
     def _exec(self, data, detectors=None, **kwargs):
+        if self.white_noise_max is not None:
+            # Ensure that the min is also set
+            if self.white_noise_min is None:
+                msg = "You must set both of the min / max values or neither of them"
+                raise RuntimeError(msg)
+
         for obs in data.obs:
             dets = obs.select_local_detectors(detectors, flagmask=self.det_mask)
             if len(dets) == 0:
@@ -95,32 +115,59 @@ class NoiseFilter(Operator):
             # Construct the N_tt'^-1 kernels for these detectors
             kernels = list()
             kern_freq = None
+            psd = None
             for d in dets:
                 nse = obs[self.noise_model]
                 freq = nse.freq(d)
                 if kern_freq is None:
                     kern_freq = freq
+                    psd = np.zeros(len(kern_freq), dtype=np.float64)
                 else:
                     if not np.allclose(kern_freq, freq):
                         msg = "All detectors in the noise model must have the same"
                         msg += " frequency binning"
                         raise RuntimeError(msg)
-                psd = np.array(nse.psd(d))
-                psd_max = np.amax(psd)
-                psd_limit = 1.0e-8 * psd_max
+                psd[:] = nse.psd(d).value
+
+                # Estimate the NET, so that we can properly treat the 1/f in the
+                # kernel.
+                if self.white_noise_max is None:
+                    net = estimate_net(freq.value, psd)
+                else:
+                    plateau_samples = np.logical_and(
+                        (freq > self.white_noise_min.to_value(u.Hz)),
+                        (freq < self.white_noise_max.to_value(u.Hz)),
+                    )
+                    net = np.sqrt(np.mean(psd[plateau_samples]))
+
+                # The white noise value sets the scale things we care about.  If
+                # other frequencies have been filtered out, make sure that these
+                # components do not blow up in the kernel.  Also normalize the
+                # kernel so that the white noise levels are not impacted.
+                net_sq = net**2
+                psd_limit = 1.0e-3 * net_sq
                 cut = psd < psd_limit
                 psd[cut] = psd_limit
-                kernels.append(1.0 / psd**2)
+                psd[:] = 1 / psd
+                norm = net_sq
+                psd *= norm
+                psd[0] = 0
+                kernels.append(psd)
             kernels = np.array(kernels)
 
             # Convolve this inverse noise covariance with the detector timestreams
+            debug_root = None
+            if self.debug is not None:
+                debug_root = os.path.join(
+                    self.debug, f"noise_filter_{obs.name}_{dets[0]}-{dets[-1]}"
+                )
             convolve(
                 signal,
                 rate,
                 kernel_freq=kern_freq,
                 kernels=kernels,
                 algorithm="numpy",
-                debug=self.debug_root,
+                debug=debug_root,
             )
 
     def _finalize(self, data, **kwargs):

src/toast/ops/noise_model.py
@@ -104,6 +104,65 @@ class DefaultNoiseModel(Operator):
         return prov
 
 
+def estimate_net(freqs, data):
+    """Estimate the NET from the high frequency PSD.
+
+    This assumes that at high frequency the PSD has a white noise "plateau".  A simple
+    parabola is fit to the last bit of the spectrum and this is used to compute the
+    NET.
+
+    Args:
+        freqs (array):  The frequency values in Hz
+        data (array):  The PSD in arbitrary units
+
+    Returns:
+        (float):  The estimated NET.
+
+    """
+
+    def quad_func(x, a, b, c):
+        # Parabola
+        return a * (x - b) ** 2 + c
+
+    def lin_func(x, a, b, c):
+        # Line
+        return a * (x - b) + c
+
+    n_psd = len(data)
+    offset = int(0.8 * n_psd)
+    try_quad = True
+    if n_psd - offset < 10:
+        # Too few points
+        try_quad = False
+        if n_psd < 10:
+            # Crazy...
+            offset = 0
+        else:
+            offset = n_psd - 10
+
+    ffreq = np.log(freqs[offset:])
+    fdata = np.log(data[offset:])
+    if try_quad:
+        try:
+            params, params_cov = curve_fit(
+                quad_func, ffreq, fdata, p0=[1.0, ffreq[-1], fdata[-1]]
+            )
+            # It worked!
+            fdata = quad_func(ffreq, params[0], params[1], params[2])
+            fdata = np.exp(fdata)
+            return np.sqrt(fdata[-1])
+        except RuntimeError:
+            pass
+
+    params, params_cov = curve_fit(
+        lin_func, ffreq, fdata, p0=[0.0, ffreq[-1], fdata[-1]]
+    )
+    fdata = lin_func(ffreq, params[0], params[1], params[2])
+    fdata = np.exp(fdata)
+    net = np.sqrt(fdata[-1])
+    return net
+
+
 @trait_docs
 class FitNoiseModel(Operator):
     """Perform a least squares fit to an existing noise model.
@@ -259,65 +318,6 @@ class FitNoiseModel(Operator):
                 # We are storing this in a new key
                 ob[self.out_model] = new_model
         return
-
-    def _estimate_net(self, freqs, data):
-        """Estimate the NET from the high frequency PSD.
-
-        This assumes that at high frequency the PSD has a white noise "plateau".  A simple
-        parabola is fit to the last bit of the spectrum and this is used to compute the
-        NET.
-
-        Args:
-            freqs (array):  The frequency values in Hz
-            data (array):  The PSD in arbitrary units
-
-        Returns:
-            (float):  The estimated NET.
-
-        """
-        log = Logger.get()
-
-        def quad_func(x, a, b, c):
-            # Parabola
-            return a * (x - b) ** 2 + c
-
-        def lin_func(x, a, b, c):
-            # Line
-            return a * (x - b) + c
-
-        n_psd = len(data)
-        offset = int(0.8 * n_psd)
-        try_quad = True
-        if n_psd - offset < 10:
-            # Too few points
-            try_quad = False
-            if n_psd < 10:
-                # Crazy...
-                offset = 0
-            else:
-                offset = n_psd - 10
-
-        ffreq = np.log(freqs[offset:])
-        fdata = np.log(data[offset:])
-        if try_quad:
-            try:
-                params, params_cov = curve_fit(
-                    quad_func, ffreq, fdata, p0=[1.0, ffreq[-1], fdata[-1]]
-                )
-                # It worked!
-                fdata = quad_func(ffreq, params[0], params[1], params[2])
-                fdata = np.exp(fdata)
-                return np.sqrt(fdata[-1])
-            except RuntimeError:
-                pass
-
-        params, params_cov = curve_fit(
-            lin_func, ffreq, fdata, p0=[0.0, ffreq[-1], fdata[-1]]
-        )
-        fdata = lin_func(ffreq, params[0], params[1], params[2])
-        fdata = np.exp(fdata)
-        net = np.sqrt(fdata[-1])
-        return net
 
     def _evaluate_model(self, freqs, fmin, net, fknee, alpha):
         """Evaluate the noise model
@@ -529,7 +529,7 @@ class FitNoiseModel(Operator):
         raw_fmin = self.f_min.to_value(u.Hz)
 
         if self.white_noise_max is None:
-            net = self._estimate_net(input_freqs, input_data)
+            net = estimate_net(input_freqs, input_data)
         else:
             plateau_samples = np.logical_and(
                 (input_freqs > self.white_noise_min.to_value(u.Hz)),