5a7c23cac93a2425081176eab0e42adb372c949b
Theodore Kisner <tskisner.public@gmail.com>
2026-01-05 19:32:18 -0800

    Address review comments

--- etc/hdf5_formats/save_load.py
+++ etc/hdf5_formats/save_load.py
@@ -4,7 +4,7 @@
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
 
-"""Test saving and loading different HDF5 formats"""
+"""Test saving and loading HDF5 formats"""
 
 import argparse
 
