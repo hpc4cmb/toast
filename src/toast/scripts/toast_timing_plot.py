fc5ca9f97adbe9c9c565e88465125c938c514859
keskitalo <reijo.keskitalo@gmail.com>
2026-01-12 11:31:43 -0800

    Update copyright

--- src/toast/scripts/toast_timing_plot.py
+++ src/toast/scripts/toast_timing_plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2021-2021 by the parties listed in the AUTHORS file.
+# Copyright (c) 2021-2026 by the parties listed in the AUTHORS file.
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
 
