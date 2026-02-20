5896a63591b952d713c2fa3e33ef7b6d90201558
keskitalo <reijo.keskitalo@gmail.com>
2026-02-20 10:33:36 -0800

    Add option to use the focalplane to identify pixels

--- src/toast/ops/decimate_focalplane.py
+++ src/toast/ops/decimate_focalplane.py
@@ -38,6 +38,13 @@ class DecimateFocalplane(Operator):
         help="Bit mask value for per-detector flagging",
     )
 
+    pixel_property = Unicode(
+        None,
+        allow_none=True,
+        help="Focalplane property for identifying different pixels. "
+        "Overrides `detectors_per_pixel`",
+    )
+
     @traitlets.validate("det_mask")
     def _check_det_mask(self, proposal):
         check = proposal["value"]
@@ -53,7 +60,7 @@ class DecimateFocalplane(Operator):
         if self.nskip == 1:
             # No flagging to do
             return
-        
+
         log = Logger.get()
         wcomm = data.comm.comm_world
         comm = data.comm.comm_group
@@ -66,28 +73,54 @@ class DecimateFocalplane(Operator):
                 log.error(msg)
                 raise RuntimeError(msg)
 
-            local_dets = sorted(ob.select_local_detectors(detectors))
-            good_dets = set(ob.select_local_detectors(detectors, flagmask=self.det_mask))
-            ndet = len(local_dets)
-            ndet_per_pix = self.detectors_per_pixel
-            npix = ndet // ndet_per_pix
-            nskip = self.nskip
+            # The focalplane is identical on all processes in this group
+            # so we assign and flag pixels locally
 
+            fp = ob.telescope.focalplane
+
+            if self.pixel_property is not None:
+                # Assign detectors to pixels based on their focalplane properties
+                if self.pixel_property not in fp.properties:
+                    msg = f"{self.pixel_property} is not a property of the "
+                    msg += f"focaplane in {ob.name}"
+                    raise RuntimeError(msg)
+                pixels = sorted(set(fp.detector_data[self.pixel_property]))
+                keep_pixels = set(pixels[:: self.nskip])
+                flag_pixels = set(pixels) - keep_pixels
+                det_to_pixel = dict(
+                    zip(fp.detectors, fp.detector_data[self.pixel_property])
+                )
+            else:
+                # Use detector index to infer a dummy pixel name
+                ndet_per_pix = self.detectors_per_pixel
+                ndet = len(fp.detectors)
+                npix = ndet // ndet_per_pix
+                pixels = np.arange(npix, dtype=int)
+                keep_pixels = set(pixels[:: self.nskip])
+                flag_pixels = set(pixels) - keep_pixels
+                det_to_pixel = {}
+                for idet, det in enumerate(fp.detectors):
+                    det_to_pixel[det] = idet // ndet_per_pix
+
+            local_dets = ob.select_local_detectors(detectors)
+            good_dets = set(
+                ob.select_local_detectors(detectors, flagmask=self.det_mask)
+            )
+            ndet = len(local_dets)
             ntot = len(local_dets)
             ngood = len(good_dets)
             nbad = ntot - ngood
             ndecimated = 0
 
             decimate_flags = {}
-            for ipix in range(npix):
-                if ipix % nskip != 0:
+            for det in local_dets:
+                pix = det_to_pixel[det]
+                if pix in flag_pixels:
                     # Flag this pixel
-                    offset = ipix * ndet_per_pix
-                    for idet in range(offset, offset + ndet_per_pix):
-                        det = local_dets[idet]
-                        decimate_flags[det] = self.det_mask
-                        if det in good_dets:
-                            ndecimated += 1
+                    decimate_flags[det] = self.det_mask
+                    if det in good_dets:
+                        ndecimated += 1
+
             ob.update_local_detector_flags(decimate_flags)
 
             if comm is not None:
@@ -108,7 +141,9 @@ class DecimateFocalplane(Operator):
         if detectors is None:
             log.info_rank(f"Applied {type(self).__name__} in", comm=wcomm, timer=timer0)
         else:
-            log.debug_rank(f"Applied {type(self).__name__} in", comm=wcomm, timer=timer0)
+            log.debug_rank(
+                f"Applied {type(self).__name__} in", comm=wcomm, timer=timer0
+            )
 
         return
 
--- src/toast/tests/ops_decimate_focalplane.py
+++ src/toast/tests/ops_decimate_focalplane.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 by the parties listed in the AUTHORS file.
+# Copyright (c) 2025-2026 by the parties listed in the AUTHORS file.
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
 
@@ -9,7 +9,7 @@ import numpy as np
 from .. import ops as ops
 from ..mpi import MPI
 from ..observation import default_values as defaults
-from .helpers import close_data, create_ground_data, create_outdir
+from .helpers import close_data, create_satellite_data, create_outdir
 from .mpi import MPITestCase
 
 
@@ -20,7 +20,7 @@ class DecimateFocalplaneTest(MPITestCase):
 
     def test_no_decimation(self):
         """Test that nskip=1 results in no flagging."""
-        data = create_ground_data(self.comm, pixel_per_process=10)
+        data = create_satellite_data(self.comm, pixel_per_process=10)
 
         # Count detectors before decimation
         pre_total_dets = 0
@@ -45,9 +45,7 @@ class DecimateFocalplaneTest(MPITestCase):
             )
 
         if data.comm.comm_world is not None:
-            pre_total_dets = data.comm.comm_world.allreduce(
-                pre_total_dets, op=MPI.SUM
-            )
+            pre_total_dets = data.comm.comm_world.allreduce(pre_total_dets, op=MPI.SUM)
             post_total_dets = data.comm.comm_world.allreduce(
                 post_total_dets, op=MPI.SUM
             )
@@ -59,7 +57,7 @@ class DecimateFocalplaneTest(MPITestCase):
 
     def test_basic_decimation(self):
         """Test basic decimation with detectors_per_pixel=1."""
-        data = create_ground_data(self.comm, pixel_per_process=20)
+        data = create_satellite_data(self.comm, pixel_per_process=20)
 
         # Count detectors before decimation
         pre_good_dets = 0
@@ -91,15 +89,9 @@ class DecimateFocalplaneTest(MPITestCase):
             )
 
         if data.comm.comm_world is not None:
-            pre_good_dets = data.comm.comm_world.allreduce(
-                pre_good_dets, op=MPI.SUM
-            )
-            pre_total_dets = data.comm.comm_world.allreduce(
-                pre_total_dets, op=MPI.SUM
-            )
-            post_good_dets = data.comm.comm_world.allreduce(
-                post_good_dets, op=MPI.SUM
-            )
+            pre_good_dets = data.comm.comm_world.allreduce(pre_good_dets, op=MPI.SUM)
+            pre_total_dets = data.comm.comm_world.allreduce(pre_total_dets, op=MPI.SUM)
+            post_good_dets = data.comm.comm_world.allreduce(post_good_dets, op=MPI.SUM)
             post_total_dets = data.comm.comm_world.allreduce(
                 post_total_dets, op=MPI.SUM
             )
@@ -121,36 +113,38 @@ class DecimateFocalplaneTest(MPITestCase):
 
     def test_pixel_grouping(self):
         """Test decimation with detectors_per_pixel=2."""
-        data = create_ground_data(
+        data = create_satellite_data(
             self.comm, pixel_per_process=30, flagged_pixels=False
         )
 
         # Apply decimation with pixel grouping
         nskip = 3
         detectors_per_pixel = 2
+        flagmask = defaults.det_mask_invalid
         decimate = ops.DecimateFocalplane(
             nskip=nskip,
             detectors_per_pixel=detectors_per_pixel,
-            det_mask=defaults.det_mask_invalid,
+            det_mask=flagmask,
         )
         decimate.apply(data)
 
         # Verify that detectors are flagged in groups
         # Since flagged_pixels=False, all detectors start unflagged
         for obs in data.obs:
-            local_dets = sorted(obs.local_detectors)
-            ndet = len(local_dets)
+            local_dets = set(obs.local_detectors)
+            all_dets = obs.telescope.focalplane.detectors
+            ndet = len(all_dets)
             npix = ndet // detectors_per_pixel
 
             for ipix in range(npix):
                 # Check if this pixel should be flagged
-                should_be_flagged = (ipix % nskip != 0)
+                should_be_flagged = ipix % nskip != 0
 
                 offset = ipix * detectors_per_pixel
                 for idet in range(offset, offset + detectors_per_pixel):
-                    if idet >= ndet:
-                        break
-                    det = local_dets[idet]
+                    det = all_dets[idet]
+                    if det not in local_dets:
+                        continue
                     # Get detector flags
                     det_flag = obs.local_detector_flags[det]
                     is_flagged = (det_flag & defaults.det_mask_invalid) != 0
@@ -169,9 +163,54 @@ class DecimateFocalplaneTest(MPITestCase):
 
         close_data(data)
 
+    def test_pixel_attribute(self):
+        """Test decimation with focalplane property for pixel name."""
+        data = create_satellite_data(
+            self.comm, pixel_per_process=30, flagged_pixels=False
+        )
+
+        # Apply decimation with pixel grouping
+        nskip = 3
+        flagmask = defaults.det_mask_invalid
+        decimate = ops.DecimateFocalplane(
+            nskip=nskip,
+            pixel_property="pixel",
+            det_mask=flagmask,
+        )
+        decimate.apply(data)
+
+        # Verify that detectors are flagged in groups
+        # Since flagged_pixels=False, all detectors start unflagged
+        for obs in data.obs:
+            # Verify that the fraction of flagged detectors is reasonable
+            local_dets = obs.local_detectors
+            good_dets = obs.select_local_detectors(flagmask=flagmask)
+            good_frac = len(good_dets) / len(local_dets)
+            self.assertTrue(good_frac > 1 / nskip / 2)
+            self.assertTrue(good_frac < 1 / nskip * 2)
+
+            # Confirm that all detectors in the pixel have matching flagging
+            fp = obs.telescope.focalplane
+            for det in local_dets:
+                det_pix = fp[det]["pixel"]
+                other_detectors = [
+                    name
+                    for (name, pixel) in zip(
+                        fp.detector_data["name"], fp.detector_data["pixel"]
+                    )
+                    if pixel == det_pix
+                ]
+                for other_det in other_detectors:
+                    self.assertTrue(
+                        obs.local_detector_flags[det]
+                        == obs.local_detector_flags[other_det]
+                    )
+
+        close_data(data)
+
     def test_interaction_with_existing_flags(self):
         """Test that decimation works correctly with pre-existing detector flags."""
-        data = create_ground_data(self.comm, pixel_per_process=20)
+        data = create_satellite_data(self.comm, pixel_per_process=20)
 
         # Pre-flag some detectors
         for obs in data.obs:
@@ -228,7 +267,7 @@ class DecimateFocalplaneTest(MPITestCase):
 
     def test_decimation_pattern(self):
         """Test that the decimation pattern is correct - keep every nskip-th detector."""
-        data = create_ground_data(
+        data = create_satellite_data(
             self.comm, pixel_per_process=50, single_group=True, flagged_pixels=False
         )
 
@@ -243,31 +282,33 @@ class DecimateFocalplaneTest(MPITestCase):
         # Check the pattern for each observation
         # Since we used flagged_pixels=False, all detectors start unflagged
         for obs in data.obs:
-            local_dets = sorted(obs.local_detectors)
+            all_detectors = obs.telescope.focalplane.detectors
+            local_dets = set(obs.local_detectors)
             ndet = len(local_dets)
 
             kept_count = 0
             flagged_count = 0
 
-            for idx in range(ndet):
-                det = local_dets[idx]
+            for idet, det in enumerate(all_detectors):
+                if det not in local_dets:
+                    continue
                 det_flag = obs.local_detector_flags[det]
                 is_flagged = (det_flag & defaults.det_mask_invalid) != 0
 
                 # Every nskip-th detector (0, nskip, 2*nskip, ...) should NOT be flagged by decimation
                 # All others should be flagged
-                if idx % nskip == 0:
+                if idet % nskip == 0:
                     # This detector should be kept (not flagged by decimation)
                     self.assertFalse(
                         is_flagged,
-                        f"Detector {det} at index {idx} should not be flagged (idx % nskip == 0)",
+                        f"Detector {det} at index {idet} should not be flagged (idet % nskip == 0)",
                     )
                     kept_count += 1
                 else:
                     # This detector should be flagged by decimation
                     self.assertTrue(
                         is_flagged,
-                        f"Detector {det} at index {idx} should be flagged (idx % nskip != 0)",
+                        f"Detector {det} at index {idet} should be flagged (idet % nskip != 0)",
                     )
                     flagged_count += 1
 
