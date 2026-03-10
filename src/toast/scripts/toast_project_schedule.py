1cd3d94ad0ec212d58b46dc0725ae99c55e0bb30
keskitalo <reijo.keskitalo@gmail.com>
2026-03-10 15:53:56 -0700

    Update copyright

--- src/toast/scripts/toast_project_schedule.py
+++ src/toast/scripts/toast_project_schedule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2025 by the parties listed in the AUTHORS file.
+# Copyright (c) 2025-2026 by the parties listed in the AUTHORS file.
 # All rights reserved.  Use of this source code is governed by
 # a BSD-style license that can be found in the LICENSE file.
 
